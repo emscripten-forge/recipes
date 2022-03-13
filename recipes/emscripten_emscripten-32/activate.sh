8d7dd3d4ab71b82611fe72ebebb812116d3bbf14
DerThorsten <derthorstenbeier@gmail.com>
2022-03-13 18:29:25 +0100

    even more emscripten hacks

--- recipes/emscripten_emscripten-32/activate.sh
+++ recipes/emscripten_emscripten-32/activate.sh
@@ -1,9 +1,11 @@
 echo "ACTIVATE ALIASES FRESH!!!"
 
 unset PYTHON
-emsdk install  3.1.2
-emsdk activate 3.1.2
 
+pushd $EMSDK_DIR
+./emsdk install  3.1.2
+./emsdk activate 3.1.2
+popd
 
 export CMAKE_ARGS="$CMAKE_ARGS -DCMAKE_FIND_ROOT_PATH_MODE_PACKAGE=ON -DCMAKE_POSITION_INDEPENDENT_CODE:BOOL=true"
 
