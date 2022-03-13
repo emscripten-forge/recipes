1b25f0cd185f761e601e6b8266ebea4bcba2d00a
DerThorsten <derthorstenbeier@gmail.com>
2022-03-13 16:23:14 +0100

    fixed command name

--- emscripten_emscripten-32/deactivate.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-unset cmake
-unset make
-# unset configue
--- mpdecimal/build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-
-if [[ $target_platform == "emscripten-32" ]]; then
-    emconfigure ./configure --prefix="${PREFIX}"
-    make 
-    make install
-fi
