668fd5ba1d66eb5bbbb1272718660a7f67ae2189
DerThorsten <derthorstenbeier@gmail.com>
2022-03-15 11:42:50 +0100

    adding to path in env

--- recipes/emscripten_emscripten-32/activate.sh
+++ recipes/emscripten_emscripten-32/activate.sh
@@ -6,6 +6,7 @@ pushd $CONDA_EMSDK_DIR
 ./emsdk install  3.1.2
 ./emsdk activate 3.1.2
 source emsdk_env.sh
+export PATH="$CONDA_EMSDK_DIR/upstream/emscripten/":$PATH
 popd
 
 export CMAKE_ARGS="$CMAKE_ARGS -DCMAKE_FIND_ROOT_PATH_MODE_PACKAGE=ON -DCMAKE_POSITION_INDEPENDENT_CODE:BOOL=true"
