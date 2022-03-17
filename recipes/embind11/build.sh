3ae21f2f1ae864334fee37fdc4569f9e2c8800e9
DerThorsten <derthorstenbeier@gmail.com>
2022-03-17 11:20:33 +0100

    build with boa

---
+++ recipes/embind11/build.sh
@@ -0,0 +1,18 @@
+ls
+mkdir build
+cd build
+
+
+export CMAKE_PREFIX_PATH=$PREFIX 
+export CMAKE_SYSTEM_PREFIX_PATH=$PREFIX 
+
+
+# Configure step
+cmake ${CMAKE_ARGS} ..              \
+    -GNinja                         \
+    -DCMAKE_BUILD_TYPE=Release      \
+    -DCMAKE_PREFIX_PATH=$PREFIX     \
+    -DCMAKE_INSTALL_PREFIX=$PREFIX  \
+
+# Build step
+ninja install
