6efe3ff930ff8bb3c5da3d6ae538865e8e270c9a
DerThorsten <derthorstenbeier@gmail.com>
2022-03-22 14:21:29 +0100

    using python 3.10

--- recipes/python/build.sh
+++ recipes/python/build.sh
@@ -1,75 +1,56 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 
+export DBGFLAGS=-g0
+export OPTFLAGS=-O2
+export CFLAGS_BASE="${DBGFLAGS} ${DBGFLAGS} -fPIC"
 
-# Following is needed for building extensions like zlib
-CPPFLAGS=${CPPFLAGS}"-I${PREFIX}/include"
+export PYTHON_CFLAGS=${CFLAGS_BASE}
 
-export LDFLAGS="-L ${PREFIX}/lib"
-# -lffi -lsqlite3 -lbz2"
-export CPPFLAGS CFLAGS CXXFLAGS LDFLAGS
+
+LIB=libpython3.10.a
+SYSCONFIG_NAME=_sysconfigdata__emscripten_
 
 if [[ $target_platform == "emscripten-32" ]]; then
+    cp ${RECIPE_DIR}/config.site .
+
+    CONFIG_SITE=./config.site READELF=true emconfigure \
+      ./configure \
+          CFLAGS="${PYTHON_CFLAGS}" \
+          CPPFLAGS="-I${PREFIX}/include" \
+          LDFLAGS="-L${PREFIX}/lib -lffi -lz" \
+          --without-pymalloc \
+          --disable-shared \
+          --disable-ipv6 \
+          --enable-big-digits=30 \
+          --enable-optimizations \
+          --host=wasm32-unknown-emscripten\
+          --build=$(./config.guess) \
+          --prefix=${PREFIX}  \
+    
+
+    cp ${RECIPE_DIR}/Setup.local ./Modules/
+
+    cat ${RECIPE_DIR}/pyconfig.undefs.h >> ./pyconfig.h
 
 
-    # use our own config site
-    cp ${RECIPE_DIR}/conda.config.site-wasm32-emscripten conda.config.site-wasm32-emscripten
+    emmake make CROSS_COMPILE=yes ${LIB} -j8
 
-    # create build dir
-    mkdir -p builddir/emscripten-browser
-    pushd builddir/emscripten-browser
-
-
-    ##########
-    # configure / makefile 
-    # exports PYTHON as python.html
-    ###############
-    unset PYTHON
-    # export PYTHON=${BUILD_PREFIX}/bin/python3.11
-
-    # make zlib available (todo check if this makes sense in the conda context)
-    embuilder build zlib
-
-
-
-    # CONFIG_SITE=../../conda.config.site-wasm32-emscripten  \
-    # CONFIG_SITE=../../Tools/wasm/config.site-wasm32-emscripten \
-    CONFIG_SITE=../../conda.config.site-wasm32-emscripten READELF=true \
-    emconfigure ../../configure -C \
-        CFLAGS="-fPIC -O2" \
-        --host=wasm32-unknown-emscripten \
-        --enable-optimizations \
-        --without-pymalloc \
-        --disable-shared \
-        --disable-ipv6 \
-        --build=$(../../config.guess) \
-        --with-emscripten-target=browser \
-        --with-build-python=python3.11 \
-        --prefix=${PREFIX}
-
-        # CPPFLAGS=${CPPFLAGS}\
-        # LDFLAGS=${LDFLAGS}\
-        # --enable-optimizations \
-        
-    # build dir is the current dir
-    # cp ${RECIPE_DIR}/from_pyodide/Setup.local Modules/
-    # cat ${RECIPE_DIR}/from_pyodide/pyconfig.undefs.h >> pyconfig.h
 
     sed -i -e 's/libinstall:.*/libinstall:/' Makefile; 
-
-    make CROSS_COMPILE=yes  -j$(nproc) || true
-    make CROSS_COMPILE=yes  install || true
-    # make CROSS_COMPILE=yes -j$(nproc) || true
-    # make CROSS_COMPILE=yes install
-
-    mkdir -p $PREFIX/bin/
-    mkdir -p $PREFIX/lib/python_internal
-    cp -t $PREFIX/bin/ python.* 
-    cp -t $PREFIX/lib/python_internal Modules/_decimal/libmpdec/libmpdec.a
-    cp -t $PREFIX/lib/python_internal Modules/expat/libexpat.a
+    
+    emmake make PYTHON_FOR_BUILD=${BUILD_PREFIX}/bin/python3.10 CROSS_COMPILE=yes inclinstall libinstall ${LIB} 
+    cp ${LIB}  ${PREFIX}/lib/ 
 
 
-    popd
+
+    # # emmake make install
+    # echo "LS"
+    # ls
+    # ls Modules
+    # mkdir -p $PREFIX/lib/python_internal
+    # cp -t $PREFIX/lib/python_internal Modules/_decimal/libmpdec/libmpdec.a
+    # cp -t $PREFIX/lib/python_internal Modules/expat/libexpat.a
 
 else
     mkdir -p build
@@ -80,5 +61,4 @@ else
     make -j$(nproc)
     make install
     popd
-
 fi
