f28de21015399624d305108ad0b8ded423a6e8ab
DerThorsten <derthorstenbeier@gmail.com>
2022-04-03 11:05:32 +0200

    wip

--- build_packages.sh
+++ build_packages.sh
@@ -1,27 +1,23 @@
 #!/bin/bash
 set -e
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+
 BUILD_NAME=$1
 SKIP=$2
 PKG_NAMES=($(python build_all_helper.py $BUILD_NAME 0 | tr -d '[],'))
-FEATURES=($(python build_all_helper.py $BUILD_NAME 1 | tr -d '[],'))
-BUILD_OR_HOST=($(python build_all_helper.py $BUILD_NAME 2 | tr -d '[],'))
+BUILD_OR_HOST=($(python build_all_helper.py $BUILD_NAME 1 | tr -d '[],'))
 
 
 for i in "${!PKG_NAMES[@]}"; do
     PKG_NAME=${PKG_NAMES[i]}
-    FEATS=${FEATURES[i]}
-    if [[ ${FEATS} == "-" ]]; then
-        echo $PKG_NAME "NO FEAT"
-        F=""
-    else
-        echo $PKG_NAME "FEATS" $FEATS
-        F="--features [$FEATS]"
-    fi
+
 
     if [[ ${BUILD_OR_HOST[i]} == "build" ]]; then
-        boa build recipes/$PKG_NAME $F $SKIP
+        boa build recipes/$PKG_NAME  $SKIP
     else
-        boa build recipes/$PKG_NAME --target-platform=emscripten-32  $F $SKIP
+        boa build recipes/$PKG_NAME --target-platform=emscripten-32  $SKIP
+        python better_test_package.py $SCRIPT_DIR/recipes/$PKG_NAME
     fi
 done
