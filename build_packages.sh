f3b4effe25ac16f92055595fa02649f2283246fa
Thorsten Beier <derthorstenbeier@gmail.com>
2022-04-03 13:06:50 +0200

    Testit (#28)
    * tests + numpy

--- build_packages.sh
+++ build_packages.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+
 BUILD_NAME=$1
 SKIP=$2
 PKG_NAMES=($(python build_all_helper.py $BUILD_NAME 0 | tr -d '[],'))
@@ -9,9 +12,12 @@ BUILD_OR_HOST=($(python build_all_helper.py $BUILD_NAME 1 | tr -d '[],'))
 
 for i in "${!PKG_NAMES[@]}"; do
     PKG_NAME=${PKG_NAMES[i]}
+
+
     if [[ ${BUILD_OR_HOST[i]} == "build" ]]; then
-        boa build recipes/$PKG_NAME $SKIP
+        boa build recipes/$PKG_NAME  $SKIP
     else
-        boa build recipes/$PKG_NAME --target-platform=emscripten-32 $SKIP
+        boa build recipes/$PKG_NAME --target-platform=emscripten-32  $SKIP
+        python better_test_package.py $SCRIPT_DIR/recipes/$PKG_NAME
     fi
 done
---
+++ recipes/numpy/test_numpy.py
@@ -0,0 +1,5 @@
+def test_numpy():
+    import numpy
+
+    ones = numpy.ones(shape=[2,3])
+    assert ones.shape == (2,3)
