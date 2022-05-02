8fd8ab48cfcf81fadee82a11689fa01a620fad33
DerThorsten <derthorstenbeier@gmail.com>
2022-05-02 10:02:46 +0200

    moved from converted to root

--- testing/node_test_package.py
+++ testing/node_test_package.py
@@ -46,7 +46,13 @@ let res = (async function() {
   myModule.initialize_interpreter()
   myModule.print = print
   myModule.error = print
+  try{
   var ret = myModule.run_tests("/tests")
+  }
+  catch(e){
+    console.log(e)
+    throw "Test(s) failed!"+String(e); 
+  }
   return ret
 })().then(
   (r) => {
@@ -62,64 +68,73 @@ let res = (async function() {
 
 def get_pytest_files(recipe_dir, recipe):
     recipe_dir = os.path.abspath(recipe_dir)
-    extra = recipe.get('extra',{})
-    emscripten_tests = extra.get('emscripten_tests',{})
-    python = emscripten_tests.get('python',{})
-    pytest_files = python.get('pytest_files',[])
+    extra = recipe.get("extra", {})
+    emscripten_tests = extra.get("emscripten_tests", {})
+    python = emscripten_tests.get("python", {})
+    pytest_files = python.get("pytest_files", [])
     pytest_files = [os.path.join(recipe_dir, f) for f in pytest_files]
     return pytest_files
 
 
 def create_test_env(pkg_name, prefix):
     # cmd = ['$MAMBA_EXE' ,'create','--prefix', prefix,'--platform=emscripten-32'] + [pkg_name] #+ ['--dryrun']
-    print("prefix",prefix)
-    cmd = [f'$MAMBA_EXE create --yes --prefix {prefix} --platform=emscripten-32   python pytest_driver_node pytest {pkg_name}']
+    print("prefix", prefix)
+    cmd = [
+        f"$MAMBA_EXE create --yes --prefix {prefix} --platform=emscripten-32   python pytest_driver_node pytest {pkg_name}"
+    ]
     ret = subprocess.run(cmd, shell=True)
     #  stderr=subprocess.PIPE, stdout=subprocess.PIPE)
     returncode = ret.returncode
     assert returncode == 0
 
+
 def pack(prefix, pytest_files):
     assert len(pytest_files) <= 1, "atm only one file is allowed"
 
-    cmd = [f"emboa pack python core {prefix} --version=3.10 --export-name='global.Module'"]
+    cmd = [
+        f"emboa pack python core {prefix} --version=3.10 --export-name='global.Module'"
+    ]
     ret = subprocess.run(cmd, shell=True)
 
-    cmd = [f"emboa pack file  {pytest_files[0]}  '/tests'  testdata --export-name='global.Module'"]
+    cmd = [
+        f"emboa pack file  {pytest_files[0]}  '/tests'  testdata --export-name='global.Module'"
+    ]
     ret = subprocess.run(cmd, shell=True)
 
 
-def copy_from_prefix(prefix,work_dir):
+def copy_from_prefix(prefix, work_dir):
     # copy wasm / js file to work dir
-    js_file = os.path.join(prefix,'bin','pytest_driver.js')
+    js_file = os.path.join(prefix, "bin", "pytest_driver.js")
     shutil.copy(js_file, work_dir)
-    wasm_file = os.path.join(prefix,'bin','pytest_driver.wasm')
+    wasm_file = os.path.join(prefix, "bin", "pytest_driver.wasm")
     shutil.copy(wasm_file, work_dir)
 
 
 def patch(work_dir):
-    js_file = os.path.join(work_dir,'pytest_driver.js')
+    js_file = os.path.join(work_dir, "pytest_driver.js")
     # Read in the file
-    with open(js_file, 'r') as file :
-      filedata = file.read()
+    with open(js_file, "r") as file:
+        filedata = file.read()
 
     # Replace the target string
-    filedata = filedata.replace('push(audioPlugin)', 'push(imagePlugin)')
+    filedata = filedata.replace("push(audioPlugin)", "push(imagePlugin)")
 
     # Write the file out again
-    with open(js_file, 'w') as file:
-      file.write(filedata)
+    with open(js_file, "w") as file:
+        file.write(filedata)
+
 
 def run_node_tests(work_dir):
     # Write the file out again
-    with open(os.path.join(work_dir,"test.js"), 'w') as file:
+    with open(os.path.join(work_dir, "test.js"), "w") as file:
         file.write(NODE_TEST_FILE_STR)
-    
+
     os.chdir(work_dir)
-    cmd = [f'node  --no-warnings test.js']
+    cmd = [f"node   --trace-uncaught test.js"]
     ret = subprocess.run(cmd, shell=True)
     if ret.returncode != 0:
         raise RuntimeError("Tests Failed")
+
 
 @contextmanager
 def test_dir_context(pkg_name, debug, debug_dir):
@@ -136,19 +151,21 @@ def test_dir_context(pkg_name, debug, debug_dir):
 
 def test_package(recipe_dir, debug, debug_dir):
     assert os.path.isdir(recipe_dir)
-    recipe_file = os.path.join(recipe_dir, 'recipe.yaml')
+    recipe_file = os.path.join(recipe_dir, "recipe.yaml")
 
     has_tests = False
-    with open(recipe_file, 'r') as stream:
-        recipe=yaml.safe_load(stream)
+    with open(recipe_file, "r") as stream:
+        recipe = yaml.safe_load(stream)
     pytest_files = get_pytest_files(recipe_dir, recipe)
     has_tests = len(pytest_files) > 0
 
     if has_tests:
-        pkg_name = recipe['package']['name']
-        with test_dir_context(pkg_name=pkg_name, debug=debug, debug_dir=debug_dir) as test_dir:
-            prefix = os.path.join(test_dir,'prefix')
-            work_dir = os.path.join(test_dir,'work')
+        pkg_name = recipe["package"]["name"]
+        with test_dir_context(
+            pkg_name=pkg_name, debug=debug, debug_dir=debug_dir
+        ) as test_dir:
+            prefix = os.path.join(test_dir, "prefix")
+            work_dir = os.path.join(test_dir, "work")
             os.mkdir(work_dir)
             os.chdir(work_dir)
             create_test_env(pkg_name=pkg_name, prefix=prefix)
@@ -159,13 +176,15 @@ def test_package(recipe_dir, debug, debug_dir):
             patch(work_dir=work_dir)
             run_node_tests(work_dir=work_dir)
 
+
 if __name__ == "__main__":
 
     import argparse
+
     parser = argparse.ArgumentParser()
     parser.add_argument("recipe_dir")
-    parser.add_argument('--debug',     action=argparse.BooleanOptionalAction)
-    parser.add_argument('--debug-dir', type=str)
+    parser.add_argument("--debug", action=argparse.BooleanOptionalAction)
+    parser.add_argument("--debug-dir", type=str)
     args = parser.parse_args()
     print(args.debug_dir)
     if args.debug and args.debug_dir is None:
@@ -173,4 +192,5 @@ if __name__ == "__main__":
     test_package(args.recipe_dir, debug=args.debug, debug_dir=args.debug_dir)
 
     import sys
+
     sys.exit(0)
