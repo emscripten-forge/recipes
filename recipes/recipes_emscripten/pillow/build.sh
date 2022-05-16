b8457106cd53f6a3985ac859668052447ab83865
DerThorsten <derthorstenbeier@gmail.com>
2022-05-16 15:00:06 +0200

    use embuilder

--- recipes/recipes_emscripten/pillow/build.sh
+++ recipes/recipes_emscripten/pillow/build.sh
@@ -1,3 +1,8 @@
+pushd $CONDA_EMSDK_DIR
+./upstream/emscripten/embuilder build freetype --pic
+./upstream/emscripten/embuilder build libjpeg --pic
+popd
+
 export LDFLAGS="${LDFLAGS} -s USE_LIBJPEG"
 export CFLAGS="${CFLAGS} -s USE_ZLIB=1 -s USE_LIBJPEG=1 -s USE_FREETYPE=1 -s SIDE_MODULE=1"
 ${PYTHON} -m pip  install .
