008c06ff026b95791586106b3f476db197360488
DerThorsten <derthorstenbeier@gmail.com>
2022-05-31 17:41:21 +0200

    updated stack

--- recipes/recipes_emscripten/wasm_requests/test_wasm_requests.py
+++ recipes/recipes_emscripten/wasm_requests/test_wasm_requests.py
@@ -12,6 +12,7 @@ def test_wasm_requests():
     # get with query params
     r = wasm_requests.get("https://httpbin.org/get", params=dict(foo="bar", fobar=1))
     result = r.json()
+    headers = r.headers
 
     assert result["args"]["foo"] == "bar"
     assert result["args"]["fobar"] == "1"
