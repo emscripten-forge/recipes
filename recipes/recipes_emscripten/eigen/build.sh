689eac7d3f34647a9376df6c52024c28e39bbe3e
Silvio Traversaro <silvio@traversaro.it>
2022-07-14 23:40:47 +0200

    Create build.sh

---
+++ recipes/recipes_emscripten/eigen/build.sh
@@ -0,0 +1,11 @@
+mkdir build
+cd build
+
+# Configure step
+cmake ${CMAKE_ARGS} ..                    \
+    -GNinja                               \
+    -DCMAKE_PREFIX_PATH:PATH=${PREFIX}    \ 
+    -DCMAKE_INSTALL_PREFIX:PATH=${PREFIX} \
+    -DCMAKE_BUILD_TYPE=Release
+    
+ninja install
