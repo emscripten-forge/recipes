a7c7811cdc9a76beb19f9cea6190431ccb67792b
Silvio Traversaro <silvio@traversaro.it>
2022-07-19 19:28:43 +0200

    Create build.sh

---
+++ recipes/recipes_emscripten/idyntree/build.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+mkdir build
+cd build
+
+cmake ${CMAKE_ARGS} -GNinja .. \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DBUILD_TESTING:BOOL=${BUILD_TESTING} \
+      -DIDYNTREE_USES_IPOPT:BOOL=OFF \
+      -DIDYNTREE_USES_OSQPEIGEN:BOOL=OFF \
+      -DIDYNTREE_USES_IRRLICHT:BOOL=OFF \
+      -DIDYNTREE_USES_ASSIMP:BOOL=OFF \
+      -DIDYNTREE_USES_MATLAB:BOOL=OFF \
+      -DIDYNTREE_USES_PYTHON:BOOL=ON \
+      -DIDYNTREE_USES_OCTAVE:BOOL=OFF \
+      -DIDYNTREE_USES_LUA:BOOL=OFF \
+      -DIDYNTREE_COMPILES_YARP_TOOLS:BOOL=OFF \
+      -DPython3_EXECUTABLE:PATH=$PYTHON \
+      -DIDYNTREE_DETECT_ACTIVE_PYTHON_SITEPACKAGES:BOOL=ON \
+      -DIDYNTREE_PYTHON_PIP_METADATA_INSTALLER=conda
+
+cmake --build . --config Release 
+cmake --build . --config Release --target install
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR}" != "" ]]; then
+  ctest --output-on-failure -C Release
+fi
