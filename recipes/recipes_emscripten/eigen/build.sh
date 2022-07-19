4b2f079b82ceba52af2ee54f28700d5cf63e7716
Wolf Vollprecht <w.vollprecht@gmail.com>
2022-07-19 17:04:01 +0200

    remove trailing whitespace

--- recipes/recipes_emscripten/eigen/build.sh
+++ recipes/recipes_emscripten/eigen/build.sh
@@ -5,7 +5,7 @@ mkdir -p $PREFIX
 # Configure step
 cmake ${CMAKE_ARGS} ..                    \
     -GNinja                               \
-    -DCMAKE_PREFIX_PATH:PATH=${PREFIX}    \ 
+    -DCMAKE_PREFIX_PATH:PATH=${PREFIX}    \
     -DCMAKE_INSTALL_PREFIX:PATH=${PREFIX} \
     -DCMAKE_BUILD_TYPE=Release
     
