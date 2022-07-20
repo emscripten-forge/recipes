85e2c209f7cb0b2578b803133b349d33365e382d
Silvio Traversaro <silvio@traversaro.it>
2022-07-20 09:03:09 +0200

    Update build.sh

--- recipes/recipes_emscripten/idyntree/build.sh
+++ recipes/recipes_emscripten/idyntree/build.sh
@@ -6,7 +6,7 @@ cd build
 # See https://github.com/microsoft/vcpkg/pull/11753
 export CMAKE_PREFIX_PATH=$PREFIX:/
 export CMAKE_SYSTEM_PREFIX_PATH=$PREFIX:/
-export CMAKE_FIND_ROOT_PATH$PREFIX
+export CMAKE_FIND_ROOT_PATH=$PREFIX
 
 echo "=====> CMAKE_ARGS: ${CMAKE_ARGS}"
 
