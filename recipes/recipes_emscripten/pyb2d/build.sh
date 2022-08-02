92526e0bf9f7e08e8e464109b655536a05dc3096
Thorsten Beier <derthorstenbeier@gmail.com>
2022-08-02 12:41:13 +0200

    Pyb2d (#135)
     adding pyb2d

---
+++ recipes/recipes_emscripten/pyb2d/build.sh
@@ -0,0 +1,21 @@
+mkdir build
+cd build
+
+export CMAKE_PREFIX_PATH=$PREFIX
+export CMAKE_SYSTEM_PREFIX_PATH=$PREFIX
+
+
+# Configure step
+cmake ${CMAKE_ARGS} ..                                \
+    -GNinja                                           \
+    -DCMAKE_BUILD_TYPE=Release                        \
+    -DCMAKE_PREFIX_PATH=$PREFIX                       \
+    -DCMAKE_INSTALL_PREFIX=$PREFIX                    \
+    -DPYTHON_NUMPY_INCLUDE_DIR="$PREFIX/lib/python3.10/site-packages/numpy/core/include" \
+    -DPYTHON_MODULE_INSTALL_DIR="$PREFIX/lib/python3.10/site-packages"
+# Build step
+ninja
+
+
+
+ninja install
