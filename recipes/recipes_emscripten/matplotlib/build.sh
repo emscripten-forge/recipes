0b5e64e0c2aa95960854005980c8260f76fa070c
martinRenou <martin.renou@gmail.com>
2022-08-04 14:13:30 +0200

    Matplotlib: Reduce package space

--- recipes/recipes_emscripten/matplotlib/build.sh
+++ recipes/recipes_emscripten/matplotlib/build.sh
@@ -9,4 +9,13 @@ export CFLAGS="$CFLAGS -s USE_FREETYPE=1 -s USE_LIBPNG=1 -s USE_ZLIB=1"
 
 $PYTHON -m pip install . --no-deps
 
-cp $RECIPE_DIR/src/fontlist.json $PREFIX/lib/python$PY_VER/site-packages/matplotlib/
+export MATPLOTLIB_LOCATION=$PREFIX/lib/python$PY_VER/site-packages/matplotlib
+
+# Pre-built font cache
+cp $RECIPE_DIR/src/fontlist.json $MATPLOTLIB_LOCATION/
+cp $RECIPE_DIR/src/Humor-Sans-1.0.ttf $MATPLOTLIB_LOCATION/mpl-data/fonts/ttf/Humor-Sans.ttf
+
+# Save space by removing unneeded code
+rm -rf $MATPLOTLIB_LOCATION/backends/qt_editor
+rm -rf $MATPLOTLIB_LOCATION/backends/web_backend
+rm -rf $MATPLOTLIB_LOCATION/sphinxext
