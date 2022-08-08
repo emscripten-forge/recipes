c64a0047193ab8dc908e53d0c300526286a67f4a
martinRenou <martin.renou@gmail.com>
2022-08-08 14:48:05 +0200

    Fix hardcoded Python version in pyerfa build script

--- recipes/recipes_emscripten/pyerfa/build.sh
+++ recipes/recipes_emscripten/pyerfa/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-export CFLAGS="${CFLAGS}  -I$BUILD_PREFIX/lib/python3.10/site-packages/numpy/core/include/"
+export CFLAGS="${CFLAGS}  -I$BUILD_PREFIX/lib/python$PY_VER/site-packages/numpy/core/include/"
 ${PYTHON} -m pip  install .
