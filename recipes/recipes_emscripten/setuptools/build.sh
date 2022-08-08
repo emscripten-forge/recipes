6ad6b92920e75de6b7f8bb59b437d0a73501410e
martinRenou <martin.renou@gmail.com>
2022-08-08 15:29:40 +0200

    Debug

--- recipes/recipes_emscripten/setuptools/build.sh
+++ recipes/recipes_emscripten/setuptools/build.sh
@@ -2,3 +2,5 @@
 ${PYTHON} -m pip install . -vv--no-deps
 
 find "$BUILD_PREFIX/lib/python$PY_VER/site-packages/setuptools" -name '*.exe' -delete
+
+ls "$BUILD_PREFIX/lib/python$PY_VER/site-packages/setuptools"
