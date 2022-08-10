19c844581340461d0430a4ed97fc620164f75fcf
Thorsten Beier <derthorstenbeier@gmail.com>
2022-08-10 11:22:03 +0200

    update pyjs recipe to v0.8.0 (#173)
    * update pyjs recipe to v0.8.0
    
    * disable node tests

--- recipes/recipes_emscripten/pyjs/build.sh
+++ recipes/recipes_emscripten/pyjs/build.sh
@@ -1,16 +1,17 @@
 mkdir build
 cd build
 
-
 export CMAKE_PREFIX_PATH=$PREFIX 
 export CMAKE_SYSTEM_PREFIX_PATH=$PREFIX 
-
 
 # Configure step
 cmake ${CMAKE_ARGS} ..              \
     -GNinja                         \
     -DCMAKE_PREFIX_PATH=$PREFIX     \
     -DCMAKE_INSTALL_PREFIX=$PREFIX  \
+    -DBUILD_RUNTIME_BROWSER=ON \
+    -DBUILD_RUNTIME_NODE=ON \
+    -DWITH_NODE_TESTS=OFF
 
 
 # Build step
