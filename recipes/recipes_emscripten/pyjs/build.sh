a55817f54f23a30259b3ff6b3670e979c809786b
DerThorsten <derthorstenbeier@gmail.com>
2022-08-11 08:51:30 +0200

    use explicit release bld for pyjs

--- recipes/recipes_emscripten/pyjs/build.sh
+++ recipes/recipes_emscripten/pyjs/build.sh
@@ -7,6 +7,7 @@ export CMAKE_SYSTEM_PREFIX_PATH=$PREFIX
 # Configure step
 cmake ${CMAKE_ARGS} ..              \
     -GNinja                         \
+    -DCMAKE_BUILD_TYPE=Release      \
     -DCMAKE_PREFIX_PATH=$PREFIX     \
     -DCMAKE_INSTALL_PREFIX=$PREFIX  \
     -DBUILD_RUNTIME_BROWSER=ON \
