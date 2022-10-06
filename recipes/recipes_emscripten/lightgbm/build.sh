07305aa589078d7a610bcfdc74a85c9b171de46c
Isabel Paredes <iparedes314@gmail.com>
2022-10-06 15:49:55 +0200

    Build shared libraries

--- recipes/recipes_emscripten/lightgbm/build.sh
+++ recipes/recipes_emscripten/lightgbm/build.sh
@@ -1,4 +1,11 @@
-cmake ./compile -DUSE_OPENMP=OFF
+#!/bin/bash
+
+cmake ./compile ${CMAKE_ARGS} -DCMAKE_INSTALL_PREFIX=$PREFIX \
+    -DCMAKE_INSTALL_LIBDIR=$PREFIX/lib \
+    -DCMAKE_PROJECT_INCLUDE=$RECIPE_DIR/overwriteProp.cmake \
+    -DBUILD_SHARED_LIBS=ON \
+    -DBUILD_STATIC_LIBS=ON \
+    -DUSE_OPENMP=OFF
 
 make -j${CPU_COUNT}
 
