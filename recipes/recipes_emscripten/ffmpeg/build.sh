45e06d31b06dd825b790380b829f546aaa529d07
Isabel Paredes <iparedes314@gmail.com>
2022-10-13 11:54:30 +0200

    Clean up build

--- recipes/recipes_emscripten/ffmpeg/build.sh
+++ recipes/recipes_emscripten/ffmpeg/build.sh
@@ -1,25 +1,5 @@
 #!/bin/bash
 
-# emconfigure ./configure \
-#     --extra-cflags="-fPIC" \
-#     --disable-x86asm \
-#     --disable-asm \
-#     --disable-inline-asm \
-#     --disable-doc \
-#     --disable-stripping \
-#     --disable-programs \
-#     --disable-pthreads \
-#     --nm="$CONDA_EMSDK_DIR/upstream/bin/llvm-nm -g" \
-#     --ar=emar --cc=emcc --cxx=em++ --objcc=emcc --dep-cc=emcc --ranlib=emranlib \
-#     --prefix=${PREFIX}
-# # emmake make -j${CPU_COUNT}
-# emmake make
-# emmake make install
-
-# Adapted from https://itnext.io/build-ffmpeg-webassembly-version-ffmpeg-js-part-2-compile-with-emscripten-4c581e8c9a16
-CFLAGS="-s USE_PTHREADS"
-LDFLAGS="$CFLAGS -s INITIAL_MEMORY=33554432"
-
 ARGS=(
     --target-os=none
     --arch=x86_32
@@ -38,9 +18,6 @@ ARGS=(
     --cxx=em++
     --objcc=emcc
     --dep-cc=emcc
-    --extra-cflags="$CFLAGS"
-    --extra-cxxflags="$CFLAGS"
-    --extra-ldflags="$LDFLAGS"
 )
 
 emconfigure ./configure "${ARGS[@]}"
