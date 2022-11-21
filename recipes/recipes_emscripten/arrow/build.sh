a7b6eaeb8e7536496e9749d170886274f84560ce
DerThorsten <derthorstenbeier@gmail.com>
2022-11-21 10:21:10 +0100

    typo

--- recipes/recipes_emscripten/arrow/build.sh
+++ recipes/recipes_emscripten/arrow/build.sh
@@ -16,7 +16,7 @@ cmake ${CMAKE_ARGS} \
     -DARROW_ENABLE_TIMING_TESTS=OFF \
     -DARROW_BUILD_SHARED=OFF \
     -DARROW_COMPUTE=ON \
-    -Dutf8proc_LIB=${PREFIX}lib/pkgconfig \
+    -Dutf8proc_LIB=${PREFIX}/lib/libutf8proc.a \
     -Dutf8proc_INCLUDE_DIR=${PREFIX}/include \
     -S ../cpp -B ./
 
