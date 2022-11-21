e7fa4e2ae3c9e46b16ffe44e709d230e0fd4d248
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-21 00:05:11 +0000

    Adding tarfile member sanitization to extractall()

--- recipes/recipes_emscripten/matplotlib/setupext.py
+++ recipes/recipes_emscripten/matplotlib/setupext.py
@@ -135,7 +135,26 @@ def get_and_extract_tarball(urls, sha, dirname):
                 raise IOError(
                     f"The downloaded tgz file was expected to have {dirname} "
                     f"as sole top-level directory, but that is not the case")
-            tgz.extractall("build")
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tgz, "build")
     return toplevel
 
 
