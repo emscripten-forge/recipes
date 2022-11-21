ca61f525cc49c8b92d9eb4d3eec8fb2e214350db
DerThorsten <derthorstenbeier@gmail.com>
2022-11-21 12:36:02 +0100

    wip

--- recipes/recipes_emscripten/arrow-python/build.sh
+++ recipes/recipes_emscripten/arrow-python/build.sh
@@ -2,6 +2,7 @@
 
 if [[ $target_platform == "emscripten-32" ]]; then
     cp ${RECIPE_DIR}/setup.py ${SRC_DIR}/python/
+    cp ${RECIPE_DIR}/CMakeLists.txt ${SRC_DIR}/python/
     cp ${RECIPE_DIR}/FindPythonLibsNew.cmake ${SRC_DIR}/cpp/cmake_modules/
     cp ${RECIPE_DIR}/FindPython3Alt.cmake ${SRC_DIR}/cpp/cmake_modules/
     cp ${RECIPE_DIR}/SetupCxxFlags.cmake ${SRC_DIR}/cpp/cmake_modules/
