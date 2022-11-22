596340b88c56f93931d0c9bae0c743d25f9e3f9b
Thorsten Beier <derthorstenbeier@gmail.com>
2022-11-22 09:37:53 +0100

    Re2 (#223)
    * added txt
    
    * added re2 recipe

---
+++ recipes/recipes_emscripten/re2/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -ex
+
+mkdir build-cmake
+pushd build-cmake
+
+cmake ${CMAKE_ARGS} -GNinja \
+  -DCMAKE_PREFIX_PATH=$PREFIX \
+  -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+  -DCMAKE_INSTALL_LIBDIR=lib \
+  -DRE2_BUILD_TESTING=OFF \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DBUILD_SHARED_LIBS=OFF \
+  ..
+ninja install
+popd
+
+# Also do this installation to get .pc files. This duplicates the compilation but gets us all necessary components without patching.
+make -j "${CPU_COUNT}" prefix=${PREFIX} shared-install
