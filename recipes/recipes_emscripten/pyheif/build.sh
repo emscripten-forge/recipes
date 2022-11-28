c74cb3674c20ff43d440c61188317e4ce2e267ad
Isabel Paredes <iparedes314@gmail.com>
2022-11-28 21:53:52 +0100

    Add patch

--- recipes/recipes_emscripten/pyheif/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sed -i "s@include_dirs = \[@include_dirs = ['\\${CONDA_EMSDK_DIR}/include', @" libheif/libheif_build.py
-sed -i "s@library_dirs = \[@library_dirs = ['\\${CONDA_EMSDK_DIR}/lib', @" libheif/libheif_build.py
-
-${PYTHON} -m pip install . -vv
