3d2ee18df61cdf851658621d3d2925f951e4d9c6
DerThorsten <derthorstenbeier@gmail.com>
2022-12-13 11:59:32 +0100

    fixed pillow build

--- recipes/recipes_emscripten/pillow/build.sh
+++ recipes/recipes_emscripten/pillow/build.sh
@@ -1,7 +1,9 @@
-pushd $CONDA_EMSDK_DIR
-./upstream/emscripten/embuilder build freetype --pic
-./upstream/emscripten/embuilder build libjpeg --pic
-popd
+echo "ACTUAL_CONDA_EMSDK_DIR $ACTUAL_CONDA_EMSDK_DIR: " $ACTUAL_CONDA_EMSDK_DIR
+
+# pushd $CONDA_EMSDK_DIR
+$ACTUAL_CONDA_EMSDK_DIR/upstream/emscripten/embuilder build freetype --pic
+$ACTUAL_CONDA_EMSDK_DIR/upstream/emscripten/embuilder build libjpeg --pic
+# popd
 
 export LDFLAGS="${LDFLAGS} -s USE_LIBJPEG"
 export CFLAGS="${CFLAGS} -s USE_ZLIB=1 -s USE_LIBJPEG=1 -s USE_FREETYPE=1 -s SIDE_MODULE=1"
