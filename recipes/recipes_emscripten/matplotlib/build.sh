5161346364e9e5acd4264b9bfae6936890f6987a
DerThorsten <derthorstenbeier@gmail.com>
2022-12-13 16:44:37 +0100

    wip

--- recipes/recipes_emscripten/matplotlib/build.sh
+++ recipes/recipes_emscripten/matplotlib/build.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-pushd $CONDA_EMSDK_DIR
-./upstream/emscripten/embuilder build freetype --pic
-popd
+$ACTUAL_CONDA_EMSDK_DIR/upstream/emscripten/embuilder build freetype --pic
 
 export LDFLAGS="$LDFLAGS -s USE_FREETYPE=1 -s USE_LIBPNG=1 -s USE_ZLIB=1"
 export CFLAGS="$CFLAGS -s USE_FREETYPE=1 -s USE_LIBPNG=1 -s USE_ZLIB=1"
