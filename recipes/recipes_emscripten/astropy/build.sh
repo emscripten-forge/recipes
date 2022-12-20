6a1612f954d0e48930626f5091cebe0eea1f8073
DerThorsten <derthorstenbeier@gmail.com>
2022-12-20 10:14:03 +0100

    disable error via -Wno-incompatible-function-pointer-types *fingers crossed*

--- recipes/recipes_emscripten/astropy/build.sh
+++ recipes/recipes_emscripten/astropy/build.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
+
+
+DISABLED=-Wno-incompatible-function-pointer-types
+
+export CFLAGS="$CFLAGS     $DISABLED"
+export CXXFLAGS="$CXXFLAGS $DISABLED"
+
 ${PYTHON}  -m pip install . --no-deps  -vvv
