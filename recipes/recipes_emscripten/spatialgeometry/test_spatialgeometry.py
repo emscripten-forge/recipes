a57892d218e43d1e613249f98e997bbcb0a69848
DerThorsten <derthorstenbeier@gmail.com>
2022-12-29 17:50:53 +0100

    hotfixed spatialgeometry

--- recipes/recipes_emscripten/spatialgeometry/test_spatialgeometry.py
+++ recipes/recipes_emscripten/spatialgeometry/test_spatialgeometry.py
@@ -1,17 +1,24 @@
+import pyjs
+
+
+@pytest.mark.skipif(
+    not pyjs.js.Function('return typeof importScripts === "function"')(),
+    reason="requires browser, not node",
+)
 def test_spatialgeometry():
     import spatialgeometry as gm
 
     # Test cylinder shape
-    shape1 = gm.Cylinder(5,5)
+    shape1 = gm.Cylinder(5, 5)
     shape1_check = {
-        'stype': 'cylinder', 
-        't': [0.0, 0.0, 0.0], 
-        'q': [0.0, 0.0, 0.0, 1.0], 
-        'v': [0.0, 0.0, 0.0, 0.0, 0.0, 0.0], 
-        'color': 5000268, 
-        'opacity': 1.0, 
-        'radius': 5.0, 
-        'length': 5.0
+        "stype": "cylinder",
+        "t": [0.0, 0.0, 0.0],
+        "q": [0.0, 0.0, 0.0, 1.0],
+        "v": [0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+        "color": 5000268,
+        "opacity": 1.0,
+        "radius": 5.0,
+        "length": 5.0,
     }
     assert shape1.to_dict() == shape1_check
 
