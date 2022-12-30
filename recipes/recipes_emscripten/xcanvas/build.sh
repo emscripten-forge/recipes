d4531608eaf01217d5b85e448ca247ae6944ead8
Johan Mabille <johan.mabille@gmail.com>
2022-12-30 10:36:56 +0100

    Fixed install prefix of xcanvas

--- recipes/recipes_emscripten/xcanvas/build.sh
+++ recipes/recipes_emscripten/xcanvas/build.sh
@@ -7,6 +7,7 @@ cmake ${CMAKE_ARGS} ..              \
     -GNinja                         \
     -DCMAKE_BUILD_TYPE=Release      \
     -DCMAKE_PREFIX_PATH=$PREFIX     \
+    -DCMAKE_INSTALL_PREFIX=$PREFIX  \
     -DXCANVAS_BUILD_SHARED_LIBS=OFF \
     -DXCANVAS_BUILD_STATIC_LIBS=ON
 
