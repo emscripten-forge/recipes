c82c3d762714fad4e767d89813df28123060f3dc
DerThorsten <derthorstenbeier@gmail.com>
2023-01-30 11:15:19 +0100

    added numpy include path

--- recipes/recipes_emscripten/arrow-python/build.sh
+++ recipes/recipes_emscripten/arrow-python/build.sh
@@ -8,5 +8,12 @@ if [[ $target_platform == "emscripten-32" ]]; then
     cp ${RECIPE_DIR}/SetupCxxFlags.cmake ${SRC_DIR}/cpp/cmake_modules/
 fi
 
+
+
+export INCLUDE_NUMPY_FLAGS="-I$BUILD_PREFIX/lib/python3.10/site-packages/numpy/core/include   -I$PREFIX/lib/python3.10/site-packages/numpy/core/include" 
+
+export CFLAGS="$CFLAGS $INCLUDE_NUMPY_FLAGS"
+export CXXFLAGS="$CXXFLAGS $INCLUDE_NUMPY_FLAGS"
+
 cd python
 ${PYTHON} -m pip install . -vvv
