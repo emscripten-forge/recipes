32077c67aae8f58313547cd933a6f8033aea4291
DerThorsten <derthorstenbeier@gmail.com>
2023-01-31 19:56:31 +0100

    debug nelson II

--- recipes/recipes_emscripten/nelson/build.sh
+++ recipes/recipes_emscripten/nelson/build.sh
@@ -10,7 +10,8 @@ cmake ${CMAKE_ARGS} ..                                \
     -DCMAKE_BUILD_TYPE=Release                        \
     -DCMAKE_PREFIX_PATH=$PREFIX                       \
     -DCMAKE_INSTALL_PREFIX=$PREFIX                    \
-    -DBUILD_SHARED_LIBS=OFF
+    -DBUILD_SHARED_LIBS=OFF \
+    -DWITHOUT_main_MODULE=ON \
 
 # Build step
 ninja
