333cca4585f89f3dd16337e8e8ce7c43602200df
DerThorsten <derthorstenbeier@gmail.com>
2023-03-09 10:40:25 +0100

    remove build path from python sysconfig data

---
+++ recipes/recipes_emscripten/python/patch_sysconfigdata.py
@@ -0,0 +1,42 @@
+def fix_build_shared(bldshared):
+    assert "emcc" in bldshared
+    tokens = bldshared.split(" ")
+    fixed_tokens = []
+    for token in tokens:
+        if token.endswith("emcc"):
+            fixed_tokens.append("emcc")
+        else:
+            fixed_tokens.append(token)
+
+    return " ".join(fixed_tokens)
+
+
+def fix_config_args(config_args):
+    print(config_args)
+
+
+if __name__ == "__main__":
+    import argparse
+    import json
+
+    parser = argparse.ArgumentParser(prog="PatchSysConfigData")
+
+    parser.add_argument("--fname-in", required=True)
+    parser.add_argument("--fname-out", required=True)
+    parser.add_argument("--emcc", required=True)
+    parser.add_argument("--emar", required=True)
+    parser.add_argument("--emcpp", required=True)
+
+    args = parser.parse_args()
+
+    fname_in = args.fname_in
+
+    with open(fname_in, "r") as f:
+        content = f.read()
+
+    content = content.replace(args.emcc, "emcc")
+    content = content.replace(args.emcpp, "em++")
+    content = content.replace(args.emar, "emar")
+
+    with open(args.fname_out, "w") as f:
+        f.write(content)
