b29f8da51cadb21dc33be064cfe79526736a9c7b
Angus Hollands <goosey15@gmail.com>
2023-03-10 12:04:29 +0000

    fix: typo

--- recipes/recipes_emscripten/awkward-cpp/build.sh
+++ recipes/recipes_emscripten/awkward-cpp/build.sh
@@ -2,6 +2,6 @@
 
 # Setup toolchain
 emscripten_root=$(em-config EMSCRIPTEN_ROOT)
-export CMAKE_ARGS="${CMAKE_ARGS -DEMSCRIPTEN=1 -DCMAKE_TOOLCHAIN_FILE=${emscripten_root}/cmake/Modules/Platform/Emscripten.cmake"
+export CMAKE_ARGS="${CMAKE_ARGS} -DEMSCRIPTEN=1 -DCMAKE_TOOLCHAIN_FILE=${emscripten_root}/cmake/Modules/Platform/Emscripten.cmake"
 
 python -m pip install . -vv
