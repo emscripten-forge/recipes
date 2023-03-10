11cfc07da02ed557fbfc891422147552cb528187
Angus Hollands <goosey15@gmail.com>
2023-03-10 12:03:57 +0000

    fix: add build script

---
+++ recipes/recipes_emscripten/awkward-cpp/build.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Setup toolchain
+emscripten_root=$(em-config EMSCRIPTEN_ROOT)
+export CMAKE_ARGS="${CMAKE_ARGS -DEMSCRIPTEN=1 -DCMAKE_TOOLCHAIN_FILE=${emscripten_root}/cmake/Modules/Platform/Emscripten.cmake"
+
+python -m pip install . -vv
