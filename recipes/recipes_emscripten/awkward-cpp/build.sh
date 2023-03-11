890be2abbb8487d9c8b8be871445ba8bb0fadefd
Angus Hollands <goosey15@gmail.com>
2023-03-11 20:35:19 +0000

    fix: building of package

--- recipes/recipes_emscripten/awkward-cpp/build.sh
+++ recipes/recipes_emscripten/awkward-cpp/build.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
-# Setup toolchain
-emscripten_root=$(em-config EMSCRIPTEN_ROOT)
-toolchain_path="${emscripten_root}/cmake/Modules/Platform/Emscripten.cmake"
+# Setup toolchain from pyodide-build
+toolchain_path="$(python3 ${RECIPE_DIR}/find-pyodide-build-platform.py)"
+
+# Setup build arguments
 export CMAKE_ARGS="${CMAKE_ARGS} -DEMSCRIPTEN=1 -DCMAKE_TOOLCHAIN_FILE=${toolchain_path}"
 
 python -m pip install . -vv
