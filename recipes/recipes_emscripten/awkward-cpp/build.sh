d8e206d2be63e062fe41a0e0c8b0bb145a978cc3
Angus Hollands <goosey15@gmail.com>
2023-03-11 21:55:01 +0000

    wip: try side-module

--- recipes/recipes_emscripten/awkward-cpp/build.sh
+++ recipes/recipes_emscripten/awkward-cpp/build.sh
@@ -4,6 +4,6 @@
 toolchain_path="$(python3 ${RECIPE_DIR}/find-pyodide-build-platform.py)"
 
 # Setup build arguments
-export CMAKE_ARGS="${CMAKE_ARGS} -DEMSCRIPTEN=1 -DCMAKE_TOOLCHAIN_FILE=${toolchain_path}"
+export CMAKE_ARGS="${CMAKE_ARGS} -DEMSCRIPTEN=1 -DCMAKE_TOOLCHAIN_FILE=${toolchain_path} -DCMAKE_PROJECT_INCLUDE=${RECIPE_DIR}/overwriteProp.cmake"
 
 python -m pip install . -vv
