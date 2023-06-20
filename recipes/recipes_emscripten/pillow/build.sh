54736b60667b69f728f2ccb65997647b8b363eb4
martinRenou <martin.renou@gmail.com>
2023-06-20 16:46:04 +0200

    Print path

--- recipes/recipes_emscripten/pillow/build.sh
+++ recipes/recipes_emscripten/pillow/build.sh
@@ -1,8 +1,10 @@
 echo "EMSCRIPTEN_FORGE_EMSDK_DIR $EMSCRIPTEN_FORGE_EMSDK_DIR: " $EMSCRIPTEN_FORGE_EMSDK_DIR
 
+${PYTHON} -c "import sys; print(sys.path)"
+
 # pushd $CONDA_EMSDK_DIR
-${PYTHON} $EMSCRIPTEN_FORGE_EMSDK_DIR/upstream/emscripten/embuilder.py build freetype --pic
-${PYTHON} $EMSCRIPTEN_FORGE_EMSDK_DIR/upstream/emscripten/embuilder.py build libjpeg --pic
+$EMSCRIPTEN_FORGE_EMSDK_DIR/upstream/emscripten/embuilder build freetype --pic
+$EMSCRIPTEN_FORGE_EMSDK_DIR/upstream/emscripten/embuilder build libjpeg --pic
 # popd
 
 export LDFLAGS="${LDFLAGS} -s USE_LIBJPEG"
