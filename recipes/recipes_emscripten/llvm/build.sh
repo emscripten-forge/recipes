e7b69d31cf01f98f822f81cde13774304f696975
Thorsten Beier <derthorstenbeier@gmail.com>
2023-07-24 09:00:24 +0200

    added emcmake

--- recipes/recipes_emscripten/llvm/build.sh
+++ recipes/recipes_emscripten/llvm/build.sh
@@ -5,7 +5,7 @@ export CMAKE_PREFIX_PATH=$PREFIX
 export CMAKE_SYSTEM_PREFIX_PATH=$PREFIX
 
 # Configure step
-cmake ${CMAKE_ARGS} -S ../llvm -B .                 \
+emcmake cmake ${CMAKE_ARGS} -S ../llvm -B .                 \
     -GNinja                                         \
     -DCMAKE_BUILD_TYPE=MinSizeRel                   \
     -DCMAKE_PREFIX_PATH=$PREFIX                     \
