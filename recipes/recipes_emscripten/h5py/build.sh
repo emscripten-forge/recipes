36e474a3cacb8972f2553453c2895512d788b006
Jonathan Shimwell <drshimwell@gmail.com>
2023-11-20 18:13:57 +0000

    avoid ros3 driver support with env

--- recipes/recipes_emscripten/h5py/build.sh
+++ recipes/recipes_emscripten/h5py/build.sh
@@ -5,4 +5,7 @@ export HDF5_DIR=${PREFIX}
 # tell setup.py to not 'pip install' exact package requirements
 export H5PY_SETUP_REQUIRES="0"
 
+# tell setup_configure.py not to compile with ROS3 driver support
+export H5PY_ROS3="0"
+
 "${PYTHON}" -m pip install . --no-deps --ignore-installed --no-cache-dir -vv
