634e008bdc6e960e1732e5c66a5edde520f5753d
Anutosh Bhat <87052487+anutosh491@users.noreply.github.com>
2023-11-28 17:15:32 +0530

    Update recipes/recipes_emscripten/argparse/build.sh
    Co-authored-by: Thorsten Beier <derthorstenbeier@gmail.com>

--- recipes/recipes_emscripten/argparse/build.sh
+++ recipes/recipes_emscripten/argparse/build.sh
@@ -4,6 +4,7 @@ mkdir build && cd build
 
 cmake -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DCMAKE_INSTALL_LIBDIR=lib \
+      -DARGPARSE_BUILD_TESTS=OFF \
       -DCMAKE_BUILD_TYPE=Release \
       $SRC_DIR
 
