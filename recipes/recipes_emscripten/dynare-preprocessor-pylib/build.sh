62352e6f8e372738f47e441516887cdbf9378ef1
Pablo Winant <pablo.winant@gmail.com>
2023-11-30 18:34:45 +0100

    Update build.sh

--- recipes/recipes_emscripten/dynare-preprocessor-pylib/build.sh
+++ recipes/recipes_emscripten/dynare-preprocessor-pylib/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export BOOST_ROOT=$CONDA_PREFIX
-meson setup --buildtype=release build_preproc -Dcpp_link_args='-pthread' --cross-file="scripts/wasm32.ini"
+meson setup --buildtype=release build_preproc -Dcpp_args='-pthread'  -Dcpp_link_args='-pthread' --cross-file="scripts/wasm32.ini"
 meson compile -C build_preproc
 meson install -C build_preproc --destdir="../dist"
 cp -r dist/usr/local/lib/ $PREFIX
