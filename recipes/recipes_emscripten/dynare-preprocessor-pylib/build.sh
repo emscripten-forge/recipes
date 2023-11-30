d9da120ff3ac2f90c3f94f6e2722cf63a12b6282
Pablo Winant <pablo.winant@gmail.com>
2023-11-30 16:37:29 +0100

    First implementation of dynare python library.

---
+++ recipes/recipes_emscripten/dynare-preprocessor-pylib/build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export BOOST_ROOT=$PREFIX
+meson setup --buildtype=release build_preproc -Dcpp_link_args='-pthread' --cross-file="scripts/wasm32.ini"
+meson compile -C build_preproc
+meson install -C build_preproc --destdir="../dist"
+cp -r dist/usr/local/lib/ $PREFIX
+# meson install -C build_preproc --libdir=$PREFIX
---
+++ recipes/recipes_emscripten/dynare-preprocessor-pylib/run_test.sh
@@ -0,0 +1,2 @@
+# works only with file access
+# python -c "import dynare_preprocessor as dyn; txt = open('example1.mod').read() ; res = dyn.preprocess(txt) ; print(res)"
