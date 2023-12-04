16c1f1b835c7bd64129c23cf18262dd93480dffc
Pablo Winant <pablo.winant@gmail.com>
2023-12-04 16:01:18 +0100

    No need to manually pass python include dir

--- recipes/recipes_emscripten/dynare-preprocessor-pylib/build.sh
+++ recipes/recipes_emscripten/dynare-preprocessor-pylib/build.sh
@@ -6,8 +6,8 @@ cp $RECIPE_DIR/meson.build src/meson.build
 
 meson setup --prefix=$PREFIX --bindir=$PREFIX/bin --libdir=$PREFIX/lib --includedir=$PREFIX/include \
     --buildtype=release build_preproc \
-    -Dcpp_args="-pthread -w  -Wno-enum-constexpr-conversion -I${PREFIX}/include/python3.11 -I${PREFIX}/include/pybind11"  \
-    -Dcpp_link_args="-pthread -w  -Wno-enum-constexpr-conversion -I${PREFIX}/include/python3.11 -I${PREFIX}/include/pybind11" \
+    -Dcpp_args="-pthread -w  -Wno-enum-constexpr-conversion -I${PREFIX}/include/pybind11"  \
+    -Dcpp_link_args="-pthread -w  -Wno-enum-constexpr-conversion -I${PREFIX}/include/pybind11" \
     --cross-file=$RECIPE_DIR/wasm_32.ini
 
 meson compile -C build_preproc
