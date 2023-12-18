0a2b6c838e146fad4fecb0c5ecfb13ad0d85dc73
anutosh491 <andersonbhat491@gmail.com>
2023-12-18 20:33:15 +0530

    Added llvm16

---
+++ recipes/recipes_emscripten/llvm16/build.sh
@@ -0,0 +1,31 @@
+mkdir build
+cd build
+
+export CMAKE_PREFIX_PATH=$PREFIX
+export CMAKE_SYSTEM_PREFIX_PATH=$PREFIX
+
+# clear LDFLAGS flags because they contain sWASM_BIGINT
+export LDFLAGS=""
+
+
+# Configure step
+emcmake cmake ${CMAKE_ARGS} -S ../llvm16 -B .       \
+    -DCMAKE_BUILD_TYPE=MinSizeRel                   \
+    -DCMAKE_PREFIX_PATH=$PREFIX                     \
+    -DCMAKE_INSTALL_PREFIX=$PREFIX                  \
+    -DLLVM_HOST_TRIPLE=wasm32-unknown-emscripten    \
+    -DLLVM_TARGETS_TO_BUILD="WebAssembly"           \
+    -DLLVM_INCLUDE_BENCHMARKS=OFF                   \
+    -DLLVM_INCLUDE_EXAMPLES=OFF                     \
+    -DLLVM_INCLUDE_TESTS=OFF                        \
+    -DLLVM_ENABLE_LIBEDIT=OFF                       \
+    -DLLVM_ENABLE_PROJECTS="clang;lld"              \
+    -DCMAKE_CXX_FLAGS="-Dwait4=__syscall_wait4"     \
+    -DCMAKE_VERBOSE_MAKEFILE=ON                     \
+    -DCMAKE_CXX_FLAGS="-isystem $EMSCRIPTEN_FORGE_EMSDK_DIR/upstream/emscripten/cache/sysroot/include/c++/v1"
+
+# Build step
+EMCC_CFLAGS='-sERROR_ON_UNDEFINED_SYMBOLS=0' emmake make -j${CPU_COUNT}
+
+# Install step
+emmake make install
