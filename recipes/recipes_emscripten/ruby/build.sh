350f641784678b1eb16ae34124ce8b4d769d2db4
Isabel Paredes <isabel.paredes@quantstack.net>
2024-02-20 17:44:52 +0100

    Update recipe

--- recipes/recipes_emscripten/ruby/build.sh
+++ recipes/recipes_emscripten/ruby/build.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-RUBY_PACKED=$RECIPE_DIR/ruby-3.2-wasm32-unknown-emscripten-full.tar.gz
-
-tar -xzf $RUBY_PACKED
-RUBY_SOURCE_DIR=ruby-3.2-wasm32-unknown-emscripten-full/usr/local
-
 # Copy files to the right place
-cp -r ${RUBY_SOURCE_DIR}/bin $PREFIX/bin
-cp -r ${RUBY_SOURCE_DIR}/lib $PREFIX/lib
-cp -r ${RUBY_SOURCE_DIR}/include $PREFIX/include
-cp -r ${RUBY_SOURCE_DIR}/share $PREFIX/share
+cp -r usr/local/bin $PREFIX/bin
+cp -r usr/local/lib $PREFIX/lib
+cp -r usr/local/include $PREFIX/include
+cp -r usr/local/share $PREFIX/share
