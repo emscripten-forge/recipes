8afef12be195a551c3f8f694e3b1157122b55ffd
mcbarton <150042563+mcbarton@users.noreply.github.com>
2024-03-06 21:07:30 +0000

    Add CppInterOp recipe

---
+++ recipes/recipes_emscripten/CppInterOp/build.sh
@@ -0,0 +1,23 @@
+mkdir build
+cd build
+
+export CMAKE_PREFIX_PATH=$PREFIX
+export CMAKE_SYSTEM_PREFIX_PATH=$PREFIX
+
+# Configure step
+emcmake  cmake -DCMAKE_BUILD_TYPE=${{ env.BUILD_TYPE }} \
+    -DUSE_CLING=OFF                                     \
+    -DUSE_REPL=ON                                       \
+    -DCMAKE_PREFIX_PATH=$PREFIX                         \
+    -DLLVM_DIR=$PREFIX       			        \
+    -DClang_DIR=$PREFIX				        \
+    -DBUILD_SHARED_LIBS=ON                              \
+    -DCMAKE_INSTALL_PREFIX=$PREFIX        	        \
+    -DCMAKE_FIND_ROOT_PATH_MODE_PACKAGE=ON              \
+    ../
+
+# Build step
+EMCC_CFLAGS='-sERROR_ON_UNDEFINED_SYMBOLS=0' emmake make -j1
+
+# Install step
+emmake make install
