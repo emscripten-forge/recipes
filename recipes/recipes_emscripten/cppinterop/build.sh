123e6f5d28bc46ea6c8727b8dc0cbaa75234d4d1
mcbarton <150042563+mcbarton@users.noreply.github.com>
2024-03-07 14:34:28 +0000

    Set build type cmake

--- recipes/recipes_emscripten/cppinterop/build.sh
+++ recipes/recipes_emscripten/cppinterop/build.sh
@@ -5,14 +5,14 @@ export CMAKE_PREFIX_PATH=$PREFIX
 export CMAKE_SYSTEM_PREFIX_PATH=$PREFIX
 
 # Configure step
-emcmake  cmake -DCMAKE_BUILD_TYPE=${{ env.BUILD_TYPE }} \
+emcmake  cmake -DCMAKE_BUILD_TYPE=Release               \
     -DUSE_CLING=OFF                                     \
     -DUSE_REPL=ON                                       \
     -DCMAKE_PREFIX_PATH=$PREFIX                         \
-    -DLLVM_DIR=$PREFIX       			        \
-    -DClang_DIR=$PREFIX				        \
+    -DLLVM_DIR=$PREFIX       			                \
+    -DClang_DIR=$PREFIX				                    \
     -DBUILD_SHARED_LIBS=ON                              \
-    -DCMAKE_INSTALL_PREFIX=$PREFIX        	        \
+    -DCMAKE_INSTALL_PREFIX=$PREFIX        	            \
     -DCMAKE_FIND_ROOT_PATH_MODE_PACKAGE=ON              \
     ../
 
