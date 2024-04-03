e3c5a4a2bc21bc62b26b4b375b2eece58eb72564
DerThorsten <derthorstenbeier@gmail.com>
2024-04-03 13:04:35 +0200

    added pytester

---
+++ recipes/recipes/pytester/pytester/main_mount/main.py
@@ -0,0 +1,6 @@
+import pytest
+
+if __name__ == "__main__":
+    retcode = pytest.main()
+    if retcode != pytest.ExitCode.OK:
+        raise RuntimeError(f"pytest failed with return code: {retcode}")
