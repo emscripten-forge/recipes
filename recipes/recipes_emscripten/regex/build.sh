420854f4d4efce84c155113824ea08ccf920d4b1
DerThorsten <derthorstenbeier@gmail.com>
2024-04-03 11:08:05 +0200

    refine

--- recipes/recipes_emscripten/regex/build.sh
+++ recipes/recipes_emscripten/regex/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-export LDFLAGS="-s MODULARIZE=1  -s LINKABLE=1  -s EXPORT_ALL=1  -s WASM=1  -std=c++14  -s LZ4=1 -s SIDE_MODULE=1 -sWASM_BIGINT"
-${PYTHON} -m pip install .
+# export LDFLAGS="-s MODULARIZE=1  -s LINKABLE=1  -s EXPORT_ALL=1  -s WASM=1  -std=c++14  -s LZ4=1 -s SIDE_MODULE=1 -sWASM_BIGINT"
+# ${PYTHON} -m pip install .
