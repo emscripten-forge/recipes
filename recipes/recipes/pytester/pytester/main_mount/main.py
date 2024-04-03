cd3f6c09c76d5a136e8f5011d08b5b6739d188d2
DerThorsten <derthorstenbeier@gmail.com>
2024-04-03 17:07:19 +0200

    syscode

--- recipes/recipes/pytester/pytester/main_mount/main.py
+++ recipes/recipes/pytester/pytester/main_mount/main.py
@@ -1,6 +1,8 @@
 import pytest
+import sys
 
 if __name__ == "__main__":
     retcode = pytest.main()
     if retcode != pytest.ExitCode.OK:
         raise RuntimeError(f"pytest failed with return code: {retcode}")
+    sys.exit(0)
