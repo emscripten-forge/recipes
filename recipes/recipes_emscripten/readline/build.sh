690fef2f3364cfaae6a53f89aaabc8b13818c810
Isabel Paredes <isabel.paredes@quantstack.net>
2024-04-11 16:41:27 +0200

    Add readline

---
+++ recipes/recipes_emscripten/readline/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -ex
+
+# Get an updated config.sub and config.guess
+cp $BUILD_PREFIX/share/libtool/build-aux/config.* support/
+
+emconfigure ./configure --prefix=${PREFIX}  \
+            --disable-static
+
+# make SHLIB_LIBS="$(pkg-config --libs ncurses)" -j${CPU_COUNT} ${VERBOSE_AT}
+make -j${CPU_COUNT}
+
+make install
