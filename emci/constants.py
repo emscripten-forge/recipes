7ff2004a691495ba96182dc1962352e10ef33c34
DerThorsten <derthorstenbeier@gmail.com>
2024-04-17 09:54:21 +0200

    rattler changes

--- emci/constants.py
+++ emci/constants.py
@@ -1,7 +1,6 @@
 import os
 from collections import OrderedDict
 from pathlib import Path
-from empack.file_patterns import pkg_file_filter_from_yaml
 
 
 RECIPES_SUBDIR_MAPPING = OrderedDict(
@@ -14,7 +13,6 @@ REPO_ROOT = Path(THIS_DIR).parents[0].resolve()
 RECIPES_ROOT = REPO_ROOT / "recipes"
 RECIPES_EMSCRIPTEN_DIR = RECIPES_ROOT / "recipes_emscripten"
 CONFIG_PATH = os.path.join(REPO_ROOT, "empack_config.yaml")
-PKG_FILE_FILTER = pkg_file_filter_from_yaml(CONFIG_PATH)
 
 
 # rattler build related
@@ -22,7 +20,10 @@ RATTLER_CONDA_BUILD_CONFIG_PATH = os.path.join(REPO_ROOT, "rattler_conda_build_c
 
 # env var to force the use of boa instead of rattler for  testing the legacy build system
 FORCE_BOA = bool(int(os.environ.get("FORCE_BOA", False)))
+FORCE_RATTLER = bool(int(os.environ.get("FORCE_RATTLER", False)))
 
+if FORCE_BOA and FORCE_RATTLER:
+    raise ValueError("FORCE_BOA and FORCE_RATTLER cannot be both set")
 
 CONDA_PREFIX = os.environ.get("CONDA_PREFIX")
 if CONDA_PREFIX is None:
