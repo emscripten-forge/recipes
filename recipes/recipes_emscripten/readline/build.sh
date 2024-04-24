9f6b2f3800581eff9180b5e5e2944fd953483c76
Isabel Paredes <isabel.paredes@quantstack.net>
2024-04-24 18:17:32 +0200

    Update recipe

--- recipes/recipes_emscripten/readline/build.sh
+++ recipes/recipes_emscripten/readline/build.sh
@@ -5,8 +5,8 @@ set -ex
 # Get an updated config.sub and config.guess
 cp $BUILD_PREFIX/share/libtool/build-aux/config.* support/
 
-emconfigure ./configure --prefix=${PREFIX}  \
-            --disable-static
+emconfigure ./configure \
+    --prefix=${PREFIX}
 
 # make SHLIB_LIBS="$(pkg-config --libs ncurses)" -j${CPU_COUNT} ${VERBOSE_AT}
 make -j${CPU_COUNT}
