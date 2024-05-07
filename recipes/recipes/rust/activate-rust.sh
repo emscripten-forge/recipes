5899aee0ae4f5d5831f67962980e20e783fd67e6
DerThorsten <derthorstenbeier@gmail.com>
2024-05-07 11:02:39 +0200

    fix rust

--- recipes/recipes/rust/activate-rust.sh
+++ recipes/recipes/rust/activate-rust.sh
@@ -8,6 +8,13 @@ export RUSTUP_HOME=$HOME/.rustup_emscripten_forge
 export CARGO_HOME=$HOME/.cargo_emscripten_forge
 export PATH=$CARGO_HOME/bin:$PATH
 
+# if $CARO_HOME does not exist, install rustup
+if [ ! -d "$CARGO_HOME" ]; then
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y #--default-toolchain=1.78.0
+    rustup install nightly-2024-04-22
+    rustup default nightly-2024-04-22
+    rustup target add wasm32-unknown-emscripten
+fi
 
 
 export OPENSSL_INCLUDE_PATH=$PREFIX/include
