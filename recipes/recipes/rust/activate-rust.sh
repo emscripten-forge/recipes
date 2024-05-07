1dce29d1cfa198b9a6d2d5186fadc168ac3bc3b0
DerThorsten <derthorstenbeier@gmail.com>
2024-05-07 10:42:41 +0200

    install to home

--- recipes/recipes/rust/activate-rust.sh
+++ recipes/recipes/rust/activate-rust.sh
@@ -9,32 +9,6 @@ export CARGO_HOME=$HOME/.cargo_emscripten_forge
 export PATH=$CARGO_HOME/bin:$PATH
 
 
-#echo $PKG_VERSION > ${PREFIX}/.rust_version
-
-# is there a .bashenv file?
-if [ -f ${HOME}/.bashenv ]; then
-    # create a backup of the .bashenv file
-    echo "Creating a backup of the .bashenv file"
-    cp ${HOME}/.bashenv ${HOME}/.bashenv.bak
-fi
-# is there a .bash_profile:
-if [ -f ${HOME}/.bash_profile ]; then
-    # create a backup of the .bash_profile file
-    echo "Creating a backup of the .bash_profile file"
-    cp ${HOME}/.bash_profile ${HOME}/.bash_profile.bak
-fi
-
-
-
-# load the rust version from the .rust_version file
-
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y #--default-toolchain=1.78.0
-rustup install nightly-2024-04-22
-rustup default nightly-2024-04-22
-rustup target add wasm32-unknown-emscripten
-
-
-
 
 export OPENSSL_INCLUDE_PATH=$PREFIX/include
 export OPENSSL_LIBRARY_PATH=$PREFIX/lib
