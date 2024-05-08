6b0217dad419f4f472c555dc1072af75e220bf24
DerThorsten <derthorstenbeier@gmail.com>
2024-05-08 11:45:13 +0200

    only use rattler in bot

--- emci/bot/bump_recipes_versions.py
+++ emci/bot/bump_recipes_versions.py
@@ -122,7 +122,7 @@ def make_pr_title(name, old_version, new_version):
 
 def bump_recipe_version(recipe_dir):
 
-    recipe_locations = [("recipe_legacy.yaml", False), ("rattler.yaml", True)]
+    recipe_locations = [ ("recipe.yaml", True)]
 
     current_version = None
     new_version = None
