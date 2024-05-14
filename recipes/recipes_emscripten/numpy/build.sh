d0daf0918670672f70de2b94f3f3ddd0c449b391
Thorsten Beier <derthorstenbeier@gmail.com>
2024-05-14 19:00:18 +0200

    with wasm bigint flag

--- recipes/recipes_emscripten/numpy/build.sh
+++ recipes/recipes_emscripten/numpy/build.sh
@@ -7,6 +7,8 @@ rm -r -f branding
 export CFLAGS="$CFLAGS -Wno-return-type -Wno-implicit-function-declaration"
 export MESON_CROSS_FILE=$RECIPE_DIR/emscripten.meson.cross
 
+export LDFLAGS="$LDFLAGS -sWASM_BIGINT"
+
 ${PYTHON} -m pip install . -vvv --no-deps --no-build-isolation \
     -Csetup-args="-Dallow-noblas=true" \
     -Csetup-args="--cross-file=$MESON_CROSS_FILE"
