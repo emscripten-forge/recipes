784499f0cc57c3ebd5a0685b6ee12b6dc15ba5eb
Ian Thomas <ianthomas23@gmail.com>
2024-05-16 12:03:51 +0100

    Debug

--- recipes/recipes_emscripten/numpy/build.sh
+++ recipes/recipes_emscripten/numpy/build.sh
@@ -7,7 +7,7 @@ rm -r -f branding
 export CFLAGS="$CFLAGS -Wno-return-type -Wno-implicit-function-declaration"
 export MESON_CROSS_FILE=$RECIPE_DIR/emscripten.meson.cross
 
-export LDFLAGS="$LDFLAGS -sWASM_BIGINT"
+export LDFLAGS="$LDFLAGS -sWASM_BIGINT -Wno-map-unrecognized-libraries"
 
 ${PYTHON} -m pip install . -vvv --no-deps --no-build-isolation \
     -Csetup-args="-Dallow-noblas=true" \
