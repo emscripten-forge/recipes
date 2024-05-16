7d38c79f378fc7003f280740b7011990a37867a7
DerThorsten <derthorstenbeier@gmail.com>
2024-05-16 08:29:34 +0200

    defines

--- recipes/recipes_emscripten/lfortran/build.sh
+++ recipes/recipes_emscripten/lfortran/build.sh
@@ -1,6 +1,8 @@
 set -e
 set -x
 
+export CXXFLAGS="${CXXFLAGS} -D__STDC_FORMAT_MACROS -D_LIBCPP_DISABLE_AVAILABILITY"
+
 mkdir -p src/bin/asset_dir
 cp -v $BUILD_PREFIX/share/lfortran/lib/*.mod src/bin/asset_dir
 
