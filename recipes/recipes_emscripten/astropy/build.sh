7dfe9999a30ce3445e01bf855d0ab69c6b64445d
Thorsten Beier <derthorstenbeier@gmail.com>
2024-05-16 10:09:40 +0200

    Update build.sh

--- recipes/recipes_emscripten/astropy/build.sh
+++ recipes/recipes_emscripten/astropy/build.sh
@@ -3,13 +3,6 @@
 
 DISABLED=-Wno-incompatible-function-pointer-types
 
-
-# Create the test() function
-from .tests.runner import TestRunner
-
-test = TestRunner.make_test_runner_in(__path__[0])
-
-
 sed -i 's\from .tests.runner import TestRunner\# from .tests.runner import TestRunner\g' astropy/__init__.py
 sed -i 's\test = TestRunner.make_test_runner_in(__path__[0])\# test = TestRunner.make_test_runner_in(__path__[0])\g' astropy/__init__.py
 
