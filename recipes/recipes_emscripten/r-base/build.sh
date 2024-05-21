ddcb38e217f7f6d6010c4630278056bcf549a328
Isabel Paredes <isabel.paredes@quantstack.net>
2024-05-21 16:43:15 +0200

    Update recipe

--- recipes/recipes_emscripten/r-base/build.sh
+++ recipes/recipes_emscripten/r-base/build.sh
@@ -9,6 +9,7 @@ export FC=lfortran
 emconfigure ./configure \
     --prefix=$PREFIX    \
     --with-lapack=no    \
-    --with-blas=no
+    --with-blas=no      \
+    --with-readline=no
 
 emmake make -j${CPU_COUNT}
