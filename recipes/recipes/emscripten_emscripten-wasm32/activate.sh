dd39ea23de894225757279654c88912c40f0be60
DerThorsten <derthorstenbeier@gmail.com>
2024-05-23 09:39:52 +0200

    repack emscripten

--- recipes/recipes/emscripten_emscripten-wasm32/activate.sh
+++ recipes/recipes/emscripten_emscripten-wasm32/activate.sh
@@ -6,25 +6,10 @@ if [ -z ${CONDA_FORGE_EMSCRIPTEN_ACTIVATED+x} ]; then
     export EMSDK_PYTHON=${BUILD_PREFIX}/bin/python3
     export PYTHON=${BUILD_PREFIX}/bin/python3
 
-
-    
-
-    CONDA_EMSDK_DIR_CONFIG_FILE=$HOME/.emsdkdir
-    if test -f "$CONDA_EMSDK_DIR_CONFIG_FILE"; then
-        echo "Found config file $CONDA_EMSDK_DIR_CONFIG_FILE"
-    else
-        # return an error
-        echo "Config file $CONDA_EMSDK_DIR_CONFIG_FILE not found"
-        return 1
-    fi
-
-
+    CONDA_EMSDK_DIR=$PREFIX/opt/emsdk
+ 
     export EMSCRIPTEN_VERSION=$PKG_VERSION
-
-    export CONDA_EMSDK_DIR=$(<$CONDA_EMSDK_DIR_CONFIG_FILE)
     export EMSCRIPTEN_FORGE_EMSDK_DIR=$CONDA_EMSDK_DIR
-    echo "Using EMSCRIPTEN_FORGE_EMSDK_DIR $CONDA_EMSDK_DIR_CONFIG_FILE: " $EMSCRIPTEN_FORGE_EMSDK_DIR
-
     $CONDA_EMSDK_DIR/emsdk activate --embedded --build=Release $EMSCRIPTEN_VERSION
     
     source $CONDA_EMSDK_DIR/emsdk_env.sh
--- recipes/recipes/emscripten_emscripten-wasm32/build_compiler_package.sh
+++ recipes/recipes/emscripten_emscripten-wasm32/build_compiler_package.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 # Copy the [de]activate scripts to $PREFIX/etc/conda/[de]activate.d.
 # This will allow them to be run on environment activation.
 for CHANGE in "activate" "deactivate"
@@ -10,3 +8,26 @@ done
 
 mkdir -p ${PREFIX}/bin
 cp "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.sh" ${PREFIX}/bin/activate_emscripten.sh
+
+
+
+./emsdk install $PKG_VERSION
+mkdir -p $PREFIX/opt/emsdk/
+cp -r ./ $PREFIX/opt/emsdk/
+rm -rf $PREFIX/opt/emsdk/.git/
+rm -rf $PREFIX/opt/emsdk/upstream/emscripten/test/
+rm -rf $PREFIX/opt/emsdk/downloads
+rm -rf $PREFIX/opt/emsdk/python
+rm -rf $PREFIX/opt/emsdk/node
+rm -rf $PREFIX/opt/emsdk/bazel
+
+mkdir -p $PREFIX/bin
+for file in $PREFIX/opt/emsdk/bin/*; do
+    ln -s "$file" "$PREFIX/bin/"
+done
+# TODO can we get rid of `upstream/emscripten/system`?
+# that folder contains stuff like `eigen`, ...
+
+# - ./emsdk activate ${{ version }}
+# - source ./emsdk_env.sh
+# - exit 1
