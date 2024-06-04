60f4c6f20b44c182a823ec9af73db7caefb5d2eb
Isabel Paredes <isabel.paredes@quantstack.net>
2024-06-04 14:19:32 +0200

    Update flags

--- recipes/recipes_emscripten/r-base/build.sh
+++ recipes/recipes_emscripten/r-base/build.sh
@@ -13,10 +13,18 @@ export FFLAGS="$FFLAGS \
     --implicit-interface"
 export FPICFLAGS="-fPIC"
 
+export r_cv_header_zlib_h=yes
+export r_cv_have_bzlib=yes
+
 emconfigure ./configure \
     --prefix=$PREFIX    \
+    --build=$BUILD      \
+    --host=$HOST        \
     --with-lapack=no    \
     --with-blas=no      \
-    --with-readline=no
+    --with-readline=no  \
+    --with-x=no         \
+    --with-internal-tzcode=yes \
+    --with-recommended-packages=no
 
 emmake make -j${CPU_COUNT}
