7002ffd415284175140868acaa8286ef029e2c51
Isabel Paredes <isabel.paredes@quantstack.net>
2024-06-04 18:48:38 +0200

    Add config patch

--- recipes/recipes_emscripten/r-base/build.sh
+++ recipes/recipes_emscripten/r-base/build.sh
@@ -16,10 +16,19 @@ export FPICFLAGS="-fPIC"
 export r_cv_header_zlib_h=yes
 export r_cv_have_bzlib=yes
 
+
+# export LDFLAGS=${LDFLAGS//$PREFIX/$BUILD_PREFIX}
+# export CPPFLAGS=${CPPFLAGS//$PREFIX/$BUILD_PREFIX}
+# export NM=$($CC_FOR_BUILD -print-prog-name=nm)
+# export PKG_CONFIG_PATH=${BUILD_PREFIX}/lib/pkgconfig
+# export HOST=$BUILD_PREFIX
+# export IS_MINIMAL_R_BUILD=1
+
+# TODO: may need to compile twice, 1. regular, 2. cross-compile
+
 emconfigure ./configure \
     --prefix=$PREFIX    \
-    --build=$BUILD      \
-    --host=$HOST        \
+    --host=$ARCH \
     --with-lapack=no    \
     --with-blas=no      \
     --with-readline=no  \
