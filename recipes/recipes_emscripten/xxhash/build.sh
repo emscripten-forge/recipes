6c634465cacd6634e36af3cbfa5fada4113f1e6b
KGB99 <k.birgi@gmail.com>
2024-06-06 15:49:55 +0200

    remove CFLAGS variable

--- recipes/recipes_emscripten/xxhash/build.sh
+++ recipes/recipes_emscripten/xxhash/build.sh
@@ -2,13 +2,6 @@
 
 set -euxo pipefail
 
-export CFLAGS="${CFLAGS} -std=c99"
-#export CFLAGS="${CFLAGS:-} -std=c99"
-
-make CFLAGS="${CFLAGS}"
-make CFLAGS="${CFLAGS}" check test-xxhsum-c
-make CFLAGS="${CFLAGS}" install
-
 # Build step
-#emmake make install -j8
+emmake make install -j8
 
