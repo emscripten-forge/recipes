cc1ad797700c89008b5a2838702d0c4d85161108
Isabel Paredes <isabel.paredes@quantstack.net>
2024-06-11 17:49:47 +0200

    Disable freetype

--- recipes/recipes_emscripten/cairo/build.sh
+++ recipes/recipes_emscripten/cairo/build.sh
@@ -4,13 +4,13 @@ set -ex
 
 meson_config_args=(
     -Dfontconfig=enabled
-    -Dfreetype=enabled
+    -Dfreetype=disabled
     -Dglib=enabled
+    -Dpng=disabled
     -Dxlib=disabled
     -Dxlib-xcb=disabled
     -Dxcb=disabled
     -Dspectre=disabled
-    -Dzlib=disabled
     -Dtests=disabled
 )
 
@@ -26,3 +26,29 @@ meson setup builddir \
     --cross-file=$RECIPE_DIR/emscripten.meson.cross
 ninja -v -C builddir -j ${CPU_COUNT}
 ninja -C builddir install -j ${CPU_COUNT}
+
+
+
+# bash autogen.sh
+# chmod +x ./configure
+
+# export ax_cv_c_float_words_bigendian=no
+
+# emconfigure ./configure \
+#     --prefix="${PREFIX}" \
+#     --enable-ft \
+#     --enable-ps \
+#     --enable-pdf \
+#     --enable-svg \
+#     --disable-gtk-doc \
+#     --enable-xcb-shm
+
+# emmake make -j${CPU_COUNT}
+# # FAIL: check-link on OS X
+# # Hangs for > 10 minutes on Linux
+# #make check -j${CPU_COUNT}
+# emmake make install -j${CPU_COUNT}
+
+# # Remove any new Libtool files we may have installed. It is intended that
+# # conda-build will eventually do this automatically.
+# find $PREFIX -name '*.la' -delete
