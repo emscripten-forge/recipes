646054dc419e59622c5eabe264f76b8530042438
KGB99 <k.birgi@gmail.com>
2024-06-13 14:02:16 +0100

    add recipe and build

---
+++ recipes/recipes_emscripten/abseil-cpp/build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -exuo pipefail
+
+mkdir -p build
+cd build
+
+#if [[ "${target_platform}" == osx-* ]]; then
+#    # See https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk
+CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
+#fi
+
+#if [[ "$PKG_NAME" == "libabseil-tests" ]]; then
+CMAKE_ARGS="${CMAKE_ARGS} -DBUILD_TESTING=ON -DABSL_BUILD_TESTING=ON"
+CMAKE_ARGS="${CMAKE_ARGS} -DABSL_USE_EXTERNAL_GOOGLETEST=ON -DABSL_FIND_GOOGLETEST=ON"
+#fi
+
+emcmake -G Ninja \
+    ${CMAKE_ARGS} \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_CXX_STANDARD=17 \
+    -DCMAKE_INSTALL_LIBDIR=lib \
+    -DCMAKE_PREFIX_PATH=${PREFIX} \
+    -DBUILD_SHARED_LIBS=ON \
+    -DABSL_PROPAGATE_CXX_STD=ON \
+    ..
+
+emcmake --build .
+emcmake --install .
