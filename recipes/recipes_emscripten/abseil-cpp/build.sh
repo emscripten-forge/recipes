0c5518f83f392ba643107154c617589d2035e8cf
KGB99 <k.birgi@gmail.com>
2024-06-13 15:23:16 +0100

    build without tests

--- recipes/recipes_emscripten/abseil-cpp/build.sh
+++ recipes/recipes_emscripten/abseil-cpp/build.sh
@@ -5,17 +5,17 @@ set -exuo pipefail
 mkdir -p build
 cd build
 
-#if [[ "${target_platform}" == osx-* ]]; then
-#    # See https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk
-CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
-#fi
+if [[ "${target_platform}" == osx-* ]]; then
+    # See https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk
+    CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
+fi
 
-#if [[ "$PKG_NAME" == "libabseil-tests" ]]; then
-CMAKE_ARGS="${CMAKE_ARGS} -DBUILD_TESTING=ON -DABSL_BUILD_TESTING=ON"
-CMAKE_ARGS="${CMAKE_ARGS} -DABSL_USE_EXTERNAL_GOOGLETEST=ON -DABSL_FIND_GOOGLETEST=ON"
-#fi
+if [[ "$PKG_NAME" == "libabseil-tests" ]]; then
+    CMAKE_ARGS="${CMAKE_ARGS} -DBUILD_TESTING=ON -DABSL_BUILD_TESTING=ON"
+    CMAKE_ARGS="${CMAKE_ARGS} -DABSL_USE_EXTERNAL_GOOGLETEST=ON -DABSL_FIND_GOOGLETEST=ON"
+fi
 
-emcmake -G Ninja \
+cmake -GNinja \
     ${CMAKE_ARGS} \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_CXX_STANDARD=17 \
@@ -25,5 +25,5 @@ emcmake -G Ninja \
     -DABSL_PROPAGATE_CXX_STD=ON \
     ..
 
-emcmake --build .
-emcmake --install .
+# Build step
+ninja install
