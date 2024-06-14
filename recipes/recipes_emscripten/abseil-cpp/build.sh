ebe5da920e85d675d2c69e76ab1dad85016386ee
KGB99 <k.birgi@gmail.com>
2024-06-14 13:27:11 +0100

    remove libabseil-tests

--- recipes/recipes_emscripten/abseil-cpp/build.sh
+++ recipes/recipes_emscripten/abseil-cpp/build.sh
@@ -5,16 +5,6 @@ set -exuo pipefail
 mkdir -p build
 cd build
 
-if [[ "${target_platform}" == osx-* ]]; then
-    # See https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk
-    CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
-fi
-
-if [[ "$PKG_NAME" == "libabseil-tests" ]]; then
-    CMAKE_ARGS="${CMAKE_ARGS} -DBUILD_TESTING=ON -DABSL_BUILD_TESTING=ON"
-    CMAKE_ARGS="${CMAKE_ARGS} -DABSL_USE_EXTERNAL_GOOGLETEST=ON -DABSL_FIND_GOOGLETEST=ON"
-fi
-
 emcmake cmake -GNinja \
     ${CMAKE_ARGS} \
     -DCMAKE_BUILD_TYPE=Release \
