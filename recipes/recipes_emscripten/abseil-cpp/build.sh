d27aa50bca6fd9ad5a281edc7e79346a1940c037
KGB99 <k.birgi@gmail.com>
2024-06-14 11:14:07 +0100

    change cmake to emcmake

--- recipes/recipes_emscripten/abseil-cpp/build.sh
+++ recipes/recipes_emscripten/abseil-cpp/build.sh
@@ -15,13 +15,13 @@ if [[ "$PKG_NAME" == "libabseil-tests" ]]; then
     CMAKE_ARGS="${CMAKE_ARGS} -DABSL_USE_EXTERNAL_GOOGLETEST=ON -DABSL_FIND_GOOGLETEST=ON"
 fi
 
-cmake -GNinja \
+emcmake cmake -GNinja \
     ${CMAKE_ARGS} \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_CXX_STANDARD=17 \
     -DCMAKE_INSTALL_LIBDIR=lib \
     -DCMAKE_PREFIX_PATH=${PREFIX} \
-    -DBUILD_SHARED_LIBS=ON \
+    -DBUILD_SHARED_LIBS=OFF \
     -DABSL_PROPAGATE_CXX_STD=ON \
     ..
 
