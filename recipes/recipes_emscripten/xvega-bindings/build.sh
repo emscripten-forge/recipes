138f0b48b07ce32a5068990adeccb1898f41a461
Isabel Paredes <isabel.paredes@quantstack.net>
2024-06-17 17:41:23 +0200

    Remove unused vars

--- recipes/recipes_emscripten/xvega-bindings/build.sh
+++ recipes/recipes_emscripten/xvega-bindings/build.sh
@@ -7,9 +7,7 @@ cmake ${CMAKE_ARGS} ..             \
     -GNinja                        \
     -DCMAKE_BUILD_TYPE=Release     \
     -DCMAKE_PREFIX_PATH=$PREFIX    \
-    -DCMAKE_INSTALL_PREFIX=$PREFIX \
-    -DXVEGA_DISABLE_ARCH_NATIVE=ON \
-    -DCMAKE_POSITION_INDEPENDENT_CODE=ON 
-    
+    -DCMAKE_INSTALL_PREFIX=$PREFIX
+
 # Build step
 ninja install
