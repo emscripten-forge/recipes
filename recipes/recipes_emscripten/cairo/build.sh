89a9f0322c35c43b6cab0bb321510665bb11731c
Isabel Paredes <isabel.paredes@quantstack.net>
2024-06-19 17:21:35 +0200

    Clean up

--- recipes/recipes_emscripten/cairo/build.sh
+++ recipes/recipes_emscripten/cairo/build.sh
@@ -24,31 +24,6 @@ meson setup builddir \
     -Dlibdir=lib \
     --wrap-mode=nofallback \
     --cross-file=$RECIPE_DIR/emscripten.meson.cross
+
 ninja -v -C builddir -j ${CPU_COUNT}
 ninja -C builddir install -j ${CPU_COUNT}
-
-
-
-# bash autogen.sh
-# chmod +x ./configure
-
-# export ax_cv_c_float_words_bigendian=no
-
-# emconfigure ./configure \
-#     --prefix="${PREFIX}" \
-#     --enable-ft \
-#     --enable-ps \
-#     --enable-pdf \
-#     --enable-svg \
-#     --disable-gtk-doc \
-#     --enable-xcb-shm
-
-# emmake make -j${CPU_COUNT}
-# # FAIL: check-link on OS X
-# # Hangs for > 10 minutes on Linux
-# #make check -j${CPU_COUNT}
-# emmake make install -j${CPU_COUNT}
-
-# # Remove any new Libtool files we may have installed. It is intended that
-# # conda-build will eventually do this automatically.
-# find $PREFIX -name '*.la' -delete
