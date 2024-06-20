02ea5523c9dc3de5803759f384e4b98f193d160c
KGB99 <80490551+KGB99@users.noreply.github.com>
2024-06-20 18:02:56 +0100

    Add Geos (#1128)
    * first attempt
    
    * add patches for build
    
    * add existence tests
    
    * add ctest and cleanup
    
    * change ctest
    
    * remove ctest

---
+++ recipes/recipes_emscripten/geos/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+mkdir -p build && cd build
+
+emcmake cmake ${CMAKE_ARGS} \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+      -DCMAKE_INSTALL_LIBDIR=lib \
+      -DBUILD_SHARED_LIBS=OFF \
+      ..
+
+emmake make -j${CPU_COUNT} #${VERBOSE_CM}
+
+emmake make install -j${CPU_COUNT}
