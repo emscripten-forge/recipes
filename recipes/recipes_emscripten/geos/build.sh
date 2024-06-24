39f66070fdfb2497b6d5b4f3a7b7fe9410891a69
KGB99 <k.birgi@gmail.com>
2024-06-24 11:06:21 +0100

    update geos to be a shared library

--- recipes/recipes_emscripten/geos/build.sh
+++ recipes/recipes_emscripten/geos/build.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-
+printenv
+exit
 mkdir -p build && cd build
 
 emcmake cmake ${CMAKE_ARGS} \
       -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_INSTALL_PREFIX=${PREFIX} \
       -DCMAKE_INSTALL_LIBDIR=lib \
-      -DBUILD_SHARED_LIBS=OFF \
+      -DBUILD_SHARED_LIBS=ON \
+      -DCMAKE_PROJECT_INCLUDE=../overwriteProp.cmake
       ..
 
 emmake make -j${CPU_COUNT} #${VERBOSE_CM}
