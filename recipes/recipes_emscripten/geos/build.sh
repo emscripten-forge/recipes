4cffd629be2598119c2a561055c9d4e295961036
KGB99 <k.birgi@gmail.com>
2024-06-24 11:34:23 +0100

    attempt to make geos shared

--- recipes/recipes_emscripten/geos/build.sh
+++ recipes/recipes_emscripten/geos/build.sh
@@ -7,9 +7,9 @@ emcmake cmake ${CMAKE_ARGS} \
       -DCMAKE_INSTALL_PREFIX=${PREFIX} \
       -DCMAKE_INSTALL_LIBDIR=lib \
       -DBUILD_SHARED_LIBS=ON \
-      -DCMAKE_PROJECT_INCLUDE=../overwriteProp.cmake
+      -DCMAKE_PROJECT_INCLUDE=${RECIPE_DIR}/overwriteProp.cmake \
       ..
 
-emmake make -j${CPU_COUNT} #${VERBOSE_CM}
+emmake make -j${CPU_COUNT}
 
 emmake make install -j${CPU_COUNT}
