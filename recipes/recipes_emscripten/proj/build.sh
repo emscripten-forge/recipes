6579e4824b304be11acd5666374c5eeb8355bae9
KGB99 <k.birgi@gmail.com>
2024-07-01 15:17:43 +0100

    pass cmake step

--- recipes/recipes_emscripten/proj/build.sh
+++ recipes/recipes_emscripten/proj/build.sh
@@ -4,16 +4,22 @@ mkdir -p build
 
 cd build
 
+export SQLITE_DIR=${BUILD_PREFIX}/lib/sqlite3.40.0
+export SQLITE=${BUILD_PREFIX}/include
+
 # build without curl as stated in
 # https://github.com/OSGeo/PROJ/issues/1957
 emcmake cmake ${CMAKE_ARGS} .. \
       -DCMAKE_BUILD_TYPE=Release \
-      -DBUILD_SHARED_LIBS=ON \
+      -DBUILD_SHARED_LIBS=OFF \
+      -DBUILD_TESTING=OFF \
       -DCMAKE_INSTALL_PREFIX=${PREFIX} \
       -DCMAKE_INSTALL_LIBDIR=lib \
       -DENABLE_CURL=OFF \
       -DENABLE_TIFF=OFF \
       -DBUILD_PROJSYNC=OFF \
+      -DSQLite3_INCLUDE_DIR=${SQLITE} \
+      -DSQLite3_LIBRARY=${SQLITE} \
 
 #emmake make -j${CPU_COUNT} ${VERBOSE_CM}
 
