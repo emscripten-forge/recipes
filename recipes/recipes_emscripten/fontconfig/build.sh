506d833b403188f01e33375ce0f8a0212e0e0842
Isabel Paredes <isabel.paredes@quantstack.net>
2024-07-02 16:25:41 +0200

    Update recipe
    Working
    
    Clean up
    
    Update tests
    
    Fix formatting
    
    Working v2.14.0

--- recipes/recipes_emscripten/fontconfig/build.sh
+++ recipes/recipes_emscripten/fontconfig/build.sh
@@ -1,27 +1,16 @@
-export LDFLAGS="${LDFLAGS} -sUSE_FREETYPE=1 -sUSE_PTHREADS=0"
-export PTHREAD_CFLAGS=" "
+#/bin/bash
 
-# delete this file (is excluded in webr)
-rm ./src/fcobjshash.h
+meson_setup_args=(
+    -Dtests=disabled
+    -Ddefault_library=static
+    -Dtools=disabled
+)
 
-mkdir build && cd build
+meson setup builddir \
+    ${meson_setup_args[@]} \
+    --prefix=$PREFIX \
+    --buildtype=release \
+    --prefer-static \
+    --cross-file=$RECIPE_DIR/emscripten.meson.cross
 
-emconfigure ../configure \
-    ac_cv_func_fstatfs=no \
-    ac_cv_func_link=no \
-    --enable-shared=no \
-    --enable-static=yes \
-    --enable-expat \
-    --prefix=$PREFIX
-
-emmake make RUN_FC_CACHE_TEST=false install
-
-# Copy .wasm $PREFIX/bin/ files also
-cp fc-cache/fc-cache.wasm $PREFIX/bin/
-cp fc-cat/fc-cat.wasm $PREFIX/bin/
-cp fc-list/fc-list.wasm $PREFIX/bin/
-cp fc-match/fc-match.wasm $PREFIX/bin/
-cp fc-pattern/fc-pattern.wasm $PREFIX/bin/
-cp fc-query/fc-query.wasm $PREFIX/bin/
-cp fc-scan/fc-scan.wasm $PREFIX/bin/
-cp fc-validate/fc-validate.wasm $PREFIX/bin/
+meson install -C builddir
