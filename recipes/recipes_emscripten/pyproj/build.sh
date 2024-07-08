18b591b3d308312c64e7ffded98ee504256d6bd5
KGB99 <k.birgi@gmail.com>
2024-07-08 15:52:02 +0100

    add wrapper for wasm-ld

--- recipes/recipes_emscripten/pyproj/build.sh
+++ recipes/recipes_emscripten/pyproj/build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# override the wasm-ld command to a wrapper 
+# otherwise -R gets passed as flag
+alias wasm-ld="./wasm-ld-wrapper.sh"
+
+
 # # the binary is needed ....
 # cp $BUILD_PREFIX/bin/proj $PREFIX/bin/proj
 export PROJ_DIR=$PREFIX
@@ -7,8 +12,6 @@ export PROJ_DIR=$PREFIX
 # if version is not set, the python build script
 # tries to call the proj binary to get the version
 export PROJ_VERSION="9.4.1"
-
-
 
 ${PYTHON} -m pip install -vv .
 
---
+++ recipes/recipes_emscripten/pyproj/wasm-ld-wrapper.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo "Using wasm-ld wrapper..."
+
+# Filter out the -R flags from the arguments
+filtered_args=()
+skip_next=false
+
+for arg in "$@"; do
+    if [ "$skip_next" = true ]; then
+        skip_next=false
+        continue
+    fi
+
+    case "$arg" in
+        -R)
+            skip_next=true
+            ;;
+        *)
+            filtered_args+=("$arg")
+            ;;
+    esac
+done
+
+# Call the actual wasm-ld command with the filtered arguments
+wasm-ld "${filtered_args[@]}"
