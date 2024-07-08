39d6e0b73c9d32577701d69443be45f2471242a1
DerThorsten <derthorstenbeier@gmail.com>
2024-07-08 12:29:05 +0200

    bazinga

--- recipes/recipes_emscripten/pyproj/build.sh
+++ recipes/recipes_emscripten/pyproj/build.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
+
+# # the binary is needed ....
+# cp $BUILD_PREFIX/bin/proj $PREFIX/bin/proj
 export PROJ_DIR=$PREFIX
+
+# if version is not set, the python build script
+# tries to call the proj binary to get the version
+export PROJ_VERSION="9.4.1"
+
+
+
 ${PYTHON} -m pip install -vv .
+
+
+# rm $PREFIX/bin/proj
