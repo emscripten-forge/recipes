9592a6e18c216329b33d3e2b9dee19734dc5b3fc
DerThorsten <derthorstenbeier@gmail.com>
2024-07-09 07:26:13 +0200

    attempt 2

--- recipes/recipes_emscripten/pyproj/wasm-ld-wrapper.sh
+++ recipes/recipes_emscripten/pyproj/wasm-ld-wrapper.sh
@@ -5,15 +5,19 @@ echo "Using wasm-ld wrapper..."
 # Initialize an array to hold the modified arguments
 modified_args=()
 
+# Define the pattern to remove, with proper escaping for $PREFIX
+pattern='-Wl,-R$PREFIX/lib'
+
 # Iterate over all the arguments passed to the script
 for arg in "$@"; do
-  # If the argument contains "-R$PREFIX/lib", remove that part
-  if [[ "$arg" == *'-R$PREFIX/lib'* ]]; then
-    arg="${arg//-R\$PREFIX\/lib/}"
-  fi
+  # If the argument contains the pattern, remove that part
+  while [[ "$arg" == *"$pattern"* ]]; do
+    arg="${arg//-Wl,-R\$PREFIX\/lib/}"
+  done
   # Add the modified or unmodified argument to the array
   modified_args+=("$arg")
 done
+
 
 # Call the actual wasm-ld command with the filtered arguments
 echo "call  $BUILD_PREFIX/opt/emsdk/upstream/emscripten/emcc copy with args: ${modified_args[@]}"
