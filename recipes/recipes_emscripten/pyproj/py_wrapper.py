b263f875926e5a7414e2b211c159bbb13286004c
DerThorsten <derthorstenbeier@gmail.com>
2024-07-09 07:57:39 +0200

    v2

--- recipes/recipes_emscripten/pyproj/py_wrapper.py
+++ recipes/recipes_emscripten/pyproj/py_wrapper.py
@@ -8,10 +8,12 @@ if __name__ == '__main__':
     filtered_args = []
     for arg in args:
         print("arg:", arg)
-        if "-Wl,-R$PREFIX/lib" in arg:
-            print("removed -Wl,-R$PREFIX/lib")
-            continue
-        filtered_args.append(arg)
+
+
+        if ("-Wl,-R$PREFIX/lib" in arg )or ("-R" in arg):
+            print("removed arg:", arg)
+        else:
+            filtered_args.append(arg)
     
     # run the actual command in argv[1]
     subprocess.run([sys.argv[1]] + filtered_args)
