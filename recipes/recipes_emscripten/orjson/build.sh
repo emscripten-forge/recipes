e21f7646d99483444333d4ffecd728df87fb2862
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2024-07-12 15:33:20 +0000

    update build.sh

--- recipes/recipes_emscripten/orjson/build.sh
+++ recipes/recipes_emscripten/orjson/build.sh
@@ -2,5 +2,6 @@
 
 
 export MATURIN_PYTHON_SYSCONFIGDATA_DIR=${PREFIX}/etc/conda/_sysconfigdata__emscripten_wasm32-emscripten.py
+export RUSTFLAGS="-Z link-native-libraries=yes"
 ${PYTHON} -m pip  install . -vvv
 
