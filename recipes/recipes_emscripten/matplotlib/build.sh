3198c75add9c1b5fb315f6231e4ea60b6cbe4329
Ian Thomas <ianthomas23@gmail.com>
2024-07-15 09:07:03 +0100

    WIP

--- recipes/recipes_emscripten/matplotlib/build.sh
+++ recipes/recipes_emscripten/matplotlib/build.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-$EMSCRIPTEN_FORGE_EMSDK_DIR/upstream/emscripten/embuilder build freetype --pic
+cp $RECIPE_DIR/emscripten.meson.cross $SRC_DIR
+echo "python = '${PYTHON}'" >> $SRC_DIR/emscripten.meson.cross
 
-export LDFLAGS="$LDFLAGS -s USE_FREETYPE=1 -s USE_LIBPNG=1 -s USE_ZLIB=1"
-export CFLAGS="$CFLAGS -s USE_FREETYPE=1 -s USE_LIBPNG=1 -s USE_ZLIB=1"
-
-$PYTHON -m pip install . --no-deps
+${PYTHON} -m pip install . -vvv --no-deps --no-build-isolation \
+    -Csetup-args="--cross-file=$SRC_DIR/emscripten.meson.cross"
 
 export MATPLOTLIB_LOCATION=$PREFIX/lib/python$PY_VER/site-packages/matplotlib
+echo $MATPLOTLIB_LOCATION
+ls $MATPLOTLIB_LOCATION
 
 # Pre-built font cache
 cp $RECIPE_DIR/src/fontlist.json $MATPLOTLIB_LOCATION/
@@ -18,11 +19,9 @@ rm -rf $MATPLOTLIB_LOCATION/backends/qt_editor
 rm -rf $MATPLOTLIB_LOCATION/backends/web_backend
 rm -rf $MATPLOTLIB_LOCATION/sphinxext
 
-
 # rename import _qhull to import _qhull_matplotlib in file F
-F=$MATPLOTLIB_LOCATION/tri/triangulation.py
+F=$MATPLOTLIB_LOCATION/tri/_triangulation.py
 sed -i 's/_qhull/_qhull_matplotlib/g' $F
 
 # rename file $MATPLOTLIB_LOCATION/_qhull.* to $MATPLOTLIB_LOCATION/_qhull_matplotlib.*
 mv $MATPLOTLIB_LOCATION/_qhull.cpython-311-wasm32-emscripten.so $MATPLOTLIB_LOCATION/_qhull_matplotlib.cpython-311-wasm32-emscripten.so
-
