8ed36aa883235391e9ebafa00d8f4a034dfa44ed
DerThorsten <derthorstenbeier@gmail.com>
2024-07-15 10:36:29 +0200

    rust + orjson changs

--- recipes/recipes/rust/activate-rust.sh
+++ recipes/recipes/rust/activate-rust.sh
@@ -26,11 +26,11 @@ export WASM32_UNKNOWN_EMSCRIPTEN_OPENSSL_INCLUDE_DIR=$PREFIX/include
 
 
 export PYO3_CROSS=1
-export PYO3_PYTHON=python3
+export PYO3_PYTHON=$BUILD_PREFIX/bin/python
 export PYO3_CROSS_PYTHON_VERSION=$PY_VER
 export PYO3_CROSS_LIB_DIR=$PREFIX/lib
 export PYO3_CROSS_INCLUDE_DIR=$PREFIX/include
-export PYO3_PYTHON=python
+
 
 export CARGO_BUILD_TARGET="wasm32-unknown-emscripten"
 export LDFLAGS="$LDFLAGS -L${PREFIX}/lib"
--- recipes/recipes_emscripten/orjson/build.sh
+++ recipes/recipes_emscripten/orjson/build.sh
@@ -4,24 +4,29 @@
 export MATURIN_PYTHON_SYSCONFIGDATA_DIR=${PREFIX}/etc/conda/_sysconfigdata__emscripten_wasm32-emscripten.py
 
 
-# create PYO3_CONFIG_FILE file on the fly
-touch pyo3_config.toml
+# # create PYO3_CONFIG_FILE file on the fly
+# touch pyo3_config.toml
 
-# Add the content to the file with the expanded BUILD_PREFIX
-cat <<EOL > pyo3_config.toml
-[tool.pyo3]
-# Path to the Python interpreter to be used
-python = "${BUILD_PREFIX}/bin/python"
-EOL
-
-
+# # Add the content to the file with the expanded BUILD_PREFIX
+# cat <<EOL > pyo3_config.toml
+# [tool.pyo3]
+# # Path to the Python interpreter to be used
+# python = "${BUILD_PREFIX}/bin/python"
+# EOL
 
 
+# # copy python interpreter from $BUILD_PREFIX to $PREFIX
+# mkdir -p $PREFIX/bin
+# cp $BUILD_PREFIX/bin/python $PREFIX/bin/python
 
-#  copy python interpreter from $BUILD_PREFIX to $PREFIX
-export PYO3_CROSS_PYTHON_VERSION=3.12
+
+
+
+# #  copy python interpreter from $BUILD_PREFIX to $PREFIX
+# export PYO3_CROSS_PYTHON_VERSION=3.12
 export PYO3_PYTHON=$BUILD_PREFIX/bin/python
-export PYTHON_SYS_EXECUTABLE=$BUILD_PREFIX/bin/python
-export PYO3_CROSS_LIB_DIR=$PREFIX/lib
-export PYO3_CONFIG_FILE=$(pwd)/pyo3_config.toml
-${PYTHON} -m pip  install . -vvv
+# export PYTHON_SYS_EXECUTABLE=$BUILD_PREFIX/bin/python
+# export PYO3_CROSS_LIB_DIR=$PREFIX/lib
+# export PYO3_CONFIG_FILE=$(pwd)/pyo3_config.toml
+${PYTHON} -m pip  install . -vvv || true
+${PYTHON} -m pip  install . -vvv 
