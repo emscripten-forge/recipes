e9a919d85f86f7726483228c06b07d8bd0453de3
Wolf Vollprecht <w.vollprecht@gmail.com>
2024-07-16 10:39:11 +0200

    more clean up

--- recipes/recipes_emscripten/matplotlib/build-base.sh
+++ recipes/recipes_emscripten/matplotlib/build-base.sh
@@ -3,6 +3,7 @@
 # Set up the cross file
 # Note: the include folder is moved to `$BUILD_PREFIX` by the cross-python activation script
 export NUMPY_INCLUDE_DIR="$BUILD_PREFIX/lib/python${PY_VER}/site-packages/numpy/core/include"
+
 # write out the cross file
 sed "s|@(NUMPY_INCLUDE_DIR)|${NUMPY_INCLUDE_DIR}|g" $RECIPE_DIR/emscripten.meson.cross > $SRC_DIR/emscripten.meson.cross.temp
 sed "s|@(PYTHON)|${PYTHON}|g" $SRC_DIR/emscripten.meson.cross.temp > $SRC_DIR/emscripten.meson.cross
@@ -30,12 +31,3 @@ cp $RECIPE_DIR/src/Humor-Sans-1.0.ttf $MATPLOTLIB_LOCATION/mpl-data/fonts/ttf/Hu
 rm -rf $MATPLOTLIB_LOCATION/backends/qt_editor
 rm -rf $MATPLOTLIB_LOCATION/backends/web_backend
 rm -rf $MATPLOTLIB_LOCATION/sphinxext
-
-
-# # rename import _qhull to import _qhull_matplotlib in file F
-# F=$MATPLOTLIB_LOCATION/tri/triangulation.py
-# sed -i 's/_qhull/_qhull_matplotlib/g' $F
-
-# # rename file $MATPLOTLIB_LOCATION/_qhull.* to $MATPLOTLIB_LOCATION/_qhull_matplotlib.*
-# mv $MATPLOTLIB_LOCATION/_qhull.cpython-311-wasm32-emscripten.so $MATPLOTLIB_LOCATION/_qhull_matplotlib.cpython-311-wasm32-emscripten.so
-
