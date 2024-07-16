7d6f411b6de391a18ba67277f2942f1d12bb57a4
Wolf Vollprecht <w.vollprecht@gmail.com>
2024-07-16 09:53:40 +0200

    add tests to qhull

--- recipes/recipes_emscripten/qhull/build.sh
+++ recipes/recipes_emscripten/qhull/build.sh
@@ -1,5 +1,11 @@
-mkdir build && cd build
-
+mkdir -p qhull-build && cd qhull-build
 cmake -GNinja $SRC_DIR $CMAKE_ARGS
-
 ninja install
+
+cp qhull.wasm $PREFIX/bin/qhull.wasm
+cp qhull.wasm $PREFIX/bin/qhull.wasm
+cp rbox.wasm $PREFIX/bin/rbox.wasm
+cp qconvex.wasm $PREFIX/bin/qconvex.wasm
+cp qdelaunay.wasm $PREFIX/bin/qdelaunay.wasm
+cp qvoronoi.wasm $PREFIX/bin/qvoronoi.wasm
+cp qhalf.wasm $PREFIX/bin/qhalf.wasm
