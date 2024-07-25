5135d2a0ba935394b69e03b2676f804cabbe9ba1
Isabel Paredes <isabel.paredes@quantstack.net>
2024-07-25 17:19:56 +0200

    WIP

--- recipes/recipes_emscripten/r-base/build.sh
+++ recipes/recipes_emscripten/r-base/build.sh
@@ -5,51 +5,20 @@ set -ex
 export
 
 # Copy flang
-export FLANG_DIR=/home/ihuicatl/Repos/Packaging/llvm-project/_finstall
-cp -r $FLANG_DIR/bin/* $BUILD_PREFIX/bin/
-cp -r $FLANG_DIR/lib/* $BUILD_PREFIX/lib/
-cp -r $FLANG_DIR/include/* $BUILD_PREFIX/include/
-cp -r $FLANG_DIR/share/* $BUILD_PREFIX/share/
+# export FLANG_DIR=/home/ihuicatl/Repos/Packaging/llvm-project/_finstall
+# cp -r $FLANG_DIR/bin/* $BUILD_PREFIX/bin/
+# cp -r $FLANG_DIR/lib/* $BUILD_PREFIX/lib/
+# cp -r $FLANG_DIR/include/* $BUILD_PREFIX/include/
+# cp -r $FLANG_DIR/share/* $BUILD_PREFIX/share/
 
 export FC=flang-new
-# export F77=gfortran
-# export F90=gfortran
-# export F95=gfortran
-# export GFORTRAN=gfortran
-# export EMSDK_PATH=${EMSCRIPTEN_FORGE_EMSDK_DIR}
-# export FFLAGS="$FFLAGS \
-#     --target=wasm32-unknown-emscripten \
-#     --generate-object-code \
-#     --fixed-form-infer \
-#     --implicit-interface"
-# export FPICFLAGS="-fPIC"
-
-# export CC=emcc
-# export CXX=em++
-# export r_cv_header_zlib_h=yes
-# export r_cv_have_bzlib=yes
 
 
-# export LDFLAGS=${LDFLAGS//$PREFIX/$BUILD_PREFIX}
-# export CPPFLAGS=${CPPFLAGS//$PREFIX/$BUILD_PREFIX}
-# export NM=$($CC_FOR_BUILD -print-prog-name=nm)
 export PKG_CONFIG=${BUILD_PREFIX}/bin/pkg-config
 export PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig
 export PKG_CONFIG_LIBDIR=$PREFIX/lib
-# export HOST=$BUILD_PREFIX
-# export IS_MINIMAL_R_BUILD=1
 
-# TODO: may need to compile twice, 1. regular, 2. cross-compile
-
-# export r_cv_working_mktime=yes
-
-export CC=emcc
-export CXX=em++
-export CFLAGS="$CFLAGS -I$PREFIX/include"
-export CXXFLAGS="$CXXFLAGS -I$PREFIX/include"
-export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
-
-# Skip non-working checks
+# # Skip non-working checks
 export r_cv_header_zlib_h=yes # Otherwise the version check fails
 export r_cv_have_bzlib=yes
 export ac_cv_lib_lzma_lzma_version_number=yes
@@ -60,22 +29,27 @@ export r_cv_have_pcre832=yes
 export r_cv_have_curl_https=no
 export r_cv_size_max=yes
 
-export ac_cv_func_stpcpy=yes
+# export CFLAGS="-O2 --minify=0 -sALLOW_MEMORY_GROWTH=1 -sENVIRONMENT=web,worker -sEXPORTED_RUNTIME_METHODS=callMain,FS,ENV,getEnvStrings -sFORCE_FILESYSTEM=1 -sINVOKE_RUN=0 -sMODULARIZE=1 -sSINGLE_FILE=1 -sERROR_ON_UNDEFINED_SYMBOLS=0"
+
+
+# Get an updated config.sub and config.guess
+# cp $BUILD_PREFIX/share/libtool/build-aux/config.* .
 
 chmod +x configure
 emconfigure ./configure \
     --prefix=$PREFIX    \
     --build="x86_64-conda-linux-gnu" \
     --host="wasm32-unknown-emscripten" \
-    --with-pcre2=yes     \
-    --with-zlib=$PREFIX/lib/libz.a    \
-    --with-bzlib=yes     \
+    --with-pcre2=no     \
+    --with-zlib=no    \
+    --with-bzlib=no     \
     --with-lapack=no    \
     --with-blas=no      \
     --with-readline=no  \
     --with-x=no         \
-    --with-cairo=yes    \
+    --with-cairo=no    \
     --enable-static=yes \
+    --enable-shared=no  \
     --with-internal-tzcode \
     --with-recommended-packages=no
 
