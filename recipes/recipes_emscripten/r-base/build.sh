8c2c3f73301fafb5891a0000f8e13a1a7693a816
Isabel Paredes <isabel.paredes@quantstack.net>
2024-07-29 19:02:07 +0200

    Almost building

--- recipes/recipes_emscripten/r-base/build.sh
+++ recipes/recipes_emscripten/r-base/build.sh
@@ -2,17 +2,29 @@
 
 set -ex
 
-export
+# export
 
 # Copy flang
-# export FLANG_DIR=/home/ihuicatl/Repos/Packaging/llvm-project/_finstall
-# cp -r $FLANG_DIR/bin/* $BUILD_PREFIX/bin/
-# cp -r $FLANG_DIR/lib/* $BUILD_PREFIX/lib/
-# cp -r $FLANG_DIR/include/* $BUILD_PREFIX/include/
-# cp -r $FLANG_DIR/share/* $BUILD_PREFIX/share/
+export FLANG_DIR=/home/ihuicatl/Repos/Packaging/llvm-project/_finstall18
+cp -r $FLANG_DIR/bin/* $BUILD_PREFIX/bin/
+cp -r $FLANG_DIR/lib/* $BUILD_PREFIX/lib/
+cp -r $FLANG_DIR/include/* $BUILD_PREFIX/include/
+cp -r $FLANG_DIR/share/* $BUILD_PREFIX/share/
+unset FLANG_DIR
+
+# Copy Clang RT
+cp $RECIPE_DIR/libclang_rt.builtins-wasm32.a $BUILD_PREFIX/lib/
+# cp -r $WASI_DIR/bin/* $BUILD_PREFIX/bin/
+# cp -r $WASI_DIR/lib/* $BUILD_PREFIX/lib/
+# cp -r $WASI_DIR/share/* $BUILD_PREFIX/share/
+# unset WASI_DIR
+
+# Copy wabt
+# cp /home/ihuicatl/Repos/Emscripten/wabt/build/wasm-objdump $BUILD_PREFIX/bin/
+# export objdump=$BUILD_PREFIX/bin/wabm-objdump
+
 
 export FC=flang-new
-
 
 export PKG_CONFIG=${BUILD_PREFIX}/bin/pkg-config
 export PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig
@@ -26,28 +38,36 @@ export ac_cv_header_lzma_h=yes
 export r_cv_have_lzma=yes
 export r_cv_have_pcre2utf=yes
 export r_cv_have_pcre832=yes
-export r_cv_have_curl_https=no
 export r_cv_size_max=yes
+
+export ac_cv_lib_z_inflateInit2_=yes
+export ac_cv_lib_bz2_BZ2_bzlibVersion=yes
 
 # export CFLAGS="-O2 --minify=0 -sALLOW_MEMORY_GROWTH=1 -sENVIRONMENT=web,worker -sEXPORTED_RUNTIME_METHODS=callMain,FS,ENV,getEnvStrings -sFORCE_FILESYSTEM=1 -sINVOKE_RUN=0 -sMODULARIZE=1 -sSINGLE_FILE=1 -sERROR_ON_UNDEFINED_SYMBOLS=0"
 
+# Libraries to pass to the linker
+export LIBS="-lz"
+
+# linker flags, e.g. -L<lib dir> if you have libraries in a
+# nonstandard directory <lib dir>
+export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
+
+# (Objective) C/C++ preprocessor flags, e.g. -I<include dir> if
+# you have headers in a nonstandard directory <include dir>
+export CPPFLAGS="-I$PREFIX/include" # Otherwise can't find zlib.h
 
 # Get an updated config.sub and config.guess
 # cp $BUILD_PREFIX/share/libtool/build-aux/config.* .
 
-chmod +x configure
+chmod +x ./configure
 emconfigure ./configure \
     --prefix=$PREFIX    \
     --build="x86_64-conda-linux-gnu" \
     --host="wasm32-unknown-emscripten" \
-    --with-pcre2=no     \
-    --with-zlib=no    \
-    --with-bzlib=no     \
-    --with-lapack=no    \
-    --with-blas=no      \
-    --with-readline=no  \
-    --with-x=no         \
-    --with-cairo=no    \
+    --enable-blas-shlib \
+    --enable-R-shlib=no \
+    --without-readline  \
+    --without-x         \
     --enable-static=yes \
     --enable-shared=no  \
     --with-internal-tzcode \
