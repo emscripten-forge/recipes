d6ebfb3e93d8ca9ad91a1c36d11eeea2b47eccd0
Isabel Paredes <isabel.paredes@quantstack.net>
2024-07-30 16:30:02 +0200

    WIP

--- recipes/recipes_emscripten/r-base/build.sh
+++ recipes/recipes_emscripten/r-base/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -ex
+echo "HEEELLOO"
 
 # export
 
@@ -46,7 +46,7 @@ export ac_cv_lib_bz2_BZ2_bzlibVersion=yes
 # export CFLAGS="-O2 --minify=0 -sALLOW_MEMORY_GROWTH=1 -sENVIRONMENT=web,worker -sEXPORTED_RUNTIME_METHODS=callMain,FS,ENV,getEnvStrings -sFORCE_FILESYSTEM=1 -sINVOKE_RUN=0 -sMODULARIZE=1 -sSINGLE_FILE=1 -sERROR_ON_UNDEFINED_SYMBOLS=0"
 
 # Libraries to pass to the linker
-export LIBS="-lz"
+export LIBS="-lz -lFortranRuntime"
 
 # linker flags, e.g. -L<lib dir> if you have libraries in a
 # nonstandard directory <lib dir>
@@ -56,19 +56,31 @@ export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
 # you have headers in a nonstandard directory <include dir>
 export CPPFLAGS="-I$PREFIX/include" # Otherwise can't find zlib.h
 
+# Add atomics and bulk-memory features
+export CFLAGS="$CFLAGS -matomics -mbulk-memory"
+export CXXFLAGS="$CXXFLAGS -matomics -mbulk-memory"
+
+# Otherwise set to .not_implemented and cannot be used
+# Must be shared... otherwise duplicate symbol issues
+export SHLIB_EXT=".so"
+
 # Get an updated config.sub and config.guess
 # cp $BUILD_PREFIX/share/libtool/build-aux/config.* .
+
+export R="${BUILD_PREFIX}/bin/R"
+export R_ARGS="--library=${PREFIX}/lib/R/library --no-test-load"
 
 chmod +x ./configure
 emconfigure ./configure \
     --prefix=$PREFIX    \
     --build="x86_64-conda-linux-gnu" \
     --host="wasm32-unknown-emscripten" \
-    --enable-blas-shlib \
+    --disable-shared   \
+    --enable-blas-shlib=no \
     --enable-R-shlib=no \
     --without-readline  \
     --without-x         \
-    --enable-static=yes \
+    --enable-static="[Rblas, Rlapack]" \
     --enable-shared=no  \
     --with-internal-tzcode \
     --with-recommended-packages=no
