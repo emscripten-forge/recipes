5b0b7e744942f666207720a36a77101799159846
KGB99 <k.birgi@gmail.com>
2024-08-13 14:24:54 +0100

    update python from 3.11 to 3.12

--- recipes/recipes_emscripten/scipy/build.sh
+++ recipes/recipes_emscripten/scipy/build.sh
@@ -5,6 +5,6 @@ PYODIDE_PACKED=$RECIPE_DIR/scipy-1.11.1-cp311-cp311-emscripten_3_1_45_wasm32.whl
 unzip $PYODIDE_PACKED
 
 # copy complete folder scipy to side-packages
-mkdir -p $PREFIX/lib/python3.11/site-packages
-cp -r scipy $PREFIX/lib/python3.11/site-packages
-cp -r scipy-1.11.1.dist-info $PREFIX/lib/python3.11/site-packages
+mkdir -p $PREFIX/lib/python3.12/site-packages
+cp -r scipy $PREFIX/lib/python3.12/site-packages
+cp -r scipy-1.11.1.dist-info $PREFIX/lib/python3.12/site-packages
