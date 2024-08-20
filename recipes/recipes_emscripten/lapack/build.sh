038bfe188fce4d5c6fad9189340824b9c5ee62c7
Isabel Paredes <isabel.paredes@quantstack.net>
2024-08-20 13:50:09 +0200

    Add libblas and liblapack (#1284)
    * Add lapack
    
    Modify build flags
    
    Simplify recipe
    
    * Disable fortran compiler tests
    
    * Update version
    
    * Add patch to disable fortran check
    
    * Disable fortran c interface check
    
    Fix unexpected arg -pp
    
    Set a timeout
    
    Add patch to skip files with errors
    
    Enable emscripten target
    
    Generate object code
    
    * Working build of lapack, blas, and runtime
    
    * Replace local flang
    
    * Clean up recipe
    
    Clean patches
    
    Remove clang link
    
    Add tests
    
    Clean build flags
    
    Restore workflow

---
+++ recipes/recipes_emscripten/lapack/build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -ex
+
+# Using flang as a WASM cross-compiler
+# https://github.com/serge-sans-paille/llvm-project/blob/feature/flang-wasm/README.wasm.md
+# https://github.com/conda-forge/flang-feedstock/pull/69
+micromamba install -p $BUILD_PREFIX \
+    conda-forge/label/llvm_rc::libllvm19 \
+    conda-forge/label/llvm_dev::flang=19.1.0.rc2 \
+    -y --no-channel-priority
+rm $BUILD_PREFIX/bin/clang # links to clang19
+ln -s $BUILD_PREFIX/bin/clang-18 $BUILD_PREFIX/bin/clang # links to emsdk clang
+
+export FC=flang-new
+export FFLAGS="--target=wasm32-unknown-emscripten"
+
+emcmake cmake -S . -B _build \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCBLAS=no \
+    -DLAPACKE=no \
+    -DBUILD_TESTING=no \
+    -DCMAKE_Fortran_PREPROCESS=yes \
+    -DCMAKE_Fortran_COMPILER=$FC \
+    -DCMAKE_Fortran_FLAGS=$FFLAGS \
+    -DCMAKE_INSTALL_LIBDIR="lib" \
+    -DCMAKE_INSTALL_PREFIX=$PREFIX
+
+$(which cmake) --build _build
+$(which cmake) --install _build
