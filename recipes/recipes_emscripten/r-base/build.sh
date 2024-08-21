2eb17a919085d8536381bda12707310165fc0d60
Isabel Paredes <isabel.paredes@quantstack.net>
2024-08-21 17:42:42 +0200

    Override some shared flags

--- recipes/recipes_emscripten/r-base/build.sh
+++ recipes/recipes_emscripten/r-base/build.sh
@@ -53,10 +53,18 @@ export SHLIB_CFLAGS="-sSIDE_MODULE"
 #                  shared object.
 #   SHLIB_LD:      command for linking shared objects which contain object files
 #                  from a C or Fortran compiler only
+
 #   SHLIB_LDFLAGS: special flags used by SHLIB_LD
+# NOTE: Needed to override the default -shared flag, however this causes an
+# issue with zlib.
+# wasm-ld: error: $PREFIX/lib/libz.a(crc32.o): relocation R_WASM_MEMORY_ADDR_SLEB
+# cannot be used against symbol `crc_table`; recompile with -fPIC
+# export SHLIB_LDFLAGS="-sSIDE_MODULE"
+
 #   DYLIB_LD:      command for linking dynamic libraries which contain object files
 #                  from a C or Fortran compiler only
 #   DYLIB_LDFLAGS: special flags used for make a dynamic library
+export DYLIB_LDFLAGS="-sSIDE_MODULE"
 #   CXXPICFLAGS:   special flags for compiling C++ code to be turned into a shared
 #                  object
 #   SHLIB_CXXLD:   command for linking shared objects which contain object files
