1a37da9e3b86689c457c79d7b5762668d0c35b08
Isabel Paredes <isabel.paredes@quantstack.net>
2024-08-21 12:09:39 +0200

    Set identical versions for llvm and flang

--- recipes/recipes_emscripten/r-base/build.sh
+++ recipes/recipes_emscripten/r-base/build.sh
@@ -6,7 +6,7 @@ set -e
 # https://github.com/serge-sans-paille/llvm-project/blob/feature/flang-wasm/README.wasm.md
 # https://github.com/conda-forge/flang-feedstock/pull/69
 micromamba install -p $BUILD_PREFIX \
-    conda-forge/label/llvm_rc::libllvm19 \
+    conda-forge/label/llvm_rc::libllvm19=19.1.0.rc2 \
     conda-forge/label/llvm_dev::flang=19.1.0.rc2 \
     -y --no-channel-priority
 rm $BUILD_PREFIX/bin/clang # links to clang19
