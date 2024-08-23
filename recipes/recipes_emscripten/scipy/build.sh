b830892cba1f62fd916422cd501892fa8b18e30d
KGB99 <k.birgi@gmail.com>
2024-08-23 13:43:08 +0200

    remove flags from build

--- recipes/recipes_emscripten/scipy/build.sh
+++ recipes/recipes_emscripten/scipy/build.sh
@@ -13,10 +13,11 @@ micromamba install -p $BUILD_PREFIX \
 rm $BUILD_PREFIX/bin/clang # links to clang19
 ln -s $BUILD_PREFIX/bin/clang-18 $BUILD_PREFIX/bin/clang # links to emsdk clang
 
-export CFLAGS="-I$PREFIX/include \ 
-    -Wno-return-type \ 
-    -DUNDERSCORE_G77 \ 
-    -fvisibility=default"
+#export CFLAGS="${CFLAGS} \
+#    -I$PREFIX/include \ 
+#    -Wno-return-type \ 
+#    -DUNDERSCORE_G77 \ 
+#    -fvisibility=default"
 
 #export CXXFLAGS="
 #    -fexceptions \
@@ -27,7 +28,7 @@ export CFLAGS="-I$PREFIX/include \
 
 export LDFLAGS="-L$PREFIX/lib"
 
-export DYLIB_LDFLAGS="-sSIDE_MODULE"
+#export DYLIB_LDFLAGS="-sSIDE_MODULE"
 
 #export BACKEND_FLAGS="
 #    -build-dir=build \
@@ -48,36 +49,36 @@ export FCFLAGS="$FFLAGS --target=wasm32-unknown-emscripten"
 echo $PKG_CONFIG_PATH
 
 # Change many functions that return void into functions that return int
-#sed -i 's/void DQA/int DQA/g' scipy/integrate/__quadpack.h
-#find scipy -name "*.c*" -type f | xargs sed -i 's/extern void F_FUNC/extern int F_FUNC/g'
-#
-#sed -i 's/void F_FUNC/int F_FUNC/g' scipy/odr/__odrpack.c
-#sed -i 's/^void/int/g' scipy/odr/odrpack.h
-#sed -i 's/^void/int/g' scipy/odr/__odrpack.c
-#
-#sed -i 's/void BLAS_FUNC/int BLAS_FUNC/g' scipy/special/lapack_defs.h
-## sed -i 's/void F_FUNC/int F_FUNC/g' scipy/linalg/_lapack_subroutines.h
-#sed -i 's/extern void/extern int/g' scipy/optimize/__minpack.h
-#sed -i 's/void/int/g' scipy/linalg/cython_blas_signatures.txt
-#sed -i 's/void/int/g' scipy/linalg/cython_lapack_signatures.txt
-#sed -i 's/^void/int/g' scipy/interpolate/src/_fitpackmodule.c
-#
-#sed -i 's/extern void/extern int/g' scipy/sparse/linalg/_dsolve/SuperLU/SRC/*.{c,h}
-#sed -i 's/PUBLIC void/PUBLIC int/g' scipy/sparse/linalg/_dsolve/SuperLU/SRC/*.{c,h}
-#sed -i 's/^void/int/g' scipy/sparse/linalg/_dsolve/SuperLU/SRC/*.{c,h}
-#sed -i 's/^void/int/g' scipy/sparse/linalg/_dsolve/*.{c,h}
-#sed -i 's/void \(.\)print/int \1/g' scipy/sparse/linalg/_dsolve/SuperLU/SRC/*.{c,h}
-#sed -i 's/TYPE_GENERIC_FUNC(\(.*\), void)/TYPE_GENERIC_FUNC(\1, int)/g' scipy/sparse/linalg/_dsolve/_superluobject.h
-#
-#sed -i 's/^void/int/g' scipy/optimize/_trlib/trlib_private.h
-#sed -i 's/^void/int/g' scipy/optimize/_trlib/trlib/trlib_private.h
-#sed -i 's/^void/int/g' scipy/_build_utils/src/wrap_dummy_g77_abi.c
-#sed -i 's/, int)/)/g' scipy/optimize/_trlib/trlib_private.h
-#sed -i 's/, 1)/)/g' scipy/optimize/_trlib/trlib_private.h
-#
-#sed -i 's/^void/int/g' scipy/spatial/qhull_misc.h
-#sed -i 's/, size_t)/)/g' scipy/spatial/qhull_misc.h
-#sed -i 's/,1)/)/g' scipy/spatial/qhull_misc.h
+sed -i 's/void DQA/int DQA/g' scipy/integrate/__quadpack.h
+find scipy -name "*.c*" -type f | xargs sed -i 's/extern void F_FUNC/extern int F_FUNC/g'
+
+sed -i 's/void F_FUNC/int F_FUNC/g' scipy/odr/__odrpack.c
+sed -i 's/^void/int/g' scipy/odr/odrpack.h
+sed -i 's/^void/int/g' scipy/odr/__odrpack.c
+
+sed -i 's/void BLAS_FUNC/int BLAS_FUNC/g' scipy/special/lapack_defs.h
+# sed -i 's/void F_FUNC/int F_FUNC/g' scipy/linalg/_lapack_subroutines.h
+sed -i 's/extern void/extern int/g' scipy/optimize/__minpack.h
+sed -i 's/void/int/g' scipy/linalg/cython_blas_signatures.txt
+sed -i 's/void/int/g' scipy/linalg/cython_lapack_signatures.txt
+sed -i 's/^void/int/g' scipy/interpolate/src/_fitpackmodule.c
+
+sed -i 's/extern void/extern int/g' scipy/sparse/linalg/_dsolve/SuperLU/SRC/*.{c,h}
+sed -i 's/PUBLIC void/PUBLIC int/g' scipy/sparse/linalg/_dsolve/SuperLU/SRC/*.{c,h}
+sed -i 's/^void/int/g' scipy/sparse/linalg/_dsolve/SuperLU/SRC/*.{c,h}
+sed -i 's/^void/int/g' scipy/sparse/linalg/_dsolve/*.{c,h}
+sed -i 's/void \(.\)print/int \1/g' scipy/sparse/linalg/_dsolve/SuperLU/SRC/*.{c,h}
+sed -i 's/TYPE_GENERIC_FUNC(\(.*\), void)/TYPE_GENERIC_FUNC(\1, int)/g' scipy/sparse/linalg/_dsolve/_superluobject.h
+
+sed -i 's/^void/int/g' scipy/optimize/_trlib/trlib_private.h
+sed -i 's/^void/int/g' scipy/optimize/_trlib/trlib/trlib_private.h
+sed -i 's/^void/int/g' scipy/_build_utils/src/wrap_dummy_g77_abi.c
+sed -i 's/, int)/)/g' scipy/optimize/_trlib/trlib_private.h
+sed -i 's/, 1)/)/g' scipy/optimize/_trlib/trlib_private.h
+
+sed -i 's/^void/int/g' scipy/spatial/qhull_misc.h
+sed -i 's/, size_t)/)/g' scipy/spatial/qhull_misc.h
+sed -i 's/,1)/)/g' scipy/spatial/qhull_misc.h
 
 # Input error causes "duplicate symbol" linker errors. Empty out the file.
 echo "" > scipy/sparse/linalg/_dsolve/SuperLU/SRC/input_error.c
@@ -85,6 +86,7 @@ echo "" > scipy/sparse/linalg/_dsolve/SuperLU/SRC/input_error.c
 # meson-python already sets up a -Dbuildtype=release argument to meson, so
 # we need to strip --buildtype out of MESON_ARGS or fail due to redundancy
 #MESON_ARGS_REDUCED="$(echo $MESON_ARGS | sed 's/--buildtype release //g')"
+export MESON_ARGS=
 
 # -wnx flags mean: --wheel --no-isolation --skip-dependency-check
 $PYTHON -m build -w -n -x \
