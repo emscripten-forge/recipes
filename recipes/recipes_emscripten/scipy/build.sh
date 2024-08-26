0167028ac41edc66a238cb1fa5cc166a968ee5bf
KGB99 <k.birgi@gmail.com>
2024-08-26 11:08:27 +0200

    remove sed commands, add debug patch, inc verbosity and more

--- recipes/recipes_emscripten/scipy/build.sh
+++ recipes/recipes_emscripten/scipy/build.sh
@@ -3,6 +3,10 @@
 set -e
 
 mkdir -p build
+
+echo "-------- PRINTING ENVIRONMENT VARIABLES   --------"
+printenv
+echo "-------- DONE PRINTING                    --------"
 # Using flang as a WASM cross-compiler
 # https://github.com/serge-sans-paille/llvm-project/blob/feature/flang-wasm/README.wasm.md
 # https://github.com/conda-forge/flang-feedstock/pull/69
@@ -13,29 +17,7 @@ micromamba install -p $BUILD_PREFIX \
 rm $BUILD_PREFIX/bin/clang # links to clang19
 ln -s $BUILD_PREFIX/bin/clang-18 $BUILD_PREFIX/bin/clang # links to emsdk clang
 
-#export CFLAGS="${CFLAGS} \
-#    -I$PREFIX/include \ 
-#    -Wno-return-type \ 
-#    -DUNDERSCORE_G77 \ 
-#    -fvisibility=default"
 
-#export CXXFLAGS="
-#    -fexceptions \
-#    -fvisibility=default \
-#    "
-
-#export NUMPY_LIB=${BUILD_PREFIX}/lib/python${PYVERSION}/site-packages/numpy
-
-export LDFLAGS="-L$PREFIX/lib"
-
-#export DYLIB_LDFLAGS="-sSIDE_MODULE"
-
-#export BACKEND_FLAGS="
-#    -build-dir=build \
-#    "
-
-#   LIBS        libraries to pass to the linker, e.g. -l<library>
-export LIBS="-lFortranRuntime" # NOTE: Needed for external blas and lapack
 
 #   FC          Fortran compiler command
 export FC=flang-new
@@ -53,55 +35,22 @@ export PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig
 #   PKG_CONFIG_LIBDIR path overriding pkg-config's default search path
 export PKG_CONFIG_LIBDIR=$PREFIX/lib
 
-# Change many functions that return void into functions that return int
-sed -i 's/void DQA/int DQA/g' scipy/integrate/__quadpack.h
-find scipy -name "*.c*" -type f | xargs sed -i 's/extern void F_FUNC/extern int F_FUNC/g'
-
-sed -i 's/void F_FUNC/int F_FUNC/g' scipy/odr/__odrpack.c
-sed -i 's/^void/int/g' scipy/odr/odrpack.h
-sed -i 's/^void/int/g' scipy/odr/__odrpack.c
-
-sed -i 's/void BLAS_FUNC/int BLAS_FUNC/g' scipy/special/lapack_defs.h
-# sed -i 's/void F_FUNC/int F_FUNC/g' scipy/linalg/_lapack_subroutines.h
-sed -i 's/extern void/extern int/g' scipy/optimize/__minpack.h
-sed -i 's/void/int/g' scipy/linalg/cython_blas_signatures.txt
-sed -i 's/void/int/g' scipy/linalg/cython_lapack_signatures.txt
-sed -i 's/^void/int/g' scipy/interpolate/src/_fitpackmodule.c
-
-sed -i 's/extern void/extern int/g' scipy/sparse/linalg/_dsolve/SuperLU/SRC/*.{c,h}
-sed -i 's/PUBLIC void/PUBLIC int/g' scipy/sparse/linalg/_dsolve/SuperLU/SRC/*.{c,h}
-sed -i 's/^void/int/g' scipy/sparse/linalg/_dsolve/SuperLU/SRC/*.{c,h}
-sed -i 's/^void/int/g' scipy/sparse/linalg/_dsolve/*.{c,h}
-sed -i 's/void \(.\)print/int \1/g' scipy/sparse/linalg/_dsolve/SuperLU/SRC/*.{c,h}
-sed -i 's/TYPE_GENERIC_FUNC(\(.*\), void)/TYPE_GENERIC_FUNC(\1, int)/g' scipy/sparse/linalg/_dsolve/_superluobject.h
-
-sed -i 's/^void/int/g' scipy/optimize/_trlib/trlib_private.h
-sed -i 's/^void/int/g' scipy/optimize/_trlib/trlib/trlib_private.h
-sed -i 's/^void/int/g' scipy/_build_utils/src/wrap_dummy_g77_abi.c
-sed -i 's/, int)/)/g' scipy/optimize/_trlib/trlib_private.h
-sed -i 's/, 1)/)/g' scipy/optimize/_trlib/trlib_private.h
-
-sed -i 's/^void/int/g' scipy/spatial/qhull_misc.h
-sed -i 's/, size_t)/)/g' scipy/spatial/qhull_misc.h
-sed -i 's/,1)/)/g' scipy/spatial/qhull_misc.h
-
-# Input error causes "duplicate symbol" linker errors. Empty out the file.
-echo "" > scipy/sparse/linalg/_dsolve/SuperLU/SRC/input_error.c
-
-# meson-python already sets up a -Dbuildtype=release argument to meson, so
-# we need to strip --buildtype out of MESON_ARGS or fail due to redundancy
-#MESON_ARGS_REDUCED="$(echo $MESON_ARGS | sed 's/--buildtype release //g')"
-export MESON_ARGS=
+export MESON_ARGS="--prefix=$PREFIX -Dlibdir=lib"
 
 # -wnx flags mean: --wheel --no-isolation --skip-dependency-check
-$PYTHON -m build -w -n -x \
+cp $RECIPE_DIR/emscripten.meson.cross $SRC_DIR
+echo "python = '${PYTHON}'" >> $SRC_DIR/emscripten.meson.cross
+
+$PYTHON -m build -w -n -x -v \
     -Cbuilddir=build \
     -Cinstall-args=--tags=runtime,python-runtime,devel \
     -Csetup-args=-Dbuildtype=debug \
-    -Csetup-args=-Duse-g77-abi=true \
     -Csetup-args=-Dblas=blas \
     -Csetup-args=-Dlapack=lapack \
     -Csetup-args=-Dfortran_std=none \
-    -Csetup-args="--cross-file=$RECIPE_DIR/emscripten.meson.cross" 
+    -Csetup-args="--cross-file=$SRC_DIR/emscripten.meson.cross" \
+    -Ccompile-args="-j1" \
+    -Ccompile-args="-v" 
+#    -Csetup-args=-Duse-g77-abi=true \
 #    || (cat $BUILD_PREFIX/build/meson-logs/meson-log.txt && exit 1)
 #    -Csetup-args=${MESON_ARGS_REDUCED// / -Csetup-args=} \
