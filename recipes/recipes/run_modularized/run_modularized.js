718d9a2f98006d51eabe00c7691316603408d29a
DerThorsten <derthorstenbeier@gmail.com>
2024-09-10 15:44:34 +0200

    only call exit once

--- recipes/recipes/run_modularized/run_modularized.js
+++ recipes/recipes/run_modularized/run_modularized.js
@@ -2,12 +2,17 @@
 
 const binary_js_runner = process.argv[2];
 process.argv = process.argv.slice(3);
+let already_called = false;
 async function my_main(){
     let exitCode = 126;  // "Command invoked cannot execute"
     const ModuleF = require(binary_js_runner);
     const Module = await ModuleF({
         arguments: process.argv,
         quit: (status, toThrow) => {
+            if (already_called) {
+                return;
+            }
+            already_called = true;
             exitCode = status;
         }
     });
