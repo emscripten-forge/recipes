6b4d117e3eaddc86b2ec4103e5a0e46d22a6ce41
Thorsten Beier <derthorstenbeier@gmail.com>
2024-09-10 15:55:00 +0200

    remove call main and hotfix run_modularized (#1312)
    * Remove use of callMain in lua
    
    * Pass args in run_modularized
    
    * Correct run_modularized pin
    
    * only call exit once
    
    * cleanup
    
    * cleanup
    
    ---------
    
    Co-authored-by: Ian Thomas <ianthomas23@gmail.com>

--- recipes/recipes/run_modularized/run_modularized.js
+++ recipes/recipes/run_modularized/run_modularized.js
@@ -2,14 +2,21 @@
 
 const binary_js_runner = process.argv[2];
 process.argv = process.argv.slice(3);
+let already_called = false;
 async function my_main(){
+    let exitCode = 126;  // "Command invoked cannot execute"
     const ModuleF = require(binary_js_runner);
     const Module = await ModuleF({
-        noInitialRun: true,
-        // arguments: args_without_js_path
+        arguments: process.argv,
+        quit: (status, toThrow) => {
+            if (already_called) {
+                return;
+            }
+            already_called = true;
+            exitCode = status;
+        }
     });
-    const ret = Module.callMain(process.argv);
-    return ret;
+    return exitCode;
 }
 (async function() {
     const exitCode = await my_main();
