07699cf94bd1d2c86b077fdf3afa1bd70bb516e3
Isabel Paredes <isabel.paredes@quantstack.net>
2024-09-17 17:31:14 +0200

    Build with fPIC

--- recipes/recipes_emscripten/lapack/build.sh
+++ recipes/recipes_emscripten/lapack/build.sh
@@ -6,14 +6,14 @@ set -ex
 # https://github.com/serge-sans-paille/llvm-project/blob/feature/flang-wasm/README.wasm.md
 # https://github.com/conda-forge/flang-feedstock/pull/69
 micromamba install -p $BUILD_PREFIX \
-    conda-forge/label/llvm_rc::libllvm19 \
+    conda-forge/label/llvm_rc::libllvm19=19.1.0.rc2 \
     conda-forge/label/llvm_dev::flang=19.1.0.rc2 \
     -y --no-channel-priority
 rm $BUILD_PREFIX/bin/clang # links to clang19
 ln -s $BUILD_PREFIX/bin/clang-18 $BUILD_PREFIX/bin/clang # links to emsdk clang
 
 export FC=flang-new
-export FFLAGS="--target=wasm32-unknown-emscripten"
+export FFLAGS="--target=wasm32-unknown-emscripten -fPIC"
 
 emcmake cmake -S . -B _build \
     -DCMAKE_BUILD_TYPE=Release \
@@ -22,7 +22,7 @@ emcmake cmake -S . -B _build \
     -DBUILD_TESTING=OFF \
     -DCMAKE_Fortran_PREPROCESS=yes \
     -DCMAKE_Fortran_COMPILER=$FC \
-    -DCMAKE_Fortran_FLAGS=$FFLAGS \
+    -DCMAKE_Fortran_FLAGS="$FFLAGS" \
     -DCMAKE_INSTALL_LIBDIR="lib" \
     -DCMAKE_INSTALL_PREFIX=$PREFIX
 
