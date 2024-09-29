f6fda476576185331b00c5691f2ce25fcd90394b
Talmo Pereira <talmo@salk.edu>
2024-09-29 02:58:45 -0700

    Env vars

--- recipes/recipes_emscripten/h5py/build.sh
+++ recipes/recipes_emscripten/h5py/build.sh
@@ -9,4 +9,10 @@ export H5PY_SETUP_REQUIRES="0"
 export H5PY_ROS3="0"
 export H5PY_DIRECT_VFD='0'
 
+# Disable MPI support
+export HDF5_MPI=OFF
+
+# Explitly set HDF5 version
+export HDF5_VERSION=1.12.3
+
 "${PYTHON}" -m pip install . --no-deps --ignore-installed --no-cache-dir -vv
