83caaf3287ffe33f95ac6aa79fb3da36102ce761
Talmo Pereira <talmo@salk.edu>
2024-09-29 03:19:00 -0700

    Try emcc monkey patch

--- recipes/recipes_emscripten/h5py/build.sh
+++ recipes/recipes_emscripten/h5py/build.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# remove the emcc symlink in the $BUILD_PREFIX/bin
+rm $BUILD_PREFIX/bin/emcc
+
+# make callable
+chmod +x $RECIPE_DIR/emcc_wrapper.sh
+
+# create symlink to $RECIPE_DIR/emcc_wrapper.sh 
+ln -s $RECIPE_DIR/emcc_wrapper.sh $BUILD_PREFIX/bin/emcc
+
 export HDF5_DIR=${PREFIX}
 
 # tell setup.py to not 'pip install' exact package requirements
