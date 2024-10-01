817f8b70e62f0c59676a640638fb202dde8cb0b9
Isabel Paredes <isabel.paredes@quantstack.net>
2024-10-01 17:27:14 +0200

    Rebuild r-base64enc

--- recipes/recipes_emscripten/r-base64enc/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-echo "R_HOME=\$(PREFIX)/lib/R" > $BUILD_PREFIX/lib/R/etc/Makeconf
-cat $PREFIX/lib/R/etc/Makeconf >> $BUILD_PREFIX/lib/R/etc/Makeconf
-
-$BUILD_PREFIX/bin/R CMD INSTALL . \
-    --no-test-load --no-byte-compile --library=$PREFIX/lib/R/library
