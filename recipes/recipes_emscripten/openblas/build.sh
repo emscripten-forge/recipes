1cf0d71f39302c749a4d01fdcb2260462502673d
anutosh491 <andersonbhat491@gmail.com>
2024-10-14 12:30:37 +0530

    Attempt to add Openblas

--- recipes/recipes_emscripten/openblas/build.sh
+++ recipes/recipes_emscripten/openblas/build.sh
@@ -2,41 +2,40 @@ chmod u+x c_check
 chmod u+x f_check
 chmod u+x exports/gensymbol
 
+set -ex
 
-SED_CMD="sed"
-if [[ "$OSTYPE" == "darwin"* ]]; then
-    SED_CMD="gsed"
-fi
+# Using flang as a WASM cross-compiler
+# https://github.com/serge-sans-paille/llvm-project/blob/feature/flang-wasm/README.wasm.md
+# https://github.com/conda-forge/flang-feedstock/pull/69
+micromamba install -p $BUILD_PREFIX \
+    conda-forge/label/llvm_rc::libllvm19 \
+    conda-forge/label/llvm_dev::flang=19.1.0.rc2 \
+    -y --no-channel-priority
+rm $BUILD_PREFIX/bin/clang # links to clang19
+ln -s $BUILD_PREFIX/bin/clang-18 $BUILD_PREFIX/bin/clang # links to emsdk clang
 
-# Replace void returns by int returns
-$SED_CMD -ri 's/void(\s+)BLASFUNC/int\1BLASFUNC/g' common_interface.h
-$SED_CMD -ri 's/void(\s+)cblas_/int\1cblas_/g' cblas.h ctest/*.c
-$SED_CMD -ri 's/void(\s+)(C?NAME)/int\1\2/g' interface/*.c
-$SED_CMD -ri 's/((extern)?.+) void ([a-z0-9]+_)/\1\2 int \3/g' lapack-netlib/SRC/*.c \
-    lapack-netlib/SRC/DEPRECATED/*.c
-# For some functions (mostly handling complex I think) f2c actually
-# generate a function that returns void so I need to revert the void to int
-# change the previous line does.
-$SED_CMD -ri 's@int ([cz](dotc|dotu|ladiv))@void \1@g' lapack-netlib/SRC/*.c\
-    lapack-netlib/SRC/DEPRECATED/*.c
+export CC=emcc
+export FC=flang-new
+export FFLAGS="--target=wasm32-unknown-emscripten"
 
-emmake make libs shared CC=emcc HOSTCC=gcc TARGET=RISCV64_GENERIC NOFORTRAN=1 NO_LAPACKE=1 \
-    USE_THREAD=0 LDFLAGS="$EM_FORGE_SIDE_MODULE_LDFLAGS"
-
-# Add libf2c symbols to libopenblas.so
-emcc $PREFIX/lib/libf2c.a libopenblas.a $EM_FORGE_SIDE_MODULE_LDFLAGS \
-    -o libopenblas.so
+emmake make TARGET=GENERIC \
+    CC=$CC \
+    FC=$FC \
+    HOSTCC=clang \
+    USE_THREAD=0 \
+    lib
 
 emmake make install PREFIX=$PREFIX
 
-# make sure to remove all shared libs **that dont link to libf2c**
-rm -rf $PREFIX/lib/libopenblas.*
+mkdir -p $PREFIX/lib
+cp libopenblas.a $PREFIX/lib
 
-# copy the shared lib from the build dir to the install dir
-# (this is the one that links to libf2c)
+# $(which cmake) --build _build
+# $(which cmake) --install _build
 
-cp libopenblas.so $PREFIX/lib/
-
-
-# fix openblas.pc replace "extralib=-lpthread -lgfortran -lgfortran" with "extralib="
-$SED_CMD -i 's/extralibs=-lpthread -lgfortran -lgfortran/extralibs=/' $PREFIX/lib/pkgconfig/openblas.pc
+# PYODIDE_PACKED=$RECIPE_DIR/openblas-0.3.23.zip
+# # unzip
+# unzip $PYODIDE_PACKED
+# # copy libopenblas.so to
+# mkdir -p $PREFIX/lib
+# cp libopenblas.a $PREFIX/lib
