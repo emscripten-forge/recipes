6b8bdace7b036db69047e0416cf138e779cd629e
anutosh491 <andersonbhat491@gmail.com>
2024-10-14 12:30:37 +0530

    Attempt to add Openblas

--- recipes/recipes_emscripten/openblas/build.sh
+++ recipes/recipes_emscripten/openblas/build.sh
@@ -1,8 +1,39 @@
 #!/bin/bash
 
-PYODIDE_PACKED=$RECIPE_DIR/openblas-0.3.23.zip
-# unzip 
-unzip $PYODIDE_PACKED
-# copy libopenblas.so to 
+set -ex
+
+# Using flang as a WASM cross-compiler
+# https://github.com/serge-sans-paille/llvm-project/blob/feature/flang-wasm/README.wasm.md
+# https://github.com/conda-forge/flang-feedstock/pull/69
+micromamba install -p $BUILD_PREFIX \
+    conda-forge/label/llvm_rc::libllvm19 \
+    conda-forge/label/llvm_dev::flang=19.1.0.rc2 \
+    -y --no-channel-priority
+rm $BUILD_PREFIX/bin/clang # links to clang19
+ln -s $BUILD_PREFIX/bin/clang-18 $BUILD_PREFIX/bin/clang # links to emsdk clang
+
+export CC=emcc
+export FC=flang-new
+export FFLAGS="--target=wasm32-unknown-emscripten"
+
+emmake make TARGET=GENERIC \
+    CC=$CC \
+    FC=$FC \
+    HOSTCC=clang \
+    USE_THREAD=0 \
+    lib
+
+emmake make install PREFIX=$PREFIX
+
 mkdir -p $PREFIX/lib
-cp libopenblas.so $PREFIX/lib
+cp libopenblas.a $PREFIX/lib
+
+# $(which cmake) --build _build
+# $(which cmake) --install _build
+
+# PYODIDE_PACKED=$RECIPE_DIR/openblas-0.3.23.zip
+# # unzip
+# unzip $PYODIDE_PACKED
+# # copy libopenblas.so to
+# mkdir -p $PREFIX/lib
+# cp libopenblas.a $PREFIX/lib
