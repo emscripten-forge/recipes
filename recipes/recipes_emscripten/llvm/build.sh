16608ed1f6cd3e57a66c0e0e07c9c8164a1377a8
anutosh491 <andersonbhat491@gmail.com>
2024-10-21 12:04:27 +0530

    try building without flag

--- recipes/recipes_emscripten/llvm/build.sh
+++ recipes/recipes_emscripten/llvm/build.sh
@@ -26,7 +26,7 @@ emcmake cmake ${CMAKE_ARGS} -S ../llvm -B .         \
     -DCMAKE_CXX_FLAGS="-isystem $EMSCRIPTEN_FORGE_EMSDK_DIR/upstream/emscripten/cache/sysroot/include/c++/v1"
 
 # Build step
-EMCC_CFLAGS='-sERROR_ON_UNDEFINED_SYMBOLS=0' emmake make -j1
+emmake make -j1
 
 # Install step
 emmake make install
