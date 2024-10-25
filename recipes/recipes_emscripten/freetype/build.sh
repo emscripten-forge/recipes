8a66181078e2e0213f6fd9789d463190b67f175f
anutosh491 <andersonbhat491@gmail.com>
2024-10-25 14:51:05 +0530

    Update freetype recipe

--- recipes/recipes_emscripten/freetype/build.sh
+++ recipes/recipes_emscripten/freetype/build.sh
@@ -1,5 +1,9 @@
 mkdir build && cd build
 
-emcmake cmake .. $CMAKE_ARGS
+# NOTE: freetype needs to be compiled with atomics/bulk-memory features
+# for use cases like cairo, pango etc.
+emcmake cmake .. ${CMAKE_ARGS} \
+    -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+    -DCMAKE_C_FLAGS="-matomics -mbulk-memory"
 
 make install -j${CPU_COUNT}
