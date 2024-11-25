1692bf086fd2981b09e40bb936679153b46b79f4
Isabel Paredes <isabel.paredes@quantstack.net>
2024-11-25 14:41:59 +0100

    Update r-base (#1425)
    * Bump version
    
    * Remove r-base dependency in build env
    
    * Add dependencies for native build
    
    * Update patches
    
    * Update pre.js
    
    * Update build instructions
    
    * Clean up
    
    * Bump build number

--- recipes/recipes_emscripten/r-base/build.sh
+++ recipes/recipes_emscripten/r-base/build.sh
@@ -25,59 +25,93 @@ export ac_cv_have_decl_getrlimit=no
 export ac_cv_have_decl_sigaltstack=no
 export ac_cv_have_decl_wcsftime=no
 export ac_cv_have_decl_umask=no
+export ac_cv_have_decl_sched_getaffinity=no
+export ac_cv_have_decl_sched_setaffinity=no
 
 # SIDE_MODULE + pthread is experimental, and pthread_kill is not implemented
 export r_cv_search_pthread_kill=no
 
-export OBJDUMP=llvm-objdump
-
 # Otherwise set to .not_implemented and cannot be used
 export SHLIB_EXT=".so"
 
-mkdir _build
-cp $RECIPE_DIR/config.site _build/config.site
-cd _build
+export CONFIG_ARGS="--enable-R-static-lib \
+--without-readline  \
+--without-x \
+--enable-java=no \
+--enable-R-profiling=no \
+--enable-byte-compiled-packages=no \
+--disable-rpath \
+--disable-openmp \
+--disable-nls \
+--with-internal-tzcode \
+--with-libdeflate-compression=no \
+--with-recommended-packages=no"
 
-# NOTE: the host and build systems are explicitly set to enable the cross-
-# compiling options even though it's not fully supported.
-# Otherwise, it assumes it's not cross-compiling.
-emconfigure ../configure \
-    --prefix=$PREFIX    \
-    --build="x86_64-conda-linux-gnu" \
-    --host="wasm32-unknown-emscripten" \
-    --enable-R-static-lib \
-    --without-readline  \
-    --without-x         \
-    --enable-java=no \
-    --enable-R-profiling=no \
-    --enable-byte-compiled-packages=no \
-    --disable-rpath \
-    --disable-openmp \
-    --disable-nls \
-    --with-internal-tzcode \
-    --with-recommended-packages=no \
-|| cat config.log
+#-------------------------------------------------------------------------------
+# LINUX BUILD
+#-------------------------------------------------------------------------------
+# Building R for Linux so that we can use the R and Rscript binaries to build
+# the R internal modules for WebAssembly.
 
-# NOTE: Remove the -lFortranRuntime from the FLIBS to avoid double-linking
-# when creating the R binary
-echo "FLIBS =" >> Makeconf
+mkdir -p _build_linux
+pushd _build_linux
+(
+    # export PREFIX=$BUILD_PREFIX
+    export CC=gcc
+    export CXX=g++
+    export FC=flang-new
+    export CPPFLAGS="-I$BUILD_PREFIX/include"
+    export LDFLAGS="-L$BUILD_PREFIX/lib"
+    export FC_LEN_T=size_t
 
-emmake make -j${CPU_COUNT}
-emmake make install
+    unset CROSS_COMPILING
 
-# FIXME: The database files for the internal modules are installed in a "help"
-# directory, this copies them to the expected location. It also helps avoid
-# packaging r-base files in other R packages when using cross-r-base.
-pushd $PREFIX/lib/R/library
-    for pkg in $(ls); do
-        if [ "$pkg" == "datasets" ]; then
-            cp -n ${BUILD_PREFIX}/lib/R/library/$pkg/data/* $pkg/data/
-        elif [ -d $pkg/help ]; then
-            cp -n $pkg/help/$pkg.rd* $pkg/R/
-        fi
-    done
+    ../configure \
+        --prefix=$BUILD_PREFIX \
+        $CONFIG_ARGS
+
+    make -j${CPU_COUNT}
+    # No need to install, we just need the R binary
+)
 popd
 
-# Manually copying .wasm files
-cp src/main/R.* $PREFIX/lib/R/bin/exec/
-cp src/unix/Rscript.wasm $PREFIX/lib/R/bin/
+#-------------------------------------------------------------------------------
+# WASM BUILD
+#-------------------------------------------------------------------------------
+# Building R for WebAssembly using the R binary from the Linux build.
+
+# libz.so has an invalid ELF header which causes an error when looking for
+# opendir during the configure step. Link with libz.a instead.
+rm $PREFIX/lib/libz.so* || true
+
+mkdir -p _build_wasm
+pushd _build_wasm
+(
+    cp $RECIPE_DIR/config.site .
+
+    export CROSS_COMPILING="true"
+    export R_EXECUTABLE=$(realpath ..)/_build_linux/bin/exec/R # binary not shell wrapper
+    export R_SCRIPT_EXECUTABLE=$(realpath ..)/_build_linux/bin/Rscript
+    export LINUX_BUILD_DIR=$(realpath ..)/_build_linux
+
+    # NOTE: the host and build systems are explicitly set to enable the cross-
+    # compiling options even though it's not fully supported.
+    # Otherwise, it assumes it's not cross-compiling.
+    emconfigure ../configure \
+        --prefix=$PREFIX    \
+        --build="x86_64-conda-linux-gnu" \
+        --host="wasm32-unknown-emscripten" \
+        $CONFIG_ARGS
+
+    # NOTE: Remove the -lFortranRuntime from the FLIBS to avoid double-linking
+    # when creating the R binary
+    echo "FLIBS =" >> Makeconf
+
+    emmake make -j${CPU_COUNT}
+
+    $RECIPE_DIR/cross_libraries.sh --restore $(pwd)
+
+    emmake make install
+
+)
+popd
