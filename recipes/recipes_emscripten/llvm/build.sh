a5b07ca89e655bc2f5a711d32e4967d260002f0c
anutosh491 <andersonbhat491@gmail.com>
2024-12-03 17:35:05 +0530

    update to llvm 19.1.5

--- recipes/recipes_emscripten/llvm/build.sh
+++ recipes/recipes_emscripten/llvm/build.sh
@@ -26,7 +26,6 @@ cmake ${CMAKE_ARGS} -S ../llvm -B .                 \
     -DLLVM_ENABLE_LIBXML2=OFF                       \
     -DCLANG_ENABLE_STATIC_ANALYZER=OFF              \
     -DCLANG_ENABLE_ARCMT=OFF                        \
-    -DCLANG_ENABLE_FORMAT=OFF                       \
     -DCLANG_ENABLE_BOOTSTRAP=OFF                    \
     -DCMAKE_CXX_FLAGS="-Dwait4=__syscall_wait4"
 
