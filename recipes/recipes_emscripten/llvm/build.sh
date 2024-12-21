437498709be14f4ab39545d1d70ec61934aefa5f
Anutosh Bhat <andersonbhat491@gmail.com>
2024-12-21 22:42:45 +0530

    Add patch on llvm 19.1.6 to take care of temporary files generated at runtime (#1476)
    * Add patch on llvm 19.1.6 to take care of temporary files generated at runtime
    
    * changes as per Martin's Suggestion

--- recipes/recipes_emscripten/llvm/build.sh
+++ recipes/recipes_emscripten/llvm/build.sh
@@ -7,20 +7,19 @@ export CMAKE_SYSTEM_PREFIX_PATH=$PREFIX
 # clear LDFLAGS flags because they contain sWASM_BIGINT
 export LDFLAGS=""
 
-
 # Configure step
-cmake ${CMAKE_ARGS} -S ../llvm -B .                 \
+emcmake cmake ${CMAKE_ARGS} -S ../llvm -B .         \
     -DCMAKE_BUILD_TYPE=MinSizeRel                   \
     -DCMAKE_PREFIX_PATH=$PREFIX                     \
     -DCMAKE_INSTALL_PREFIX=$PREFIX                  \
     -DLLVM_HOST_TRIPLE=wasm32-unknown-emscripten    \
     -DLLVM_TARGETS_TO_BUILD="WebAssembly"           \
+    -DLLVM_ENABLE_ASSERTIONS=ON                     \
     -DLLVM_INCLUDE_BENCHMARKS=OFF                   \
     -DLLVM_INCLUDE_EXAMPLES=OFF                     \
     -DLLVM_INCLUDE_TESTS=OFF                        \
     -DLLVM_ENABLE_LIBEDIT=OFF                       \
     -DLLVM_ENABLE_PROJECTS="clang;lld"              \
-    -DCMAKE_VERBOSE_MAKEFILE=ON                     \
     -DLLVM_ENABLE_THREADS=OFF                       \
     -DLLVM_ENABLE_ZSTD=OFF                          \
     -DLLVM_ENABLE_LIBXML2=OFF                       \
@@ -30,10 +29,10 @@ cmake ${CMAKE_ARGS} -S ../llvm -B .                 \
     -DCMAKE_CXX_FLAGS="-Dwait4=__syscall_wait4"
 
 # Build step
-make -j4
+emmake make -j4
 
 # Install step
-make install
+emmake make install
 
 # Copy all files with ".wasm" extension to $PREFIX/bin
 cp $SRC_DIR/build/bin/*.wasm $PREFIX/bin
