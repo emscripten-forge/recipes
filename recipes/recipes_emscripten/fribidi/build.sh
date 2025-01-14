fb2010665ae780dcf923722713447e176e117e44
Isabel Paredes <isabel.paredes@quantstack.net>
2025-01-14 12:20:43 +0100

    Update fribidi

--- recipes/recipes_emscripten/fribidi/build.sh
+++ recipes/recipes_emscripten/fribidi/build.sh
@@ -1,8 +1,21 @@
+#!/bin/bash
+
+set -ex
+
 export CFLAGS="$CFLAGS -fPIC"
+export LDFLAGS="$LDFLAGS -sALLOW_MEMORY_GROWTH=1 -sSTACK_SIZE=1MB"
 
-emconfigure ./configure \
-    --enable-shared=no \
-    --enable-static=yes \
-    --prefix=$PREFIX
+meson_setup_args=(
+    -Ddocs=false
+    -Dtests=false
+    -Ddefault_library=static
+)
 
-emmake make install
+meson setup builddir \
+    ${meson_setup_args[@]} \
+    --prefix=$PREFIX \
+    --buildtype=release \
+    --prefer-static \
+    --cross-file=$RECIPE_DIR/emscripten.meson.cross
+
+meson install -C builddir
