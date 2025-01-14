b6443afe17f64916375f14b6b7309ee0c7e53eb2
Thorsten Beier <derthorstenbeier@gmail.com>
2025-01-14 13:54:26 +0100

    Update build.sh

--- recipes/recipes_emscripten/glpk/build.sh
+++ recipes/recipes_emscripten/glpk/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-CFLAGS="-fPIC" emconfigure ./configure  --prefix=$PREFIX --host ""
+CFLAGS="-fPIC" emconfigure ./configure  --prefix=$PREFIX --host emscripten-wasm32
 emmake make -j ${CPU_COUNT:-3} install
