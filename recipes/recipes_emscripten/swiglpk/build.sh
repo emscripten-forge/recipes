50c4ce22e96d206e83d591d0783caed0e4356088
Thorsten Beier <derthorstenbeier@gmail.com>
2025-01-16 08:41:58 +0100

    Update build.sh

--- recipes/recipes_emscripten/swiglpk/build.sh
+++ recipes/recipes_emscripten/swiglpk/build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-export LDFLAGS="$LDFLAGS -L$PREFIX/lib" 
+export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
+export CPPFLAGS="-I$PREFIX/include"
+export CFLAGS="-I$PREFIX/include"
+export GLPK_HEADER_PATH="$PREFIX/include"
 ${PYTHON} -m pip install .
 
