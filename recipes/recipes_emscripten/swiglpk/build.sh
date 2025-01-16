1bae1fa254cfac497041ff0d10433b8dfa99ceab
Thorsten Beier <derthorstenbeier@gmail.com>
2025-01-16 08:48:33 +0100

    Update build.sh

--- recipes/recipes_emscripten/swiglpk/build.sh
+++ recipes/recipes_emscripten/swiglpk/build.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
-export CPPFLAGS="-I$PREFIX/include"
-export CFLAGS="-I$PREFIX/include"
+export CPPFLAGS="$EM_FORGE_SIDE_MODULE_CFLAGS -I$PREFIX/include"
+export CFLAGS=$CPPFLAGS
 export GLPK_HEADER_PATH="$PREFIX/include"
+export LDFLAGS=$EM_FORGE_SIDE_MODULE_LDFLAGS
+
+    
 ${PYTHON} -m pip install .
 
