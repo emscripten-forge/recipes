591aed405ab948ef5b2d32d1c5870c937e71fd6a
Thorsten Beier <derthorstenbeier@gmail.com>
2025-01-16 08:52:34 +0100

    Trigger build for swig-glpk [3.1.73] (#1640)
    * Trigger build for swig-glpk [3.1.73]
    
    * Update build.sh
    
    * Update build.sh
    
    * Update build.sh

--- recipes/recipes_emscripten/swiglpk/build.sh
+++ recipes/recipes_emscripten/swiglpk/build.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-export LDFLAGS="$LDFLAGS -L$PREFIX/lib" 
+export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
+export CPPFLAGS="$EM_FORGE_SIDE_MODULE_CFLAGS -I$PREFIX/include"
+export CFLAGS=$CPPFLAGS
+export GLPK_HEADER_PATH="$PREFIX/include"
+export LDFLAGS="$EM_FORGE_SIDE_MODULE_LDFLAGS -L$PREFIX/lib"
+
+    
 ${PYTHON} -m pip install .
 
