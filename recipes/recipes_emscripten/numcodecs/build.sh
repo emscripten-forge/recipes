bd218fa2bfb54aeb1db56e962bcd7c81f726a7f7
Thorsten Beier <derthorstenbeier@gmail.com>
2025-01-17 14:24:44 +0100

    Update build.sh

--- recipes/recipes_emscripten/numcodecs/build.sh
+++ recipes/recipes_emscripten/numcodecs/build.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
+
+# Note: the include folder is moved to `$BUILD_PREFIX` by the cross-python activation script
+export NUMPY_INCLUDE_DIR="$BUILD_PREFIX/lib/python${PY_VER}/site-packages/numpy/_core/include"
+
+
+export CFLAGS="$CFLAGS -I$NUMPY_INCLUDE_DIR"
 ${PYTHON} -m pip install .
