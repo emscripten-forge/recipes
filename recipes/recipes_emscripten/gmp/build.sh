2e977f8eafbfa5e8670d057dd7ee8e73af87d923
DerThorsten <derthorstenbeier@gmail.com>
2025-01-17 07:24:41 +0100

    node

--- recipes/recipes_emscripten/gmp/build.sh
+++ recipes/recipes_emscripten/gmp/build.sh
@@ -1,3 +1,27 @@
+echo "Building GMP"
+
+
+
+filename=Makefile.in
+
+# Check the operating system
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    # macOS
+    sed -i '' 's#\./gen-#node ./gen-#g' "$filename"
+elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    # Linux
+    sed -i 's#\./gen-#node ./gen-#g' "$filename"
+else
+    echo "Unsupported OS: $OSTYPE"
+    exit 1
+fi
+
+
+
+
+
+
+echo "Building GMP Step 1"
 CC_FOR_BUILD=emcc CPP_FOR_BUILD=emcc HOST_CC=emcc  emconfigure ./configure \
     CFLAGS="$CFLAGS -fPIC" \
     --prefix=${PREFIX}  \
