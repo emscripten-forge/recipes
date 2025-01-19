c5f69874b4d5f0e62ae0bf36e4639d525b23d240
Tobias Fischer <info@tobiasfischer.info>
2025-01-19 14:13:02 +1000

    Fixes for main branch

--- recipes/recipes_emscripten/yaml-cpp/build.sh
+++ recipes/recipes_emscripten/yaml-cpp/build.sh
@@ -3,14 +3,18 @@
 mkdir build
 cd build
 
+export CMAKE_ARGS="$CMAKE_ARGS -DCMAKE_PROJECT_INCLUDE=${RECIPE_DIR}/overwriteProp.cmake"
+
 # Configure step
-cmake .. \
+cmake $CMAKE_ARGS \
     -GNinja \
-    -DYAML_BUILD_SHARED_LIBS=OFF \
+    -DYAML_BUILD_SHARED_LIBS=ON \
+    -DYAML_CPP_BUILD_TOOLS=OFF \
     -DYAML_CPP_BUILD_TESTS=OFF \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_PREFIX_PATH=$PREFIX \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
+    $SRC_DIR
 
 # Build step
 ninja install
--- recipes/recipes_emscripten/yaml/build.sh
+++ recipes/recipes_emscripten/yaml/build.sh
@@ -1,11 +1,13 @@
-#!/bin/bash
+mkdir build && cd build
 
-CFLAGS="-fPIC"
+export CMAKE_ARGS="$CMAKE_ARGS -DCMAKE_PROJECT_INCLUDE=${RECIPE_DIR}/overwriteProp.cmake"
 
-# Configure step
-emconfigure ./configure --prefix=${PREFIX} \
-            --enable-static     \
-            --disable-shared
+cmake $CMAKE_ARGS -DCMAKE_BUILD_TYPE=Release \
+      -DCMAKE_PREFIX_PATH=$PREFIX \
+      -DCMAKE_INSTALL_PREFIX=$PREFIX \
+      -DBUILD_SHARED_LIBS=ON \
+      -DCMAKE_INSTALL_LIBDIR=lib \
+      $SRC_DIR
 
-make -j${CPU_COUNT} ${VERBOSE_AT}
+make VERBOSE=1 -j${CPU_COUNT}
 make install
