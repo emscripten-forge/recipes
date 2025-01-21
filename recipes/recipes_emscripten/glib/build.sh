2b42a5b246d9dd9db8eff16a4e981929939edf4c
Johan Mabille <johan.mabille@gmail.com>
2025-01-21 14:35:41 +0100

    Builds glib without thread support (#1735)

--- recipes/recipes_emscripten/glib/build.sh
+++ recipes/recipes_emscripten/glib/build.sh
@@ -2,10 +2,6 @@
 
 # From https://gist.github.com/kleisauke/acfa1c09522705efa5eb0541d2d00887
 
-# Propagate -pthread into CFLAGS to ensure GObject/GIO is compiled with the
-# atomics/bulk-memory features
-export CFLAGS="$CFLAGS -pthread -matomics -mbulk-memory"
-
 meson_config_args=(
     -Dintrospection=disabled
     -Dselinux=disabled
