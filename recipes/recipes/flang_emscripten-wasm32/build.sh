c3beb6f03be3612ccd44c3abbadb32aec6d2e6d0
Thorsten Beier <derthorstenbeier@gmail.com>
2025-01-22 11:26:31 +0100

    disable tests

--- recipes/recipes/flang_emscripten-wasm32/build.sh
+++ recipes/recipes/flang_emscripten-wasm32/build.sh
@@ -7,6 +7,8 @@ cmake -S llvm -B _fbuild -GNinja \
         -DCMAKE_INSTALL_PREFIX=$PREFIX \
         -DLLVM_DEFAULT_TARGET_TRIPLE=wasm32-unknown-emscripten \
         -DLLVM_TARGETS_TO_BUILD=WebAssembly \
-        -DLLVM_ENABLE_PROJECTS="clang;flang;mlir"
+        -DLLVM_ENABLE_PROJECTS="clang;flang;mlir" \
+        -DLLVM_INCLUDE_TESTS=OFF \
+        -DLLVM_BUILD_TESTS=OFF
 cmake --build _fbuild -j 1
 cmake --build _fbuild --target install
