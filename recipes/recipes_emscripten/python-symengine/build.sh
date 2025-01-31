4f275768fbf0c6a8c1fa76d595a5cae3afa2feb2
Thorsten Beier <derthorstenbeier@gmail.com>
2025-01-31 10:54:03 +0100

    Update build.sh

--- recipes/recipes_emscripten/python-symengine/build.sh
+++ recipes/recipes_emscripten/python-symengine/build.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+
+export CXXFLAGS="--std=c++17"
+export CMAKE_CXX_STANDARD=17
+
 export LDFLAGS="-s MODULARIZE=1  -s LINKABLE=1  -s EXPORT_ALL=1  -s WASM=1  -std=c++14  -s LZ4=1 -s SIDE_MODULE=1 -sWASM_BIGINT"
 ${PYTHON} -m pip install .
