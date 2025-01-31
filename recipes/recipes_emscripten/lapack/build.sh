3c112352e9e296843dadaaeb1bc5d79a9b9a5666
Thorsten Beier <derthorstenbeier@gmail.com>
2025-01-31 16:08:06 +0100

    Update build.sh

--- recipes/recipes_emscripten/lapack/build.sh
+++ recipes/recipes_emscripten/lapack/build.sh
@@ -5,10 +5,16 @@ set -ex
 # Using flang as a WASM cross-compiler
 # https://github.com/serge-sans-paille/llvm-project/blob/feature/flang-wasm/README.wasm.md
 # https://github.com/conda-forge/flang-feedstock/pull/69
-micromamba install -p $BUILD_PREFIX \
-    conda-forge/label/llvm_rc::libllvm19=19.1.0.rc2 \
-    conda-forge/label/llvm_dev::flang=19.1.0.rc2 \
-    -y --no-channel-priority
+
+for i in {1..4}; do
+    micromamba install -p $BUILD_PREFIX \
+        conda-forge/label/llvm_rc::libllvm19=19.1.0.rc2 \
+        conda-forge/label/llvm_dev::flang=19.1.0.rc2 \
+        -y --no-channel-priority && break
+    echo "Retry $i failed, retrying..."
+    sleep 5  # Wait for 5 seconds before retrying
+done
+
 rm $BUILD_PREFIX/bin/clang # links to clang19
 ln -s $BUILD_PREFIX/bin/clang-20 $BUILD_PREFIX/bin/clang # links to emsdk clang
 
