fce82eb2427a5a6a98f0d4def9745895fc94ee5f
Anutosh Bhat <andersonbhat491@gmail.com>
2025-02-03 13:12:02 +0530

    Update build.sh

--- recipes/recipes_emscripten/sparrow/build.sh
+++ recipes/recipes_emscripten/sparrow/build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+export CXXFLAGS="${CXXFLAGS} -fPIC"
+export CFLAGS="${CFLAGS} -fPIC"
+
 # Create build directory
 mkdir -p build
 cd build
@@ -8,6 +11,7 @@ cd build
 # Configure the build with CMake
 emcmake cmake .. \
   -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_PREFIX_PATH=$PREFIX \
   -DCMAKE_INSTALL_PREFIX=$PREFIX \
   -DBUILD_EXAMPLES=OFF \
   -DBUILD_TESTS=OFF \
