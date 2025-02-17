6222bc2bbb45b009cf40a993d68904316a276417
Thorsten Beier <derthorstenbeier@gmail.com>
2025-02-17 10:56:22 +0100

    Update build.sh

--- recipes/recipes_emscripten/m4/build.sh
+++ recipes/recipes_emscripten/m4/build.sh
@@ -1,6 +1,6 @@
 # Get an updated config.sub and config.guess
 cp $BUILD_PREFIX/share/libtool/build-aux/config.* build-aux/
 
-./configure --prefix=${PREFIX} --host=${HOST}
+./configure --prefix=${PREFIX} --host=${HOST}  ac_cv_have_decl_alarm=no gl_cv_func_sleep_works=yes
 make -j${CPU_COUNT}
 make install
