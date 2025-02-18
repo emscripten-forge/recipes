570005b9119419151791ffeb379aff5f2008f7dc
DerThorsten <derthorstenbeier@gmail.com>
2025-02-18 12:37:06 +0100

    libraw

--- recipes/recipes_emscripten/libraw/build.sh
+++ recipes/recipes_emscripten/libraw/build.sh
@@ -2,9 +2,16 @@
 
 cd libraw
 
+
 autoreconf --install
 
+CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include" 
+LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+
+
 ./configure --prefix="${PREFIX}" --enable-openmp=no
+
+
 
 make -j${CPU_COUNT}
 make install
