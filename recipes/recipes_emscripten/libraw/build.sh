28cb7fe429390dff23172f216911f6d1ce383474
DerThorsten <derthorstenbeier@gmail.com>
2025-02-18 12:49:28 +0100

    play stupid games

--- recipes/recipes_emscripten/libraw/build.sh
+++ recipes/recipes_emscripten/libraw/build.sh
@@ -3,10 +3,20 @@
 cd libraw
 
 
+# this is all so stupid..why does it not zinf fking zlib
+cp ${PREFIX}/lib/libz.a .
+cp ${PREFIX}/include/zlib.h .
+cp ${PREFIX}/include/zconf.h .
+
+
 autoreconf --install
 
 CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include" 
 LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+
+
+
+
 
 
 ./configure --prefix="${PREFIX}" --enable-openmp=no
