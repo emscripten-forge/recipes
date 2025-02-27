bbe56fd1d166898316b43bf1dd1b17bac46a77c4
Isabel Paredes <isabel.paredes@quantstack.net>
2025-02-27 16:24:19 +0100

    Populate shared libs for lazy loading (#1938)

--- recipes/recipes/cross-r-base_emscripten-wasm32/activate-cross-r-base.sh
+++ recipes/recipes/cross-r-base_emscripten-wasm32/activate-cross-r-base.sh
@@ -5,3 +5,11 @@ cat "${PREFIX}/lib/R/etc/Makeconf" >> "${BUILD_PREFIX}/lib/R/etc/Makeconf"
 
 export R="${BUILD_PREFIX}/bin/R"
 export R_ARGS="--no-byte-compile --no-test-load --library=$PREFIX/lib/R/library"
+
+# Populate shared libraries to enable lazy loading
+find ${BUILD_PREFIX}/lib/R/library -path "*/libs/*.so" | while read -r so_file; do
+    relative_path="${so_file#${BUILD_PREFIX}/}"
+    if [ -f "${PREFIX}/${relative_path}" ]; then
+        cp ${so_file} ${PREFIX}/${relative_path}
+    fi
+done
