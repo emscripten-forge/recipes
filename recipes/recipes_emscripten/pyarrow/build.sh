4512cfbaa594344c9bc5c2ff1244fb4cdedd99b4
DerThorsten <derthorstenbeier@gmail.com>
2025-02-28 10:55:28 +0100

    sysconfig

--- recipes/recipes_emscripten/pyarrow/build.sh
+++ recipes/recipes_emscripten/pyarrow/build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 
-cp ${RECIPE_DIR}/setup.py ${SRC_DIR}/python/
-cp ${RECIPE_DIR}/CMakeLists.txt ${SRC_DIR}/python/
-cp ${RECIPE_DIR}/FindPythonLibsNew.cmake ${SRC_DIR}/cpp/cmake_modules/
-cp ${RECIPE_DIR}/FindPython3Alt.cmake ${SRC_DIR}/cpp/cmake_modules/
-cp ${RECIPE_DIR}/SetupCxxFlags.cmake ${SRC_DIR}/cpp/cmake_modules/
+cp RECIPE_DIR/setup.py $SRC_DIR/python/
+cp RECIPE_DIR/CMakeLists.txt $SRC_DIR/python/
+cp $RECIPE_DIR/FindPythonLibsNew.cmake $SRC_DIR/cpp/cmake_modules/
+cp $RECIPE_DIR/FindPython3Alt.cmake $SRC_DIR/cpp/cmake_modules/
+cp $RECIPE_DIR/SetupCxxFlags.cmake $SRC_DIR/cpp/cmake_modules/
 
 rm  -rf $PREFIX/bin/python*
 
