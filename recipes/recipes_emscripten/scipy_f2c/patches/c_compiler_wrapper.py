b7b5bdcaeb0798e106f0eaa6d17ebb2f45bc8979
DerThorsten <derthorstenbeier@gmail.com>
2025-03-13 10:25:06 +0100

    wip

--- recipes/recipes_emscripten/scipy_f2c/patches/c_compiler_wrapper.py
+++ recipes/recipes_emscripten/scipy_f2c/patches/c_compiler_wrapper.py
@@ -5,14 +5,14 @@ import sys
 import re
 from pathlib import Path
 
-def scipy_fix_cfile(path: str) -> None:
+
+def scipy_fix_cfile(path: Path) -> None:
     """
     Replace void return types with int return types in various generated .c and
     .h files. We can't achieve this with a simple patch because these files are
     not in the sdist, they are generated as part of the build.
     """
-    source_path = Path(path)
-    text = source_path.read_text()
+    text = path.read_text()
     text = text.replace("extern void F_WRAPPEDFUNC", "extern int F_WRAPPEDFUNC")
     text = text.replace("extern void F_FUNC", "extern int F_FUNC")
     text = text.replace("void (*f2py_func)", "int (*f2py_func)")
@@ -21,24 +21,40 @@ def scipy_fix_cfile(path: str) -> None:
     text = text.replace("void(*)", "int(*)")
     text = text.replace("static void f2py_setup_", "static int f2py_setup_")
 
-    if path.endswith("_flapackmodule.c"):
+    if path.name.endswith("_flapackmodule.c"):
         text = text.replace(",size_t", "")
         text = re.sub(r",slen\([a-z]*\)\)", ")", text)
 
-    if path.endswith("_fblasmodule.c"):
-        text = text.replace(" float (*f2py_func)", " double (*f2py_func)")
-
-    source_path.write_text(text)
+    path.write_text(text)
 
     for lib in ["lapack", "blas"]:
-        if path.endswith(f"cython_{lib}.c"):
-            header_path = Path(path).with_name(f"_{lib}_subroutines.h")
+        if path.name.endswith(f"cython_{lib}.c"):
+            header_name = f"_{lib}_subroutines.h"
+            header_dir = path.parent
+            header_path = find_header(header_dir, header_name)
+
             header_text = header_path.read_text()
             header_text = header_text.replace("void F_FUNC", "int F_FUNC")
             header_path.write_text(header_text)
 
 
+def find_header(source_dir: Path, header_name: str) -> Path:
+    """
+    Find the header file that corresponds to a source file.
+    """
+    while not (header_path := source_dir / header_name).exists():
+        # meson copies the source files into a subdirectory of the build
+        source_dir = source_dir.parent
+        if source_dir == Path("/"):
+            raise RuntimeError(f"Could not find header file {header_name}")
+
+    return header_path
+
+
+
+
+
 def scipy_fixes(args: list[str]) -> None:
     for arg in args:
         if arg.endswith(".c"):
-            scipy_fix_cfile(arg)
+            scipy_fix_cfile(Path(arg))
