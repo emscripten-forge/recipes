5ce3e2661373f4d1249c90e374328b52bfdefdd4
Isabel Paredes <isabel.paredes@quantstack.net>
2025-03-20 13:52:00 +0100

    Pass all the flags

--- recipes/recipes_emscripten/libflang/build.sh
+++ recipes/recipes_emscripten/libflang/build.sh
@@ -2,16 +2,26 @@
 
 set -ex
 
-export CFLAGS="-fPIC"
-export CXXFLAGS="-fPIC"
+export CFLAGS="-fPIC $EM_FORGE_SIDE_MODULE_LDFLAGS"
+export CXXFLAGS="-fPIC $EM_FORGE_SIDE_MODULE_LDFLAGS"
+export LDFLAGS="$EM_FORGE_SIDE_MODULE_LDFLAGS"
+
+export AR="emar"
+export ARFLAGS="$EM_FORGE_SIDE_MODULE_LDFLAGS"
+export ARCH="emar"
 
 # See https://github.com/serge-sans-paille/llvm-project/blob/feature/flang-wasm/README.wasm.md
 emcmake cmake -S flang/runtime -B _fbuild_runtime -GNinja \
                 -DCMAKE_BUILD_TYPE=Release \
-                -DCMAKE_INSTALL_PREFIX=$PREFIX
+                -DCMAKE_INSTALL_PREFIX=$PREFIX \
+                -DCMAKE_C_FLAGS="$CFLAGS" \
+                -DCMAKE_CXX_FLAGS="$CXXFLAGS" \
+                -DCMAKE_ARFLAGS="$ARFLAGS" \
+                -DCMAKE_EXE_LINKER_FLAGS="$LDFLAGS" \
+                -DBUILD_SHARED_LIBS=ON
 
 cd _fbuild_runtime
 ninja -v
 ninja install
 
-emcc $PREFIX/lib/libFortranRuntime.a -o tst.so -sSIDE_MODULE=1 
+emcc $PREFIX/lib/libFortranRuntime.a -o tst.so -sSIDE_MODULE=1
