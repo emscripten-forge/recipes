3efc3f460af9b2005ef0f8b0af2b65c8b47d640c
DerThorsten <derthorstenbeier@gmail.com>
2025-03-21 10:37:19 +0100

    wip

--- recipes/recipes_emscripten/scikit-image/build.sh
+++ recipes/recipes_emscripten/scikit-image/build.sh
@@ -1,4 +1,24 @@
-set -ex
-# clean any cython-generated .c/.cpp files (as used by "make clean")
-find . -name "*.pyx" -exec ./tools/rm_pyx_assoc_c_cpp.sh {} \;
-python -m pip install . -vv
+#!/bin/bash
+
+
+
+export CFLAGS="$CFLAGS -sWASM_BIGINT -s SIDE_MODULE=1 -Wno-implicit-function-declaration -fexceptions"
+export CXXFLAGS="$CXXFLAGS -sWASM_BIGINT -s SIDE_MODULE=1 -fexceptions"
+export LDFLAGS="$LDFLAGS -sWASM_BIGINT -s SIDE_MODULE=1 -fexceptions"
+
+
+
+cp $RECIPE_DIR/patches/_base.py $SRC_DIR/sklearn/datasets/_base.py
+
+# otherwise "cython" is not properly executable
+echo "add shebang to cython file"
+sed -i '1i#!/usr/bin/env python' $BUILD_PREFIX/bin/cython
+
+
+
+
+cp $RECIPE_DIR/emscripten.meson.cross $SRC_DIR
+echo "python = '${PYTHON}'" >> $SRC_DIR/emscripten.meson.cross
+
+${PYTHON} -m pip install . -vvv --no-deps --no-build-isolation \
+    -Csetup-args="--cross-file=$SRC_DIR/emscripten.meson.cross"
