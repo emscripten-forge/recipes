10d68243c85f5def026e38e43d063ff8dc435810
Thorsten Beier <derthorstenbeier@gmail.com>
2025-03-21 14:55:13 +0100

    Scikit img (#2056)
    * wip
    
    * forcing config
    
    * cleanup
    
    * missing file

--- recipes/recipes_emscripten/scikit-image/test_skimage.py
+++ recipes/recipes_emscripten/scikit-image/test_skimage.py
@@ -3,14 +3,17 @@ def test_basic():
     from skimage import color, data
     from skimage.util import view_as_blocks
 
+    # from skimage import io
+    # io.use_plugin('matplotlib', 'imread')
+
     # get astronaut from skimage.data in grayscale
-    l = color.rgb2gray(data.astronaut())
+    a = data.astronaut()
+    l = color.rgb2gray(a)#.astype(np.float32)
     assert l.size == 262144
     assert l.shape == (512, 512)
 
     # size of blocks
     block_shape = (4, 4)
-
     # see astronaut as a matrix of blocks (of shape block_shape)
     view = view_as_blocks(l, block_shape)
     assert view.shape == (128, 128, 4, 4)
@@ -19,7 +22,6 @@ def test_basic():
 
     to = threshold_otsu(l)
     assert to.hex() == "0x1.8e00000000000p-2"
-
     from skimage.color import rgb2gray
     from skimage.data import astronaut
     from skimage.filters import sobel
@@ -29,11 +31,13 @@ def test_basic():
     img = img_as_float(astronaut()[::2, ::2])
 
     segments_fz = felzenszwalb(img, scale=100, sigma=0.5, min_size=50)
-    segments_slic = slic(img, n_segments=250, compactness=10, sigma=1)
-    segments_quick = quickshift(img, kernel_size=3, max_dist=6, ratio=0.5)
-    gradient = sobel(rgb2gray(img))
-    segments_watershed = watershed(gradient, markers=250, compactness=0.001)
 
+    segments_slic = slic(img, n_segments=250, compactness=10, sigma=1)
+
+    segments_quick = quickshift(img, kernel_size=3, max_dist=6, ratio=0.5)
+    grayscale = rgb2gray(img)
+    gradient = sobel(grayscale)
+    segments_watershed = watershed(gradient, markers=250, compactness=0.001)
     assert len(np.unique(segments_fz)) == 194
     assert len(np.unique(segments_slic)) == 196
     assert len(np.unique(segments_quick)) == 695
