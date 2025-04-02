fc84f22ec47ea7bc0eedd947e468fae3dcee1396
anutosh491 <andersonbhat491@gmail.com>
2025-04-02 15:10:06 +0530

    Build symengine as a shared object

--- recipes/recipes_emscripten/symengine/build.sh
+++ recipes/recipes_emscripten/symengine/build.sh
@@ -5,12 +5,12 @@ emcmake cmake \
     -DBUILD_BENCHMARKS=OFF \
     -DINTEGER_CLASS=boostmp \
     -DWITH_BOOST=ON \
+    -DBoost_INCLUDE_DIR=$PREFIX/include \
+    -DBUILD_SHARED_LIBS=ON \
     -DCMAKE_PREFIX_PATH=$PREFIX \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
     -DCMAKE_INSTALL_LIBDIR=lib \
     -DBUILD_FOR_DISTRIBUTION=yes \
-    -DBUILD_SHARED_LIBS=no \
-    -DBoost_INCLUDE_DIR=$PREFIX/include \
     -DWITH_SYMENGINE_THREAD_SAFE=ON \
     -DWITH_SYMENGINE_RCP=ON \
     -DWITH_FLINT=OFF \
@@ -19,7 +19,6 @@ emcmake cmake \
     -DWITH_MPFR=OFF \
     -DWITH_MPC=OFF \
     -DWITH_OPENMP=OFF \
-    -S . \
-    -B build
+    ..
 
-emmake cmake --build build --target install
+emmake make install -j8
