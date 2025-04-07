b0da2293b174b53884406de0276d0a9a23141f2c
AntoinePrv <AntoinePrv@users.noreply.github.com>
2025-04-07 10:34:30 +0200

    First draft

---
+++ recipes/recipes_emscripten/octave/build.sh
@@ -0,0 +1,22 @@
+export FFLAGS="${FFLAGS} -g --target=wasm32-unknown-emscripten"
+
+export FLIBS="-lFortranRuntime"
+
+emconfigure ./configure \
+   --prefix="${PREFIX}" \
+   --build="x86_64-conda-linux-gnu" \
+   --host="wasm32-unknown-emscripten" \
+   --disable-dependency-tracking \
+   --enable-fortran-calling-convention="f2c" \
+   --enable-shared=yes \
+   --disable-rpath \
+   --with-blas="yes" \
+   --with-lapack="yes" \
+   --without-pcre \
+   --with-pcre2 \
+   --with-pcre2-includedir="${PREFIX}/include" \
+   --with-pcre2-libdir="-lpcre2-8" \
+   --disable-openmp \
+   --disable-readline \
+
+emmake make
