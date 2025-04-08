151062b505a1646cdc77f8b755b0bf1bdd35fc99
shimwell <mail@jshimwell.com>
2025-04-08 17:47:13 +0200

    endf recipe started

---
+++ recipes/endf/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -ex
+
+# Apply any necessary Emscripten-specific flags
+export CFLAGS="${CFLAGS} -s USE_PTHREADS=0"
+export CXXFLAGS="${CXXFLAGS} -s USE_PTHREADS=0"
+export LDFLAGS="${LDFLAGS} -s USE_PTHREADS=0"
+
+# Use pip to build the package
+${PYTHON} -m pip install . --no-deps -vv
