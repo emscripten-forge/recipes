e1701e24268bf6a3c2b38fc4184430da454eab0c
DerThorsten <derthorstenbeier@gmail.com>
2025-04-08 13:35:46 +0200

    test usage

--- recipes/recipes_emscripten/h5py/test_h5py.py
+++ recipes/recipes_emscripten/h5py/test_h5py.py
@@ -20,3 +20,18 @@ def test_import():
     import h5py.h5t
     import h5py.h5z
     import h5py.utils
+
+def test_usage():
+    import h5py
+
+    with h5py.File("mytestfile.hdf5", "a") as f:
+        dset = f.create_dataset("mydataset", (100,), dtype="i")
+        grp = f.create_group("subgroup")
+        dset2 = grp.create_dataset("another_dataset", (50,), dtype="f")
+
+        assert f.name == "/"
+        assert dset.name == "/mydataset"
+        assert dset2.name == "/subgroup/another_dataset"
+
+    f = h5py.File("mytestfile.hdf5", "r")
+    assert sorted(list(f.keys())) == ["mydataset", "subgroup"]
