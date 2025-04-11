0c5f4bbf12448f31ef59be53b8e4740ed037c088
Thorsten Beier <derthorstenbeier@gmail.com>
2025-04-11 10:53:11 +0200

    Update build.sh

--- recipes/recipes_emscripten/segyio/build.sh
+++ recipes/recipes_emscripten/segyio/build.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-mkdir build
-cd build
+# mkdir build
+# cd build
 
 
-# copy includes of python from host to build dir
-mkdir -p $BUILD_PREFIX/include/
-cp -r $PREFIX/include/python3.13/ $BUILD_PREFIX/include/
+# # copy includes of python from host to build dir
+# mkdir -p $BUILD_PREFIX/include/
+# cp -r $PREFIX/include/python3.13/ $BUILD_PREFIX/include/
 
-emcmake cmake .. -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON -DHAVE_FSTATI64=0 -DCMAKE_POSITION_INDEPENDENT_CODE=ON
-emmake make
-emmake make install
+# emcmake cmake .. -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON -DHAVE_FSTATI64=0 -DCMAKE_POSITION_INDEPENDENT_CODE=ON
+# emmake make
+# emmake make install
+
+${PYTHON} -m pip  install .
