a01a1a9364560dd9ea4f824dcb5f8a60b25f126a
Thorsten Beier <derthorstenbeier@gmail.com>
2025-04-14 11:54:38 +0200

    Update build.sh

--- recipes/recipes_emscripten/sourmash/build.sh
+++ recipes/recipes_emscripten/sourmash/build.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+
+
+# add --no-entry to compiler flags
+export CFLAGS="${CFLAGS} --no-entry"
+
 export MATURIN_PYTHON_SYSCONFIGDATA_DIR=${PREFIX}/etc/conda/_sysconfigdata__emscripten_wasm32-emscripten.py
 ${PYTHON} -m pip  install . -vvv
