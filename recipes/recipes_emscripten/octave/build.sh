309e9906284e9d2808e3f3abf2d1b26b9458bde1
AntoinePrv <AntoinePrv@users.noreply.github.com>
2025-04-16 11:19:12 +0200

    Pass linker in CFLAGS and CXXFLAGS

--- recipes/recipes_emscripten/octave/build.sh
+++ recipes/recipes_emscripten/octave/build.sh
@@ -1,16 +1,17 @@
 export FFLAGS="${FFLAGS} -g --target=wasm32-unknown-emscripten"
 export FLIBS="-lFortranRuntime"
 
-# LDFLAGS are passed to all compilers, but flang-new does not support emscripten flags
-# We split them and set emscripten-specific flags in CPPFLAGS which are always passed along
-# LDFLAGS to emscripten wrapped CC and CXX compilers.
+# LDFLAGS are passed to all compilers, but flang-new does not support emscripten flags.
+# We split them and set emscripten-specific flags in CFLAGS and CXXFLAGS which are often passed
+# along LDFLAGS to emscripten wrapped CC and CXX compilers.
 LDFLAGS_LINES="$(echo "${LDFLAGS}" |  sed -E 's/-s +/-s/g' | tr ' ' '\n')"
 export LDFLAGS="$(echo "${LDFLAGS_LINES}" | grep -v '^-s' | tr '\n' ' ')"
 
 # --disable-threads does not seem to disable parallelism, so we ignore undefined symbols for now
 EM_LDFLAGS="$(echo "${LDFLAGS_LINES}" | grep '^-s' | tr '\n' ' ')"
 EM_LDFLAGS="${EM_LDFLAGS} -sERROR_ON_UNDEFINED_SYMBOLS=0"
-export CPPFLAGS="${CPPFLAGS} ${EM_LDFLAGS}"
+export CFLAGS="${CFLAGS} ${EM_LDFLAGS}"
+export CXXFLAGS="${CXXFLAGS} ${EM_LDFLAGS}"
 
 # Forcing autotools to NOT rerun after patches
 find . -exec touch -t $(date +%Y%m%d%H%M) {} \;
