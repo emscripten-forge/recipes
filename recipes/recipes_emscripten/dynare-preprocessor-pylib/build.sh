1ce05c873c7586312f7b482612c61f70fc7ac56a
Pablo Winant <pablo.winant@gmail.com>
2025-04-16 12:19:19 +0200

    First implementation of dynare python library. (#712)
    * First implementation of dynare python library.
    
    * Renamed meta.yaml to recipe.yaml
    
    * Fix recipe.yaml
    
    * Update recipe.yaml
    
    * Update recipe.yaml
    
    * Update recipe.yaml -> move everything to build
    
    * Update build.sh
    
    * Update build.sh
    
    * Updated source (no more jthread)
    
    * Update recipe.yaml
    
    * Update recipe.yaml
    
    * Update recipe.yaml
    
    * Update recipe.yaml
    
    * Update recipe.yaml
    
    * Works on my computer ©
    
    * Remove python dependency in meson file
    
    * With cross-python
    
    * Added simple test.
    
    * TEST: Corrected name of python library
    
    * No need to manually pass python include dir
    
    * Remove useless python binary.
    
    * Added SIDE_MODULE=1
    
    * Try -sUSE_PTHREADS=1
    
    * Try --enable-threads
    
    * Try another alternative.
    
    * Test works on my computer
    
    * Update recipe.yaml
    
    * Update recipe.yaml
    
    * Update recipe.yaml
    
    * Update recipe.yaml
    
    * Update test_preproc.py
    
    * Update build.sh
    
    * Update build.sh
    
    * Update build.sh
    
    * Update wasm_32.ini
    
    * Update build.sh
    
    ---------
    
    Co-authored-by: Thorsten Beier <derthorstenbeier@gmail.com>

---
+++ recipes/recipes_emscripten/dynare-preprocessor-pylib/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+export BOOST_ROOT=$PREFIX
+
+cp $RECIPE_DIR/meson.build src/meson.build
+
+meson setup --prefix=$PREFIX --bindir=$PREFIX/bin --libdir=$PREFIX/lib --includedir=$PREFIX/include \
+    --buildtype=release build_preproc \
+    -Dcpp_args="-w  -Wno-enum-constexpr-conversion -I${PREFIX}/include/pybind11 -sSIDE_MODULE=1 -s WASM_BIGINT -fexceptions"  \
+    -Dcpp_link_args="-w  -Wno-enum-constexpr-conversion -I${PREFIX}/include/pybind11 -sSIDE_MODULE=1 -s WASM_BIGINT -fexceptions" \
+    --cross-file=$RECIPE_DIR/wasm_32.ini
+
+meson compile -C build_preproc
+meson install -C build_preproc #--destdir="../
+
+rm $PREFIX/bin/python
