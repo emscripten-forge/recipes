45d0e67116c30de8b42c75b50c35bcd80d6c7cbd
Thorsten Beier <derthorstenbeier@gmail.com>
2025-04-16 11:44:39 +0200

    Update build.sh

--- recipes/recipes_emscripten/dynare-preprocessor-pylib/build.sh
+++ recipes/recipes_emscripten/dynare-preprocessor-pylib/build.sh
@@ -6,8 +6,8 @@ cp $RECIPE_DIR/meson.build src/meson.build
 
 meson setup --prefix=$PREFIX --bindir=$PREFIX/bin --libdir=$PREFIX/lib --includedir=$PREFIX/include \
     --buildtype=release build_preproc \
-    -Dcpp_args="-w  -Wno-enum-constexpr-conversion -I${PREFIX}/include/pybind11 -sSIDE_MODULE=1"  \
-    -Dcpp_link_args="-w  -Wno-enum-constexpr-conversion -I${PREFIX}/include/pybind11 -sSIDE_MODULE=1" \
+    -Dcpp_args="-w  -Wno-enum-constexpr-conversion -I${PREFIX}/include/pybind11 -sSIDE_MODULE=1 -s WASM_BIGINT"  \
+    -Dcpp_link_args="-w  -Wno-enum-constexpr-conversion -I${PREFIX}/include/pybind11 -sSIDE_MODULE=1 -s WASM_BIGINT" \
     --cross-file=$RECIPE_DIR/wasm_32.ini
 
 meson compile -C build_preproc
