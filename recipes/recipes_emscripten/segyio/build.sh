6415c823a1f7e8f8a553c54b0aeaacd9bdc7f247
DerThorsten <derthorstenbeier@gmail.com>
2025-04-16 11:47:07 +0200

    trigger 2

--- recipes/recipes_emscripten/segyio/build.sh
+++ recipes/recipes_emscripten/segyio/build.sh
@@ -8,8 +8,10 @@
 # mkdir -p $BUILD_PREFIX/include/
 # cp -r $PREFIX/include/python3.13/ $BUILD_PREFIX/include/
 
-# emcmake cmake .. -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON -DHAVE_FSTATI64=0 -DCMAKE_POSITION_INDEPENDENT_CODE=ON
-# emmake make
-# emmake make install
-cd python
-${PYTHON} -m pip  install .
+emcmake cmake .. -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON -DHAVE_FSTATI64=0 -DCMAKE_POSITION_INDEPENDENT_CODE=ON
+emmake make
+emmake make install
+
+
+# cd python
+# ${PYTHON} -m pip  install .
