d42a2a3fb5593bdc9999c5018fcb022c935cd4e3
DerThorsten <derthorstenbeier@gmail.com>
2025-04-17 09:08:53 +0200

    cross

--- recipes/recipes_emscripten/segyio/build.sh
+++ recipes/recipes_emscripten/segyio/build.sh
@@ -8,10 +8,18 @@ cd build
 # mkdir -p $BUILD_PREFIX/include/
 # cp -r $PREFIX/include/python3.13/ $BUILD_PREFIX/include/
 
-emcmake cmake .. -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON -DHAVE_FSTATI64=0 -DCMAKE_POSITION_INDEPENDENT_CODE=ON
+emcmake cmake .. \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DBUILD_SHARED_LIBS=ON \
+    -DHAVE_FSTATI64=0 \
+    -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
+    -DBUILD_PYTHON=OFF \
+    -DBUILD_BIN=OFF 
+
+
 emmake make
 emmake make install
 
 
-# cd python
-# ${PYTHON} -m pip  install .
+cd ../python
+${PYTHON} -m pip  install .
