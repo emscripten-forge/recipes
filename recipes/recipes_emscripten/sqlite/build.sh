e3d403fa84c3ba1b235858f04362836ad6dd4487
DerThorsten <derthorstenbeier@gmail.com>
2025-04-22 10:32:23 +0200

    wip

--- recipes/recipes_emscripten/sqlite/build.sh
+++ recipes/recipes_emscripten/sqlite/build.sh
@@ -6,23 +6,24 @@ echo "#!/usr/bin/env bash" > ldconfig
 chmod +x ldconfig
 export PATH=${PWD}:$PATH
 
+export EMSDK_HOME=${BUILD_PREFIX}/opt/emsdk
 
 echo "HOST" $HOST
 echo "BUILD" $BUILD
 
 emconfigure  ./configure --prefix=${PREFIX} \
-            --build=${BUILD} \
-            --host=none \
-            --enable-threadsafe \
-            --enable-shared=no \
-            --disable-tcl \
-            CFLAGS="${CFLAGS} -I${PREFIX}/include -fPIC" \
-            LDFLAGS="${LDFLAGS} -L${PREFIX}/lib" \
-            CPPFLAGS="-DSQLITE_OMIT_POPEN"; \
+    --enable-all \
+    CFLAGS="${CFLAGS} -I${PREFIX}/include -fPIC" \
+    LDFLAGS="${LDFLAGS} -L${PREFIX}/lib" \
+    CPPFLAGS="-DSQLITE_OMIT_POPEN"; \
 
 
 emmake make -j${CPU_COUNT} ${VERBOSE_AT}
 emmake make install
+
+make sqlite3.c
+cd ext/wasm
+make
 
 # # We can remove this when we start using the new conda-build.
 # find $PREFIX -name '*.la' -delete
