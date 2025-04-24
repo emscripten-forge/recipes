7681a9c3df267a2f4442f7229b3b1d146539e037
Isabel Paredes <isabel.paredes@quantstack.net>
2025-04-24 16:26:57 +0200

    Build Fortran decimal library

--- recipes/recipes_emscripten/libflang/build.sh
+++ recipes/recipes_emscripten/libflang/build.sh
@@ -2,8 +2,8 @@
 
 set -ex
 
-export CFLAGS="-fPIC"
-export CXXFLAGS="-fPIC"
+export CFLAGS="$CFLAGS -fPIC"
+export CXXFLAGS="$CXXFLAGS -fPIC"
 
 # See https://github.com/serge-sans-paille/llvm-project/blob/feature/flang-wasm/README.wasm.md
 emcmake cmake -S flang/runtime -B _fbuild_runtime -GNinja \
@@ -11,3 +11,11 @@ emcmake cmake -S flang/runtime -B _fbuild_runtime -GNinja \
                 -DCMAKE_INSTALL_PREFIX=$PREFIX
 $(which cmake) --build _fbuild_runtime
 $(which cmake) --build _fbuild_runtime --target install
+
+
+# Build libFortranDecimal.a
+emcmake cmake -S flang/lib/Decimal -B _fbuild_decimal -GNinja \
+                -DCMAKE_BUILD_TYPE=Release \
+                -DCMAKE_INSTALL_PREFIX=$PREFIX
+$(which cmake) --build _fbuild_decimal
+$(which cmake) --build _fbuild_decimal --target install
