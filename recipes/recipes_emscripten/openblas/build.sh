36d1f705e5a89edb87973b4c4ef1ec866319400d
Isabel Paredes <isabel.paredes@quantstack.net>
2025-04-25 12:38:40 +0200

    Use flang package

--- recipes/recipes_emscripten/openblas/build.sh
+++ recipes/recipes_emscripten/openblas/build.sh
@@ -2,20 +2,6 @@
 
 set -ex
 
-# Using flang as a WASM cross-compiler
-# https://github.com/serge-sans-paille/llvm-project/blob/feature/flang-wasm/README.wasm.md
-# https://github.com/conda-forge/flang-feedstock/pull/69
-micromamba install -p $BUILD_PREFIX \
-    conda-forge/label/llvm_rc::libllvm19 \
-    conda-forge/label/llvm_dev::flang=19.1.0.rc2 \
-    -y --no-channel-priority
-rm $BUILD_PREFIX/bin/clang # links to clang19
-ln -s $BUILD_PREFIX/bin/clang-18 $BUILD_PREFIX/bin/clang # links to emsdk clang
-
-export CC=emcc
-export FC=flang-new
-export FFLAGS="--target=wasm32-unknown-emscripten"
-
 emmake make libs \
     NO_SHARED=1 \
     TARGET=RISCV64_GENERIC \
