0f990fc9fd654520cfa2f3063db8bfeef57304f2
DerThorsten <derthorstenbeier@gmail.com>
2025-05-07 11:59:47 +0200

    box2d

--- recipes/recipes_emscripten/pyb2d/build.sh
+++ recipes/recipes_emscripten/pyb2d/build.sh
@@ -1,22 +1,2 @@
-mkdir build
-cd build
-
-export CMAKE_PREFIX_PATH=$PREFIX
-export CMAKE_SYSTEM_PREFIX_PATH=$PREFIX
-
-
-# Configure step
-cmake ${CMAKE_ARGS} ..                                \
-    -GNinja                                           \
-    -DCMAKE_BUILD_TYPE=Release                        \
-    -DCMAKE_PREFIX_PATH=$PREFIX                       \
-    -DCMAKE_INSTALL_PREFIX=$PREFIX                    \
-    -DPYTHON_NUMPY_INCLUDE_DIR="$PREFIX/lib/python3.11/site-packages/numpy/core/include" \
-    -DPYTHON_MODULE_INSTALL_DIR="$PREFIX/lib/python3.11/site-packages" \
-    -DPYB2D_LIQUID_FUN=ON
-# Build step
-ninja
-
-
-
-ninja install
+#!/bin/bash
+${PYTHON} -m pip  install .
--- recipes/recipes_emscripten/pyb2d/test_pyb2d.py
+++ recipes/recipes_emscripten/pyb2d/test_pyb2d.py
@@ -1,36 +1,74 @@
+import pytest
+import numpy as np
+import pyb2d as b2d
 
-def test_pyb2d():
 
-    import b2d
-    assert b2d.BuildConfiguration.LIQUID_FUN
-    import numpy as np
+@pytest.fixture
+def world_id():
+    world_def = b2d.world_def(gravity=(0, -10))
+    world_id = b2d.create_world(world_def)
+    yield world_id
+    b2d.destroy_world(world_id)
 
-    world = b2d.world(gravity=(-9, 0))
 
-    body = world.create_dynamic_body(
-        position=(0, 0),
-        fixtures=b2d.fixture_def(shape=b2d.circle_shape(radius=1), density=1),
+
+def test_world():
+    world_def = b2d.world_def(gravity=(0, -10), user_data=42)
+    world_id = b2d.create_world(world_def)
+    world_user_data = b2d.world_get_user_data(world_id)
+    assert world_user_data == 42
+    b2d.destroy_world(world_id)
+
+
+def test_hello_world(world_id):
+
+    # create static ground body
+    body_def = b2d.body_def(position=(0, -10), type=b2d.BodyType.STATIC, user_data=10)
+    assert body_def.user_data == 10
+    groud_body_id = b2d.create_body(world_id, body_def)
+    body_user_data = b2d.body_get_user_data(groud_body_id)
+    assert body_user_data == 10
+    box = b2d.make_box(1, 1)
+
+    # surface material
+    surface_material = b2d.surface_material(
+        friction=0.3, restitution=0.5, rolling_resistance=0.3
     )
 
-    t = 5
-    fps = 40
-    dt = 1.0 / fps
-    n_steps = int(t / dt + 0.5)
+    # shape definition
+    shape_def = b2d.shape_def(density=0, material=surface_material, user_data=100)
+    shape_id = b2d.create_polygon_shape(groud_body_id, shape_def, box)
+    shape_user_data = b2d.shape_get_user_data(shape_id)
+    assert shape_user_data == 100
 
-    positions = np.zeros([n_steps, 2])
-    velocites = np.zeros([n_steps, 2])
-    timepoints = np.zeros([n_steps])
+    # create dynamic body
+    body_def = b2d.body_def(
+        position=(0, 4),
+        type=b2d.BodyType.DYNAMIC,
+        angular_damping=0.1,
+        linear_damping=0.1,
+        fixed_rotation=False,
+    )
+    dynamic_body_id = b2d.create_body(world_id, body_def)
+    circle = b2d.circle(radius=0.5)
+    surface_material = b2d.surface_material(friction=0.3, restitution=0.5)
+    shape_def = b2d.shape_def(density=1, material=surface_material, user_data=0)
+    shape_id = b2d.create_circle_shape(dynamic_body_id, shape_def, circle)
 
-    t_elapsed = 0.0
-    for i in range(n_steps):
+    body_user_data = b2d.body_get_user_data(dynamic_body_id)
+    assert body_user_data == 0
 
-        # get the bodies center of mass
-        positions[i, :] = body.world_center
+    # create debug draw
+    debug_draw = DebugDrawTest()
+    debug_draw.draw_shapes = True
+    debug_draw.draw_joints = True
+    debug_draw.draw_joint_extras = True
 
-        # get the bodies velocity
-        velocites[i, :] = body.linear_velocity
+    # simulate
+    pos_inital = b2d.body_get_position(dynamic_body_id)
+    for i in range(60):
+        b2d.world_step(world_id, 1 / 60, 6)
+        pos = b2d.body_get_position(dynamic_body_id)
+        b2d.world_draw(world_id, debug_draw)
 
-        timepoints[i] = t_elapsed
-
-        world.step(time_step=dt, velocity_iterations=1, position_iterations=1)
-        t_elapsed += dt
+    assert pos[1] < pos_inital[1]
