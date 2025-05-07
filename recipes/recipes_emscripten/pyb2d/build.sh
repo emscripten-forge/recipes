be37eb3df7d8dfcd24edde264c42ef643bee4207
DerThorsten <derthorstenbeier@gmail.com>
2025-05-07 14:33:47 +0200

    wip

--- recipes/recipes_emscripten/pyb2d/build.sh
+++ recipes/recipes_emscripten/pyb2d/build.sh
@@ -1,2 +1,43 @@
-#!/bin/bash
-${PYTHON} -m pip  install .
+mkdir build
+cd build
+
+export CMAKE_PREFIX_PATH=$PREFIX
+export CMAKE_SYSTEM_PREFIX_PATH=$PREFIX
+
+export PYTHON_LIBRARIES=$PREFIX/lib/libpython3.13.a
+export PYTHON_INCLUDE_DIR=$PREFIX/include/python3.13
+
+
+nanobind_DIR=$($PYTHON -m nanobind --cmake_dir)
+echo "nanobind_DIR: $nanobind_DIR"
+
+ls $nanobind_DIR
+
+# Configure step
+cmake ${CMAKE_ARGS} ..                                \
+    -GNinja                                           \
+    -DCMAKE_BUILD_TYPE=Release                        \
+    -DCMAKE_PREFIX_PATH=$PREFIX                       \
+    -DCMAKE_INSTALL_PREFIX=$PREFIX                    \
+    -DPython_EXECUTABLE=$PYTHON                     \
+    -DPython_Interpreter=$PYTHON                     \
+    -DPython_INCLUDE_DIRS=$PREFIX/include/python3.13 \
+    -DPython_LIBRARY=$PREFIX/lib/libpython3.13.a \
+    -DPython_LIBRARIES=$PREFIX/lib/libpython3.13.a \
+    -DNB_SUFFIX="" \
+    -DNB_SUFFIX_S="" \
+    -DEXT_SUFFIX="so" \
+    -Dnanobind_DIR=$nanobind_DIR
+
+
+# Build step
+ninja
+
+
+
+# make python side-package pyb2d
+mkdir -p $PREFIX/lib/python3.13/site-packages/pyb2d
+
+# copy $SRC_DIR/src/module/pyb2d to $PREFIX/lib/python3.13/site-packages/pyb2d
+cp -r $SRC_DIR/src/module/pyb2d $PREFIX/lib/python3.13/site-packages/
+
--- recipes/recipes_emscripten/pyb2d/test_pyb2d.py
+++ recipes/recipes_emscripten/pyb2d/test_pyb2d.py
@@ -3,6 +3,56 @@ import numpy as np
 import pyb2d as b2d
 
 
+class DebugDrawTest(b2d.DebugDraw):
+    def __init__(self):
+        super().__init__()
+
+    def draw_polygon(self, vertices, color):
+        color = hex(color)
+        assert isinstance(vertices, np.ndarray)
+
+    def draw_solid_polygon(self, transform, vertices, radius, color):
+        color = hex(color)
+        assert isinstance(vertices, np.ndarray)
+        assert radius >= 0
+        assert isinstance(transform, b2d.Transform)
+
+    def draw_circle(self, center, radius, color):
+        color = hex(color)
+        assert isinstance(center, tuple)
+        assert radius >= 0
+
+    def draw_solid_capsule(self, p1, p2, radius, color):
+        color = hex(color)
+        assert isinstance(p1, tuple)
+        assert isinstance(p2, tuple)
+        assert radius >= 0
+
+    def draw_solid_circle(self, transform, radius, color):
+        color = hex(color)
+        assert radius >= 0
+        assert isinstance(transform, b2d.Transform)
+
+    def draw_segment(self, p1, p2, color):
+        color = hex(color)
+        assert isinstance(p1, tuple)
+        assert isinstance(p2, tuple)
+
+    def draw_transform(self, xf):
+        assert isinstance(xf, b2d.Transform)
+
+    def draw_point(self, p, size, color):
+        assert isinstance(p, tuple)
+        assert size >= 0
+        color = hex(color)
+
+    def draw_aabb(self, aabb, color):
+        assert isinstance(aabb, tuple)
+        color = hex(color)
+
+
+
+
 @pytest.fixture
 def world_id():
     world_def = b2d.world_def(gravity=(0, -10))
