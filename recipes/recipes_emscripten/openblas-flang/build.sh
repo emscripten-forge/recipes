31e97f894619e3e818c50c7e2ff1a14f3f10ed4d
AntoinePrv <AntoinePrv@users.noreply.github.com>
2025-05-13 15:28:34 +0200

    Sed Blas string

--- recipes/recipes_emscripten/openblas-flang/build.sh
+++ recipes/recipes_emscripten/openblas-flang/build.sh
@@ -2,6 +2,15 @@
 
 set -ex
 
+find . -type f -name "*.f" -exec sed -i "s/'Left'/'L'/Ig" {} \;
+find . -type f -name "*.f" -exec sed -i "s/'Right'/'R'/Ig" {} \;
+find . -type f -name "*.f" -exec sed -i "s/'Upper'/'U'/Ig" {} \;
+find . -type f -name "*.f" -exec sed -i "s/'Lower'/'L'/Ig" {} \;
+find . -type f -name "*.f" -exec sed -i "s/'Transpose'/'T'/Ig" {} \;
+find . -type f -name "*.f" -exec sed -i "s/'No transpose'/'N'/Ig" {} \;
+find . -type f -name "*.f" -exec sed -i "s/'Conjugate transpose'/'C'/Ig" {} \;
+find . -type f -name "*.f" -exec sed -i "s/'Non-unit'/'N'/Ig" {} \;
+
 emmake make libs netlib \
     CC=emcc \
     HOSTCC=gcc \
