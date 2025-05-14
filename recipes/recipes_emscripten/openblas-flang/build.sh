beaf793e6787f8acfe6dc620d4ec6ee095dd3b47
AntoinePrv <AntoinePrv@users.noreply.github.com>
2025-05-14 18:07:35 +0200

    Try char array

--- recipes/recipes_emscripten/openblas-flang/build.sh
+++ recipes/recipes_emscripten/openblas-flang/build.sh
@@ -2,14 +2,10 @@
 
 set -ex
 
-find . -type f -name "*.f" -exec sed -i "s/'Left'/'L'/Ig" {} \;
-find . -type f -name "*.f" -exec sed -i "s/'Right'/'R'/Ig" {} \;
-find . -type f -name "*.f" -exec sed -i "s/'Upper'/'U'/Ig" {} \;
-find . -type f -name "*.f" -exec sed -i "s/'Lower'/'L'/Ig" {} \;
-find . -type f -name "*.f" -exec sed -i "s/'Transpose'/'T'/Ig" {} \;
-find . -type f -name "*.f" -exec sed -i "s/'No transpose'/'N'/Ig" {} \;
-find . -type f -name "*.f" -exec sed -i "s/'Conjugate transpose'/'C'/Ig" {} \;
-find . -type f -name "*.f" -exec sed -i "s/'Non-unit'/'N'/Ig" {} \;
+find lapack-netlib/BLAS/SRC \
+    -type f \
+    -name "*.f" \
+    -exec sed -ir 's/^([^*]*)CHARACTER([^*].*)/\1CHARACTER*(*)\2/' '{}' \;
 
 emmake make libs netlib \
     NO_SHARED=1 \
