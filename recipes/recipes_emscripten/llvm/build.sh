09998540828947c8a3b6f0f5a60f3a2f7b4fec1e
kr-2003 <kumar.kr.abhinav@gmail.com>
2025-05-21 15:08:34 +0530

    unset toolchain

--- recipes/recipes_emscripten/llvm/build.sh
+++ recipes/recipes_emscripten/llvm/build.sh
@@ -1,9 +1,12 @@
-mkdir native_build
-cd native_build
-cmake -DLLVM_ENABLE_PROJECTS=clang -DLLVM_TARGETS_TO_BUILD=host -DCMAKE_BUILD_TYPE=Release ../llvm/
-cmake --build . --target llvm-tblgen clang-tblgen --parallel $(nproc --all)
-export NATIVE_DIR=$PWD/bin/
-cd ..
+(
+    mkdir native_build
+    cd native_build
+    unset CMAKE_TOOLCHAIN_FILE
+    cmake -DLLVM_ENABLE_PROJECTS=clang -DLLVM_TARGETS_TO_BUILD=host -DCMAKE_BUILD_TYPE=Release ../llvm/
+    cmake --build . --target llvm-tblgen clang-tblgen --parallel $(nproc --all)
+    export NATIVE_DIR=$PWD/bin/
+    cd ..
+)
 
 mkdir build
 cd build
@@ -16,27 +19,31 @@ export LDFLAGS=""
 
 # Configure step
 emcmake cmake ${CMAKE_ARGS} -S ../llvm -B .         \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DLLVM_HOST_TRIPLE=wasm32-unknown-emscripten \
-    -DLLVM_ENABLE_ASSERTIONS=ON \
-    -DLLVM_TARGETS_TO_BUILD="WebAssembly" \
-    -DLLVM_ENABLE_LIBEDIT=OFF \
-    -DLLVM_ENABLE_PROJECTS="clang;lld" \
-    -DLLVM_ENABLE_ZSTD=OFF \
-    -DLLVM_ENABLE_LIBXML2=OFF \
-    -DCLANG_ENABLE_STATIC_ANALYZER=OFF \
-    -DCLANG_ENABLE_ARCMT=OFF \
-    -DCLANG_ENABLE_BOOTSTRAP=OFF \
+    -DCMAKE_BUILD_TYPE=Release                      \
+    -DCMAKE_PREFIX_PATH=$PREFIX                     \
+    -DCMAKE_INSTALL_PREFIX=$PREFIX                  \
+    -DLLVM_HOST_TRIPLE=wasm32-unknown-emscripten    \
+    -DLLVM_TARGETS_TO_BUILD="WebAssembly"           \
+    -DLLVM_ENABLE_ASSERTIONS=ON                     \
+    -DLLVM_ENABLE_EH=ON                             \
+    -DLLVM_ENABLE_RTTI=ON                           \
+    -DLLVM_INCLUDE_BENCHMARKS=OFF                   \
+    -DLLVM_INCLUDE_EXAMPLES=OFF                     \
+    -DLLVM_INCLUDE_TESTS=OFF                        \
+    -DLLVM_ENABLE_LIBEDIT=OFF                       \
+    -DLLVM_ENABLE_PROJECTS="clang;lld"              \
+    -DLLVM_ENABLE_THREADS=OFF                       \
+    -DLLVM_ENABLE_ZSTD=OFF                          \
+    -DLLVM_ENABLE_LIBXML2=OFF                       \
+    -DLLVM_BUILD_TOOLS=OFF                          \
+    -DCLANG_ENABLE_STATIC_ANALYZER=OFF              \
+    -DCLANG_ENABLE_ARCMT=OFF                        \
+    -DCLANG_ENABLE_BOOTSTRAP=OFF                    \
+    -DCLANG_BUILD_TOOLS=OFF                         \
     -DCMAKE_CXX_FLAGS="-Dwait4=__syscall_wait4 -fexceptions" \
-    -DLLVM_INCLUDE_BENCHMARKS=OFF \
-    -DLLVM_INCLUDE_EXAMPLES=OFF \
-    -DLLVM_INCLUDE_TESTS=OFF \
-    -DLLVM_ENABLE_THREADS=OFF \
-    -DLLVM_BUILD_TOOLS=OFF \
-    -DLLVM_ENABLE_LIBPFM=OFF \
-    -DCLANG_BUILD_TOOLS=OFF \
-    -DLLVM_TABLEGEN=$NATIVE_DIR/llvm-tblgen \
-    -DCLANG_TABLEGEN=$NATIVE_DIR/clang-tblgen \
+    -DLLVM_TABLEGEN=$NATIVE_DIR/llvm-tblgen         \
+    -DCLANG_TABLEGEN=$NATIVE_DIR/clang-tblgen       \
+    -DLLVM_ENABLE_LIBPFM=OFF                        \
 
 # Build and Install step
 emmake make clangInterpreter lldWasm -j16 install
