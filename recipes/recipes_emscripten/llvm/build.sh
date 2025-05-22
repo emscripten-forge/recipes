44ae0fd83fc7610e3ccb2332bec70b6437018473
kr-2003 <kumar.kr.abhinav@gmail.com>
2025-05-22 19:39:44 +0530

    moved tblgen to build-arch

--- recipes/recipes_emscripten/llvm/build.sh
+++ recipes/recipes_emscripten/llvm/build.sh
@@ -1,36 +1,15 @@
 #!/bin/bash
 
-# deactivation script
-TASK="deactivate"
-chmod +x ${RECIPE_DIR}/${TASK}.sh
-source ${RECIPE_DIR}/${TASK}.sh
-
-mkdir native_build
-cd native_build
-export TOOLCHAIN_FILE=$CMAKE_TOOLCHAIN_FILE
-export CROSSCOMPILING_EMULATOR=$CMAKE_CROSSCOMPILING_EMULATOR
-export CMAKE_TOOLCHAIN_FILE=""
-export CMAKE_CROSSCOMPILING_EMULATOR=""
-cmake -DLLVM_ENABLE_PROJECTS=clang -DLLVM_TARGETS_TO_BUILD=host -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER="clang" -DCMAKE_CXX_COMPILER="clang++" ../llvm/
-make llvm-tblgen clang-tblgen -j$(nproc --all)
-export NATIVE_DIR=$PWD/bin/
-cd ..
-
-# activation script
-TASK="activate"
-chmod +x ${RECIPE_DIR}/${TASK}.sh
-source ${RECIPE_DIR}/${TASK}.sh
-
 mkdir build
 cd build
 
 export CMAKE_PREFIX_PATH=$PREFIX
 export CMAKE_SYSTEM_PREFIX_PATH=$PREFIX
-export CMAKE_TOOLCHAIN_FILE=$TOOLCHAIN_FILE
-export CMAKE_CROSSCOMPILING_EMULATOR=$CROSSCOMPILING_EMULATOR
 
 # clear LDFLAGS flags because they contain sWASM_BIGINT
 export LDFLAGS=""
+
+ls -la $BUILD_PREFIX/bin
 
 # Configure step
 emcmake cmake ${CMAKE_ARGS} -S ../llvm -B .         \
@@ -56,7 +35,8 @@ emcmake cmake ${CMAKE_ARGS} -S ../llvm -B .         \
     -DCLANG_ENABLE_BOOTSTRAP=OFF                    \
     -DCLANG_BUILD_TOOLS=OFF                         \
     -DCMAKE_CXX_FLAGS="-Dwait4=__syscall_wait4 -fexceptions" \
-    -DLLVM_NATIVE_TOOL_DIR=$NATIVE_DIR              \
+    -DLLVM_TABLEGEN=$BUILD_PREFIX/bin/llvm-tblgen \
+    -DCLANG_TABLEGEN=$BUILD_PREFIX/bin/clang-tblgen 
 
 # Build and Install step
 emmake make clangInterpreter lldWasm -j16 install
