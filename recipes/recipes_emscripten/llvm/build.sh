0465828f1796c7502af734d833aea3b00df4b05a
kr-2003 <kumar.kr.abhinav@gmail.com>
2025-05-22 10:09:09 +0530

    [de]activation scripts

--- recipes/recipes_emscripten/llvm/build.sh
+++ recipes/recipes_emscripten/llvm/build.sh
@@ -1,22 +1,9 @@
 #!/bin/bash
 
-unset cmake
-
-unset CONDA_FORGE_EMSCRIPTEN_ACTIVATED
-unset PY_SIDE_LD_FLAGV
-unset EM_FORGE_OPTFLAGS
-unset EM_FORGE_DBGFLAGS
-unset EM_FORGE_LDFLAGS_BASE
-unset EM_FORGE_CFLAGS_BASE
-unset EM_FORGE_SIDE_MODULE_LDFLAGS
-unset EM_FORGE_SIDE_MODULE_CFLAGS
-
-# restore
-export LDFLAGS=$EM_OLD_LDFLAGS
-export CFLAGS=$EM_OLD_CFLAGS
-
-unset EM_OLD_LDFLAGS
-unset EM_OLD_CFLAGS
+# deactivation script
+TASK="deactivate"
+chmod +x ${RECIPE_DIR}/${TASK}.sh
+./${RECIPE_DIR}/${TASK}.sh
 
 mkdir native_build
 cd native_build
@@ -29,74 +16,10 @@ make llvm-tblgen clang-tblgen -j$(nproc --all)
 export NATIVE_DIR=$PWD/bin/
 cd ..
 
-
-export EM_OLD_LDFLAGS=$LDFLAGS
-export EM_OLD_CFLAGS=$CFLAGS
-
-
-export CONDA_FORGE_EMSCRIPTEN_ACTIVATED=1
-
-if [ -z ${BUILD_PREFIX+x} ]; then
-    export EMSDK_PYTHON=${CONDA_PREFIX}/bin/python3
-    export PYTHON=${CONDA_PREFIX}/bin/python3
-else
-    export EMSDK_PYTHON=${BUILD_PREFIX}/bin/python3
-    export PYTHON=${BUILD_PREFIX}/bin/python3
-fi
-
-CONDA_EMSDK_DIR=$CONDA_PREFIX/opt/emsdk
-
-export EMSCRIPTEN_VERSION=$PKG_VERSION
-export EMSCRIPTEN_FORGE_EMSDK_DIR=$CONDA_EMSDK_DIR
-
-# clear all prexisting cmake args / CC / CXX / AR / RANLIB
-export CC="emcc"
-export CXX="em++"
-export AR="emar"
-export RANLIB="emranlib"
-
-export CMAKE_ARGS=""
-
-# set the emscripten toolchain
-export CMAKE_ARGS="$CMAKE_ARGS -DCMAKE_TOOLCHAIN_FILE=$CONDA_EMSDK_DIR/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake"
-
-# conda prefix path
-export CMAKE_ARGS="$CMAKE_ARGS -DCMAKE_PREFIX_PATH=$PREFIX"
-
-# install prefix
-export CMAKE_ARGS="$CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=$PREFIX"
-
-# find root path mode package
-export CMAKE_ARGS="$CMAKE_ARGS -DCMAKE_FIND_ROOT_PATH_MODE_PACKAGE=ON"
-
-# fpic
-export CMAKE_ARGS="$CMAKE_ARGS -DCMAKE_POSITION_INDEPENDENT_CODE:BOOL=true"
-
-cmake () {
-    emcmake cmake "$@"
-}
-
-
-# basics
-export EM_FORGE_OPTFLAGS="-O2"
-export EM_FORGE_DBGFLAGS="-g0"
-
-# basics ld
-export EM_FORGE_LDFLAGS_BASE="-s WASM=1 -sWASM_BIGINT -L${PREFIX}/lib"
-export EM_FORGE_LDFLAGS_BASE="${EM_FORGE_OPTFLAGS} ${EM_FORGE_DBGFLAGS} ${EM_FORGE_LDFLAGS_BASE}"
-
-# basics cflags
-export EM_FORGE_CFLAGS_BASE="-fPIC"
-export EM_FORGE_CFLAGS_BASE="${EM_FORGE_OPTFLAGS} ${EM_FORGE_DBGFLAGS} ${EM_FORGE_CFLAGS_BASE}"
-
-# side module
-export EM_FORGE_SIDE_MODULE_LDFLAGS="${EM_FORGE_LDFLAGS_BASE} -s SIDE_MODULE=1"
-export EM_FORGE_SIDE_MODULE_CFLAGS="${EM_FORGE_CFLAGS_BASE} -I${PREFIX}/include"
-
-
-export LDFLAGS="${EM_FORGE_LDFLAGS_BASE} ${LDFLAGS}"
-export CFLAGS="${EM_FORGE_CFLAGS_BASE} ${CFLAGS}"
-
+# activation script
+TASK="activate"
+chmod +x ${RECIPE_DIR}/${TASK}.sh
+./${RECIPE_DIR}/${TASK}.sh
 
 mkdir build
 cd build
@@ -133,8 +56,7 @@ emcmake cmake ${CMAKE_ARGS} -S ../llvm -B .         \
     -DCLANG_ENABLE_BOOTSTRAP=OFF                    \
     -DCLANG_BUILD_TOOLS=OFF                         \
     -DCMAKE_CXX_FLAGS="-Dwait4=__syscall_wait4 -fexceptions" \
-    -DLLVM_TABLEGEN=$NATIVE_DIR/llvm-tblgen         \
-    -DCLANG_TABLEGEN=$NATIVE_DIR/clang-tblgen       \
+    -DLLVM_NATIVE_TOOL_DIR=$NATIVE_DIR              \
 
 # Build and Install step
 emmake make clangInterpreter lldWasm -j16 install
