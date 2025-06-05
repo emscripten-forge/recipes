743e5d344820a547b698a77d73d7ac3c01afc2c5
David Brochart <david.brochart@gmail.com>
2025-06-05 18:06:44 +0200

    Again

--- recipes/recipes_emscripten/lz4-c/build.sh
+++ recipes/recipes_emscripten/lz4-c/build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -ex
 
-export CFLAGS="${CFLAGS} -O3 -fPIC"
+export CFLAGS="${CFLAGS} -O3 -fPIC -DLZ4IO_MULTITHREAD=0"
 export LDFLAGS="${LDFLAGS} -lrt"
 
 # Build
-VERBOSE=1 make -j${CPU_COUNT} PREFIX=${PREFIX} -DLZ4IO_MULTITHREAD=0
+VERBOSE=1 make -j${CPU_COUNT} PREFIX=${PREFIX}
 
 # Install
 make install PREFIX=${PREFIX}
