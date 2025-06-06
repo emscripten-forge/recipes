99fe4b737dc7792b47c70b98462bb582ea25691f
R2 <136319114+emscripten-forge-bot@users.noreply.github.com>
2025-06-06 16:20:23 +0200

    Update pandas from 2.2.3 to 2.3.0 (#2380)
    * Update pandas from 2.2.3 to 2.3.0
    
    * Remove unused patch
    
    * Add package info
    
    * Update tests
    
    * Fix typo
    
    * Fix more typos
    
    ---------
    
    Co-authored-by: emscripten-forge-bot <emscripten-forge-bot@users.noreply.github.com>
    Co-authored-by: Isabel Paredes <isabel.paredes@quantstack.net>

--- recipes/recipes_emscripten/pandas/test_import_pandas.py
+++ recipes/recipes_emscripten/pandas/test_import_pandas.py
@@ -1,4 +1,23 @@
 
 def test_import_pandas():
     import pandas
-    from pandas import read_csv, DataFrame
+    from pandas import read_csv
+
+def test_pandas_series():
+    from pandas import Series
+    import numpy as np
+
+    ser = Series()
+    print("Pandas Series:", ser)
+    data = np.array(['e', 'm', 'f', 'o', 'r', 'g', 'e'])
+    ser = Series(data)
+    print("Pandas Series:", ser)
+
+def test_pandas_dataframe():
+    from pandas import DataFrame
+
+    df = DataFrame()
+    print("Pandas DataFrame:", df)
+    data = ['emscripten', 'forge', 'packages', 'python']
+    df = DataFrame(data)
+    print("Pandas DataFrame:", df)
