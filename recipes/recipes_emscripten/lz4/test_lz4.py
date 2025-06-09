737b30bd416e43e91b19b9ed36dc24c8c773e31d
David Brochart <david.brochart@gmail.com>
2025-06-09 13:59:19 +0200

    Package lz4 (#2396)
    * Package lz4
    
    * Fix test
    
    * Fix YAML
    
    * Improve test
    
    * fixing some problems
    
    * Create build.sh
    
    * Update recipe.yaml
    
    * Update test_lz4.py
    
    ---------
    
    Co-authored-by: Thorsten Beier <derthorstenbeier@gmail.com>

---
+++ recipes/recipes_emscripten/lz4/test_lz4.py
@@ -0,0 +1,9 @@
+import os
+import lz4
+import lz4.frame
+
+def test_lz4():
+    input_data = 20 * 128 * os.urandom(1024)  # Read 20 * 128kb
+    compressed = lz4.frame.compress(input_data)
+    decompressed = lz4.frame.decompress(compressed)
+    assert decompressed == input_data
