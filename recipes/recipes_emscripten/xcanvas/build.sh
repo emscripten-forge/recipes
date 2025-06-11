01789d2b3caab8366313a1f49dd1b4d920416906
anutosh491 <andersonbhat491@gmail.com>
2025-06-11 17:56:15 +0530

    Update xcanvas to be usable with xeus-cpp-lite

--- recipes/recipes_emscripten/xcanvas/build.sh
+++ recipes/recipes_emscripten/xcanvas/build.sh
@@ -3,13 +3,14 @@ mkdir build
 cd build
 
 # Configure step
-cmake ${CMAKE_ARGS} ..              \
-    -GNinja                         \
-    -DCMAKE_BUILD_TYPE=Release      \
-    -DCMAKE_PREFIX_PATH=$PREFIX     \
-    -DCMAKE_INSTALL_PREFIX=$PREFIX  \
-    -DXCANVAS_BUILD_SHARED_LIBS=OFF \
-    -DXCANVAS_BUILD_STATIC_LIBS=ON
+emcmake cmake       \
+  -DCMAKE_BUILD_TYPE=Release     \
+  -DCMAKE_PREFIX_PATH=$PREFIX    \
+  -DCMAKE_INSTALL_PREFIX=$PREFIX \
+  -DXCANVAS_BUILD_SHARED_LIBS=ON \
+  -DXCANVAS_BUILD_STATIC_LIBS=ON \
+  -DCMAKE_FIND_ROOT_PATH=$PREFIX \
+  ..
 
-# Build step
-ninja install
+# Build & Install step
+emmake make -j8 install
