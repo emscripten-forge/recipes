985e5455aab67b51ef6fab5eac81742a41f35784
anutosh491 <andersonbhat491@gmail.com>
2025-06-11 10:16:42 +0530

    Improve xwidgets recipe to provide both static & shared builds

--- recipes/recipes_emscripten/xwidgets/build.sh
+++ recipes/recipes_emscripten/xwidgets/build.sh
@@ -3,13 +3,14 @@ mkdir build
 cd build
 
 # Configure step
-cmake ${CMAKE_ARGS} ..             \
-    -GNinja                        \
-    -DCMAKE_BUILD_TYPE=Release     \
-    -DCMAKE_PREFIX_PATH=$PREFIX    \
-    -DCMAKE_INSTALL_PREFIX=$PREFIX \
-    -DXWIDGETS_BUILD_SHARED_LIBS=OFF \
-    -DXWIDGETS_BUILD_STATIC_LIBS=ON  \
+emcmake cmake       \
+  -DCMAKE_BUILD_TYPE=Release     \
+  -DCMAKE_PREFIX_PATH=$PREFIX   \
+  -DCMAKE_INSTALL_PREFIX=$PREFIX \
+  -DXWIDGETS_BUILD_SHARED_LIBS=ON \
+  -DXWIDGETS_BUILD_STATIC_LIBS=ON \
+  -DCMAKE_FIND_ROOT_PATH=$PREFIX \
+  ..
 
-# Build step
-ninja install
+# Build & Install step
+emmake make -j8 install
