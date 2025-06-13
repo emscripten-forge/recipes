a3efae2fe4deb8f7f405183fef064d6f4eab46ef
Anutosh Bhat <andersonbhat491@gmail.com>
2025-06-13 16:41:11 +0530

    Replicate CppInterOp's llvm build (#2423)

--- recipes/recipes_emscripten/llvm/build.sh
+++ recipes/recipes_emscripten/llvm/build.sh
@@ -6,6 +6,16 @@ cd build
 export CMAKE_PREFIX_PATH=$PREFIX
 export CMAKE_SYSTEM_PREFIX_PATH=$PREFIX
 
+unset EM_FORGE_OPTFLAGS
+unset EM_FORGE_DBGFLAGS
+unset EM_FORGE_LDFLAGS_BASE
+unset EM_FORGE_CFLAGS_BASE
+unset EM_FORGE_SIDE_MODULE_LDFLAGS
+unset EM_FORGE_SIDE_MODULE_CFLAGS
+
+unset CFLAGS
+unset LDFLAGS
+
 # Configure step
 emcmake cmake -S ../llvm -B .         \
     -DCMAKE_BUILD_TYPE=Release                      \
@@ -14,8 +24,6 @@ emcmake cmake -S ../llvm -B .         \
     -DLLVM_HOST_TRIPLE=wasm32-unknown-emscripten    \
     -DLLVM_TARGETS_TO_BUILD="WebAssembly"           \
     -DLLVM_ENABLE_ASSERTIONS=ON                     \
-    -DLLVM_ENABLE_EH=ON                             \
-    -DLLVM_ENABLE_RTTI=ON                           \
     -DLLVM_INCLUDE_BENCHMARKS=OFF                   \
     -DLLVM_INCLUDE_EXAMPLES=OFF                     \
     -DLLVM_INCLUDE_TESTS=OFF                        \
@@ -29,9 +37,8 @@ emcmake cmake -S ../llvm -B .         \
     -DCLANG_ENABLE_ARCMT=OFF                        \
     -DCLANG_ENABLE_BOOTSTRAP=OFF                    \
     -DCLANG_BUILD_TOOLS=OFF                         \
-    -DCMAKE_CXX_FLAGS="-Dwait4=__syscall_wait4 -fexceptions" \
-    -DLLVM_TABLEGEN=$BUILD_PREFIX/bin/llvm-tblgen \
-    -DCLANG_TABLEGEN=$BUILD_PREFIX/bin/clang-tblgen 
+    -DCMAKE_CXX_FLAGS="-Dwait4=__syscall_wait4" \
+    -DLLVM_NATIVE_TOOL_DIR=$BUILD_PREFIX/bin/
 
 # Build and Install step
 emmake make clangInterpreter lldWasm -j16 install
