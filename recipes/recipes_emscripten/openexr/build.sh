1e2efeea8ee9c9537d1bbbceafcb62bf30c2aa8e
Isabel Paredes <isabel.paredes@quantstack.net>
2025-06-19 11:54:06 +0200

    Disable threads

--- recipes/recipes_emscripten/openexr/build.sh
+++ recipes/recipes_emscripten/openexr/build.sh
@@ -5,18 +5,13 @@ set -xeuo pipefail
 mkdir build
 cd build
 
-# CXXFLAGS?
-
-# export CFLAGS="$CFLAGS -D__STDC_FORMAT_MACROS"
-# export CXXFLAGS="$CXXFLAGS -D__STDC_FORMAT_MACROS -D_LIBCPP_DISABLE_AVAILABILITY"
-
 cmake \
     ${CMAKE_ARGS} \
+    -DCMAKE_HAVE_LIBC_PTHREAD=OFF \
     -DBUILD_SHARED_LIBS=OFF \
     -DBUILD_STATIC_LIBS=ON \
     -DOPENEXR_LIB_SUFFIX="" \
     ..
-
 
 make -j2
 make install
