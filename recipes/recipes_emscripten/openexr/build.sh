74d98cf26d3c43df6de1224acbb9fd613adf89ca
R2 <136319114+emscripten-forge-bot@users.noreply.github.com>
2025-06-19 12:22:12 +0200

    Update openexr from 3.3.3 to 3.3.4 (#2395)
    Co-authored-by: emscripten-forge-bot <emscripten-forge-bot@users.noreply.github.com>
    Co-authored-by: Isabel Paredes <isabel.paredes@quantstack.net>

--- recipes/recipes_emscripten/openexr/build.sh
+++ recipes/recipes_emscripten/openexr/build.sh
@@ -5,18 +5,13 @@ set -xeuo pipefail
 mkdir build
 cd build
 
-# CXXFLAGS?
-
-# export CFLAGS="$CFLAGS -D__STDC_FORMAT_MACROS"
-# export CXXFLAGS="$CXXFLAGS -D__STDC_FORMAT_MACROS -D_LIBCPP_DISABLE_AVAILABILITY"
-
 cmake \
     ${CMAKE_ARGS} \
     -DBUILD_SHARED_LIBS=OFF \
     -DBUILD_STATIC_LIBS=ON \
     -DOPENEXR_LIB_SUFFIX="" \
+    -DOPENEXR_ENABLE_THREADING=OFF \
     ..
-
 
 make -j2
 make install
