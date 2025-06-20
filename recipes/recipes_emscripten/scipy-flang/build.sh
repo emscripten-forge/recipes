188a62bbdd6621a94ee980f2516a152ec4ea8052
Isabel Paredes <isabel.paredes@quantstack.net>
2025-06-20 15:42:09 +0200

    Install custom llvm

--- recipes/recipes_emscripten/scipy-flang/build.sh
+++ recipes/recipes_emscripten/scipy-flang/build.sh
@@ -1,5 +1,17 @@
 set -ex
 
+LLVM_PKG_URL="https://github.com/IsabelParedes/llvm-project/releases/download/v19.1.7_emscripten-wasm32/llvm_emscripten-wasm32-19.1.7-h919795d_0.tar.bz2"
+LLVM_PKG="llvm.tar.bz2"
+export LLVM_DIR=$PWD/llvm
+
+wget -O "$LLVM_PKG" "$LLVM_PKG_URL"
+mkdir -p $LLVM_DIR
+tar -xjvf $LLVM_PKG -C $LLVM_DIR --exclude='info/*' --exclude='share/*' --exclude='libexec/*'
+
+export EM_LLVM_ROOT=$LLVM_DIR
+
+echo "LLVM installation complete."
+
 # https://github.com/mesonbuild/meson/blob/e542901af6e30865715d3c3c18f703910a096ec0/mesonbuild/backend/ninjabackend.py#L94
 # Prevent from using response file. The response file that meson generates is not compatible to pyodide-build
 export MESON_RSP_THRESHOLD=131072
@@ -7,21 +19,18 @@ export MESON_RSP_THRESHOLD=131072
 export CFLAGS="-I$PREFIX/include/python3.13 $CFLAGS"
 export CXXFLAGS="-I$PREFIX/include/python3.13 $CXXFLAGS"
 
-# find . -name "meson.build" -type f -exec sed -i "s/link_language: 'fortran'/link_language: 'c'/g" {} +
-
 cp $RECIPE_DIR/meson_linkers.py $BUILD_PREFIX/lib/python3.13/site-packages/mesonbuild/linkers/linkers.py
 
 export LDFLAGS="$(echo "${LDFLAGS}" |  sed -E 's/-s +/-s/g')"
 
-cp $RECIPE_DIR/flang-new-wrapper $BUILD_PREFIX/bin/flang-new-wrapper
+cp $RECIPE_DIR/flang-new-wrapper $LLVM_DIR/bin/flang-new-wrapper
 
-export EM_LLVM_ROOT=$BUILD_PREFIX
-export FC=$BUILD_PREFIX/bin/flang-new-wrapper
+export FC=$LLVM_DIR/bin/flang-new-wrapper
 export FFLAGS="-g --target=wasm32-unknown-emscripten -fPIC"
 export FCLIBS="-lFortranRuntime"
 
 # export MESON_FORCE_BACKTRACE=1
-${PYTHON} -m pip install . -vvv --no-deps --no-build-isolation \
+${PYTHON} -m pip install . ${PIP_ARGS} --no-build-isolation \
     -Csetup-args="--cross-file=$RECIPE_DIR/emscripten.meson.cross" \
     -Csetup-args="-Dfortran_std=none" \
     -Csetup-args="-Duse-pythran=false" \
