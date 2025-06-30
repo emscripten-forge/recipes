0f1d5e41e82f13bffb8d80f21b17e0710ad5dcb3
Julien Jerphanion <git@jjerphan.xyz>
2025-06-30 14:13:58 +0200

    Add narwhals
    Signed-off-by: Julien Jerphanion <git@jjerphan.xyz>

---
+++ recipes/recipes_emscripten/narwhals/test_import_narwhals.py
@@ -0,0 +1,6 @@
+def test_import_narwhals():
+    import narwhals
+
+    # Test basic import functionality
+    print("Successfully imported narwhals")
+    print(f"narwhals version: {narwhals.__version__}") 
