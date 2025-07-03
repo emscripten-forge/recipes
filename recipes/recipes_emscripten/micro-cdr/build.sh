dcef1ef76b0330036cc5703e6850f45d3dabde06
Tobias Fischer <info@tobiasfischer.info>
2025-07-03 15:56:15 +1000

    Fix micro-cdr build

--- recipes/recipes_emscripten/micro-cdr/build.sh
+++ recipes/recipes_emscripten/micro-cdr/build.sh
@@ -1,5 +1,5 @@
 mkdir build && cd build
 # Configure step
-emcmake cmake -DCMAKE_BUILD_TYPE=Release -H. -B ./ -S ../ ${CMAKE_ARGS}
+emcmake cmake -DCMAKE_BUILD_TYPE=Release -S .. -B . -DUCDR_SUPERBUILD=OFF -DUCDR_ISOLATED_INSTALL=OFF ${CMAKE_ARGS}
 # Build step
 emmake make install -j8
