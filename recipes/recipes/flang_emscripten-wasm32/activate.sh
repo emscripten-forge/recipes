19e97bd0500edb0f875a42b2747d1c8f249215fb
Isabel Paredes <isabel.paredes@quantstack.net>
2025-07-10 16:09:54 +0200

    Fix fortran lib typo

--- recipes/recipes/flang_emscripten-wasm32/activate.sh
+++ recipes/recipes/flang_emscripten-wasm32/activate.sh
@@ -18,7 +18,7 @@ export FLANG=flang-new
 
 export FFLAGS="--target=wasm32-unknown-emscripten"
 export FPICFLAGS="-fPIC"
-export FCLIBS="-lFortranRuntime"
+export FLIBS="-lFortranRuntime"
 
 rm $BUILD_PREFIX/bin/clang || true
 ln -s $BUILD_PREFIX/bin/clang-20 $BUILD_PREFIX/bin/clang # links to emsdk clang
