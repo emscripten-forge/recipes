6cc89374680977aa28485893b103fa9321069b3f
Isabel Paredes <isabel.paredes@quantstack.net>
2025-07-10 15:42:02 +0200

    Add shared lib

--- recipes/recipes_emscripten/openblas-flang/build.sh
+++ recipes/recipes_emscripten/openblas-flang/build.sh
@@ -14,3 +14,6 @@ emmake make install PREFIX=$PREFIX NO_SHARED=1
 
 mkdir -p $PREFIX/lib
 cp libopenblas.a $PREFIX/lib
+
+emcc libopenblas.a -s SIDE_MODULE=1 -o libopenblas.so
+cp libopenblas.so $PREFIX/lib
