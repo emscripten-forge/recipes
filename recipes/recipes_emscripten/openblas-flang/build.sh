84d9ea959d2ff48e7db0bd895a0dcca356da9449
Isabel Paredes <isabel.paredes@quantstack.net>
2025-07-10 14:58:37 +0200

    Working build with static lib

--- recipes/recipes_emscripten/openblas-flang/build.sh
+++ recipes/recipes_emscripten/openblas-flang/build.sh
@@ -2,16 +2,15 @@
 
 set -ex
 
-emmake make libs shared \
+emmake make libs netlib \
     CC=emcc \
     HOSTCC=gcc \
     TARGET=RISCV64_GENERIC \
-    NOFORTRAN=1 \
     NO_LAPACKE=1 \
     USE_THREAD=0 \
     LDFLAGS="$EM_FORGE_SIDE_MODULE_LDFLAGS"
 
-emmake make install PREFIX=$PREFIX
+emmake make install PREFIX=$PREFIX NO_SHARED=1
 
 mkdir -p $PREFIX/lib
 cp libopenblas.a $PREFIX/lib
