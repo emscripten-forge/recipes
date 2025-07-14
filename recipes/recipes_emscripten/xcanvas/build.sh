7e8750204bea36dd6dfa83e578b57ae77866d4d8
R2 <136319114+emscripten-forge-bot@users.noreply.github.com>
2025-07-14 13:07:03 +0200

    Update xcanvas from 0.5.1 to 0.6.0 (#2566)
    * Update xcanvas from 0.5.1 to 0.6.0
    
    ---------
    
    Co-authored-by: emscripten-forge-bot <emscripten-forge-bot@users.noreply.github.com>
    Co-authored-by: Anutosh Bhat <andersonbhat491@gmail.com>

--- recipes/recipes_emscripten/xcanvas/build.sh
+++ recipes/recipes_emscripten/xcanvas/build.sh
@@ -1,16 +1,22 @@
-
 mkdir build
 cd build
 
 # Configure step
 emcmake cmake       \
   -DCMAKE_BUILD_TYPE=Release     \
-  -DCMAKE_PREFIX_PATH=$PREFIX    \
+  -DCMAKE_PREFIX_PATH=$PREFIX   \
   -DCMAKE_INSTALL_PREFIX=$PREFIX \
-  -DXCANVAS_BUILD_SHARED_LIBS=ON \
+  -DXCANVAS_BUILD_SHARED_LIBS=OFF \
   -DXCANVAS_BUILD_STATIC_LIBS=ON \
   -DCMAKE_FIND_ROOT_PATH=$PREFIX \
   ..
 
-# Build & Install step
+# Build step
+emmake make -j8
+
+emcc libxcanvas.a $EM_FORGE_SIDE_MODULE_LDFLAGS -o libxcanvas.so
+
+# Install step
 emmake make -j8 install
+
+cp libxcanvas.so $PREFIX/lib/
