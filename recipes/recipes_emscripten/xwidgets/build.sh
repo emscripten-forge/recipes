8d6682bcbe479e219678bc118ac66fbdd35a763d
Anutosh Bhat <andersonbhat491@gmail.com>
2025-07-14 15:31:35 +0530

    Update build.sh

--- recipes/recipes_emscripten/xwidgets/build.sh
+++ recipes/recipes_emscripten/xwidgets/build.sh
@@ -7,10 +7,14 @@ emcmake cmake       \
   -DCMAKE_BUILD_TYPE=Release     \
   -DCMAKE_PREFIX_PATH=$PREFIX   \
   -DCMAKE_INSTALL_PREFIX=$PREFIX \
-  -DXWIDGETS_BUILD_SHARED_LIBS=ON \
+  -DXWIDGETS_BUILD_SHARED_LIBS=OFF \
   -DXWIDGETS_BUILD_STATIC_LIBS=ON \
   -DCMAKE_FIND_ROOT_PATH=$PREFIX \
   ..
 
+emcc libxwidgets.a $EM_FORGE_SIDE_MODULE_LDFLAGS -o libxwidgets.so
+
 # Build & Install step
 emmake make -j8 install
+
+cp libxwidgets.so $PREFIX/lib/
