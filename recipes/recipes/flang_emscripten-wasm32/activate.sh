04b3be8fd0ce5e0acf54ad0b4ccb0036956c6574
Isabel Paredes <isabel.paredes@quantstack.net>
2025-07-21 13:33:18 +0200

    Fix clang in flang

--- recipes/recipes/flang_emscripten-wasm32/activate.sh
+++ recipes/recipes/flang_emscripten-wasm32/activate.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-# # Using flang as a WASM cross-compiler
-# # https://github.com/serge-sans-paille/llvm-project/blob/feature/flang-wasm/README.wasm.md
-# # https://github.com/conda-forge/flang-feedstock/pull/69
+# Using flang as a WASM cross-compiler
+# https://github.com/serge-sans-paille/llvm-project/blob/feature/flang-wasm/README.wasm.md
+# https://github.com/conda-forge/flang-feedstock/pull/69
 if [ ! -f "$BUILD_PREFIX/bin/flang-new" ]; then
     micromamba install -p $BUILD_PREFIX \
         conda-forge/label/emscripten::flang==$PKG_VERSION \
         -y --no-channel-priority
+
+    rm $BUILD_PREFIX/bin/clang-20 || true
+    ln -s $BUILD_PREFIX/opt/emsdk/upstream/bin/clang $BUILD_PREFIX/bin/clang-20
+    rm -r $BUILD_PREFIX/lib/clang || true
+    ln -s $BUILD_PREFIX/opt/emsdk/upstream/lib/clang $BUILD_PREFIX/lib/clang
 fi
 
 export FC=flang-new
@@ -19,6 +24,3 @@ export FLANG=flang-new
 export FFLAGS="--target=wasm32-unknown-emscripten"
 export FPICFLAGS="-fPIC"
 export FCLIBS="-lFortranRuntime"
-
-rm $BUILD_PREFIX/bin/clang || true
-ln -s $BUILD_PREFIX/bin/clang-20 $BUILD_PREFIX/bin/clang # links to emsdk clang
