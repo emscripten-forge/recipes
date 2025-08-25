e7cb59c153ffe80d4346221376453556e25c14c9
Pablo Winant <pablo.winant@gmail.com>
2025-08-25 21:15:01 +0200

    Trying to enable threads.

--- recipes/recipes_emscripten/dynare-preprocessor-pylib/build.sh
+++ recipes/recipes_emscripten/dynare-preprocessor-pylib/build.sh
@@ -6,8 +6,8 @@ cp $RECIPE_DIR/meson.build src/meson.build
 
 meson setup --prefix=$PREFIX --bindir=$PREFIX/bin --libdir=$PREFIX/lib --includedir=$PREFIX/include \
     --buildtype=release build_preproc \
-    -Dcpp_args="-w  -Wno-enum-constexpr-conversion -I${PREFIX}/include/pybind11 -sSIDE_MODULE=1 -s WASM_BIGINT -fexceptions"  \
-    -Dcpp_link_args="-w  -Wno-enum-constexpr-conversion -I${PREFIX}/include/pybind11 -sSIDE_MODULE=1 -s WASM_BIGINT -fexceptions" \
+    -Dcpp_args="-w -fexperimental-library -Wno-enum-constexpr-conversion -sSIDE_MODULE=1 -s WASM_BIGINT -fexceptions -s USE_PTHREADS=1 -std=c++20 -sPTHREAD_POOL_SIZE=2"  \
+    -Dcpp_link_args="-w -fexperimental-library  -Wno-enum-constexpr-conversion -sSIDE_MODULE=1 -s WASM_BIGINT -fexceptions -s USE_PTHREADS=1 -std=c++20 -sPTHREAD_POOL_SIZE=2" \
     --cross-file=$RECIPE_DIR/wasm_32.ini
 
 meson compile -C build_preproc
