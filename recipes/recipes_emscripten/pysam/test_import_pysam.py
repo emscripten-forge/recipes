52166534f2bcc6314f6e607bfbccfe5ec19d2d69
Thorsten Beier <derthorstenbeier@gmail.com>
2025-09-03 10:08:59 +0200

    Refactor import test for pysam into a function

--- recipes/recipes_emscripten/pysam/test_import_pysam.py
+++ recipes/recipes_emscripten/pysam/test_import_pysam.py
@@ -1,4 +1,6 @@
-import pysam
 
-# Test basic import and version
-print(f"pysam version: {pysam.__version__}")
+def test_import():
+  import pysam
+  
+  # Test basic import and version
+  print(f"pysam version: {pysam.__version__}")
