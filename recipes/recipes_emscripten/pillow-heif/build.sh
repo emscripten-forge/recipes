4334b5d589eb1f382af169c7805ddb63e28743fb
Isabel Paredes <isabel.paredes@quantstack.net>
2025-09-05 13:01:45 +0200

    Add patch

--- recipes/recipes_emscripten/pillow-heif/build.sh
+++ recipes/recipes_emscripten/pillow-heif/build.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
 # Set compiler flags to avoid system headers and use only conda environment
-export CFLAGS="${CFLAGS} -nostdinc -I${PREFIX}/include -I${BUILD_PREFIX}/include"
-export CPPFLAGS="${CPPFLAGS} -nostdinc -I${PREFIX}/include -I${BUILD_PREFIX}/include"
-
-# Add Python and numpy include paths
-export CFLAGS="${CFLAGS} -I${BUILD_PREFIX}/include/python${PY_VER} -I${BUILD_PREFIX}/lib/python${PY_VER}/site-packages/numpy/_core/include/"
+export CFLAGS="${CFLAGS} -I${PREFIX}/include"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
 
 # Ensure libheif is found
 export CFLAGS="${CFLAGS} -I${PREFIX}/include/libheif"
