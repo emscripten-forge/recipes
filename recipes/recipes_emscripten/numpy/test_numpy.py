d4330c53c5b5e7bf8b907069eaaf1394549bead8
Thorsten Beier <derthorstenbeier@gmail.com>
2025-09-06 09:37:56 +0200

    Refactor test_numpy.py to use np alias and add tests

--- recipes/recipes_emscripten/numpy/test_numpy.py
+++ recipes/recipes_emscripten/numpy/test_numpy.py
@@ -1,5 +1,31 @@
 def test_numpy():
-    import numpy
+    import numpy as np
 
-    ones = numpy.ones(shape=[2,3])
+    ones = np.ones(shape=[2,3])
     assert ones.shape == (2,3)
+    
+        
+
+    # 1. Array creation & dtype
+    a = np.arange(6).reshape(2, 3)
+    print("Array a:\n", a, "dtype:", a.dtype)
+
+    # 2. Elementwise ops & broadcasting
+    b = np.ones((2, 1))
+    print("a + b (broadcasted):\n", a + b)
+
+    # 3. Reduction
+    print("Sum of a:", np.sum(a))
+
+    # 4. Matrix multiplication (BLAS check)
+    c = np.array([[1, 2], [3, 4]])
+    d = np.array([[5, 6], [7, 8]])
+    print("Matrix product c @ d:\n", c @ d)
+
+    # 5. Linear algebra (LAPACK check)
+    eigvals, eigvecs = np.linalg.eig(c)
+    print("Eigenvalues of c:", eigvals)
+
+    # 6. Random numbers
+    rng = np.random.default_rng(42)
+    print("Random sample:", rng.random(3))
