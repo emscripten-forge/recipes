c028ca1c1e4310d271253966ecd0a6ee4eb93178
Isabel Paredes <isabel.paredes@quantstack.net>
2025-09-11 11:05:07 +0200

    Update test

--- recipes/recipes_emscripten/ruamel.yaml/test_import_ruamel_yaml.py
+++ recipes/recipes_emscripten/ruamel.yaml/test_import_ruamel_yaml.py
@@ -1,15 +1,13 @@
 def test_import_ruamel_yaml():
     import ruamel.yaml
-    
-    # Test basic YAML operations
+    from io import StringIO
+
+    # Test basic YAML loading
     yaml = ruamel.yaml.YAML()
-    
-    # Test loading/dumping
-    data = {'key': 'value', 'number': 42}
-    yaml_str = yaml.dump(data, stream=None)
-    
-    # Test that we can load it back
+
+    # Test loading YAML string
+    yaml_str = "key: value\nnumber: 42\n"
     loaded_data = yaml.load(yaml_str)
+
     assert loaded_data['key'] == 'value'
     assert loaded_data['number'] == 42
-    
