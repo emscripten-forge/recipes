2ec2307688bad23107b7cd3d838be6ee1217667a
martinRenou <martin.renou@gmail.com>
2025-09-12 16:26:52 +0200

    zlib: rebuild with emcmake

--- recipes/recipes_emscripten/zlib/build.sh
+++ recipes/recipes_emscripten/zlib/build.sh
@@ -3,11 +3,11 @@ cp $RECIPE_DIR/patches/CMakeLists.txt .
 mkdir build
 cd build
 
-export CMAKE_PREFIX_PATH=$PREFIX 
-export CMAKE_SYSTEM_PREFIX_PATH=$PREFIX 
+export CMAKE_PREFIX_PATH=$PREFIX
+export CMAKE_SYSTEM_PREFIX_PATH=$PREFIX
 
 # Configure step
-cmake ${CMAKE_ARGS} ..              \
+emcmake ${CMAKE_ARGS} ..              \
     -GNinja                         \
     -DCMAKE_BUILD_TYPE=Release      \
     -DCMAKE_PREFIX_PATH=$PREFIX     \
