ac7e80e536d36e059bfad47ae5a8d2ded2209111
Isabel Paredes <isabel.paredes@quantstack.net>
2025-09-15 14:21:24 +0200

    Add emcmake
    Redundant but for consistency

--- recipes/recipes_emscripten/zlib/build.sh
+++ recipes/recipes_emscripten/zlib/build.sh
@@ -2,7 +2,7 @@ mkdir build
 cd build
 
 # Configure step
-cmake ${CMAKE_ARGS} ..              \
+emcmake cmake ${CMAKE_ARGS} ..      \
     -GNinja                         \
     -DCMAKE_BUILD_TYPE=Release      \
     -DCMAKE_PREFIX_PATH=$PREFIX     \
