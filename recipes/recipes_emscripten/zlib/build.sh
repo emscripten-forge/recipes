cf93226d8a56ca47d700945988a3540b0cfeb57a
Isabel Paredes <isabel.paredes@quantstack.net>
2025-09-15 12:47:27 +0200

    Update zlib patches

--- recipes/recipes_emscripten/zlib/build.sh
+++ recipes/recipes_emscripten/zlib/build.sh
@@ -1,10 +1,5 @@
-cp $RECIPE_DIR/patches/CMakeLists.txt .
-
 mkdir build
 cd build
-
-export CMAKE_PREFIX_PATH=$PREFIX 
-export CMAKE_SYSTEM_PREFIX_PATH=$PREFIX 
 
 # Configure step
 cmake ${CMAKE_ARGS} ..              \
@@ -12,15 +7,11 @@ cmake ${CMAKE_ARGS} ..              \
     -DCMAKE_BUILD_TYPE=Release      \
     -DCMAKE_PREFIX_PATH=$PREFIX     \
     -DCMAKE_INSTALL_PREFIX=$PREFIX  \
-    -DBUILD_SHARED_LIBS=OFF \
-    -DBZIP2_SKIP_TOOLS=ON \
-    -DWITH_NODE_TESTS=OFF \
+    -DZLIB_BUILD_EXAMPLES=OFF       \
+    -DBUILD_SHARED_LIBS=OFF         \
     -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
+    -DCMAKE_VERBOSE_MAKEFILE=ON     \
     -DCMAKE_PROJECT_INCLUDE=$RECIPE_DIR/overwriteProp.cmake
 
-
 # Build step
-ninja install
-
-
-cp $PREFIX/lib/libz_static.a  $PREFIX/lib/libz.a
+ninja -v install
