8eb6a3e8f020419415e30f8c48420c1b2125c7e8
Isabel Paredes <isabel.paredes@quantstack.net>
2025-09-15 14:31:59 +0200

    Repackage zlib (#2955)
    * Update zlib patches
    
    * Remove patch
    
    * Add emcmake
    
    Redundant but for consistency
    
    * Remove misleading flag

--- recipes/recipes_emscripten/zlib/build.sh
+++ recipes/recipes_emscripten/zlib/build.sh
@@ -1,26 +1,16 @@
-cp $RECIPE_DIR/patches/CMakeLists.txt .
-
 mkdir build
 cd build
 
-export CMAKE_PREFIX_PATH=$PREFIX 
-export CMAKE_SYSTEM_PREFIX_PATH=$PREFIX 
-
 # Configure step
-cmake ${CMAKE_ARGS} ..              \
+emcmake cmake ${CMAKE_ARGS} ..      \
     -GNinja                         \
     -DCMAKE_BUILD_TYPE=Release      \
     -DCMAKE_PREFIX_PATH=$PREFIX     \
     -DCMAKE_INSTALL_PREFIX=$PREFIX  \
-    -DBUILD_SHARED_LIBS=OFF \
-    -DBZIP2_SKIP_TOOLS=ON \
-    -DWITH_NODE_TESTS=OFF \
+    -DZLIB_BUILD_EXAMPLES=OFF       \
     -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
+    -DCMAKE_VERBOSE_MAKEFILE=ON     \
     -DCMAKE_PROJECT_INCLUDE=$RECIPE_DIR/overwriteProp.cmake
 
-
 # Build step
-ninja install
-
-
-cp $PREFIX/lib/libz_static.a  $PREFIX/lib/libz.a
+ninja -v install
