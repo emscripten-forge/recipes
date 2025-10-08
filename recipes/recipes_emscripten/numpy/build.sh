98c63da9fe8a7b4b94e9f1430732cfd4615cb7bf
DerThorsten <derthorstenbeier@gmail.com>
2025-10-08 11:40:05 +0200

    more 4x

--- recipes/recipes_emscripten/numpy/build.sh
+++ recipes/recipes_emscripten/numpy/build.sh
@@ -4,9 +4,9 @@ echo "PYTHON"
 
 rm -r -f branding
 
-export CFLAGS="$CFLAGS -Wno-return-type -Wno-implicit-function-declaration"
-export MESON_CROSS_FILE=$RECIPE_DIR/emscripten.meson.cross
-export LDFLAGS="$LDFLAGS -sWASM_BIGINT"
+export CFLAGS="$CFLAGS -Wno-return-type -Wno-implicit-function-declaration -msimd128 -fwasm-exceptions -s SUPPORT_LONGJMP"
+export MESON_CROSS_FILE=$RECIPE_DIR/emscripten.meson.cross 
+export LDFLAGS="$LDFLAGS -sWASM_BIGINT 	-s WASM_BIGINT -fwasm-exceptions -s SUPPORT_LONGJMP"
 
 cp $RECIPE_DIR/config/config.h.in  numpy/_core/config.h.in
 # 
@@ -14,6 +14,11 @@ cp $RECIPE_DIR/config/config.h.in  numpy/_core/config.h.in
 # otherwise "cython" is not properly executable
 echo "add shebang to cython file"
 sed -i '1i#!/usr/bin/env python' $BUILD_PREFIX/bin/cython
+
+
+
+# replace -fexceptions with -fwasm-exceptions in numpy/_core
+sed -i 's/-fexceptions/-fwasm-exceptions/g' numpy/_core/meson.build
 
 
 MESON_ARGS="-Dhave_backtrace=false" ${PYTHON} -m pip install . -vvv --no-deps --no-build-isolation \
