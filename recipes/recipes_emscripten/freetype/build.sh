49130d2ffae55099d647d241dbfdb567a2627d78
DerThorsten <derthorstenbeier@gmail.com>
2025-10-16 09:59:04 +0200

    wip

--- recipes/recipes_emscripten/freetype/build.sh
+++ recipes/recipes_emscripten/freetype/build.sh
@@ -10,6 +10,7 @@ export CXXFLAGS="${CXXFLAGS} -sRELOCATABLE=1 -fwasm-exceptions"
 # for use cases like cairo, pango etc.
 emcmake cmake .. ${CMAKE_ARGS} \
     -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+    -DFT_DYNAMIC_HARFBUZZ=OFF \
     -DCMAKE_C_FLAGS="-matomics -mbulk-memory -fwasm-exceptions" \
     -DCMAKE_CXX_FLAGS="-matomics -mbulk-memory -fwasm-exceptions" \
     -DCMAKE_EXE_LINKER_FLAGS="-matomics -mbulk-memory -fwasm-exceptions"
