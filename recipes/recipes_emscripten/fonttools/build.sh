1359a2ea92ee4f32be658413ad70de426085b003
DerThorsten <derthorstenbeier@gmail.com>
2025-10-16 09:43:11 +0200

    wip

--- recipes/recipes_emscripten/fonttools/build.sh
+++ recipes/recipes_emscripten/fonttools/build.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
+
+
+
+export CFLAGS="${CFLAGS} -sRELOCATABLE=1 -fwasm-exceptions"
+export LDFLAGS="${LDFLAGS} -sRELOCATABLE=1 -fwasm-exceptions"
+export CXXFLAGS="${CXXFLAGS} -sRELOCATABLE=1 -fwasm-exceptions"
+
 ${PYTHON} -m pip install . --no-deps -v
--- recipes/recipes_emscripten/freetype/build.sh
+++ recipes/recipes_emscripten/freetype/build.sh
@@ -1,9 +1,17 @@
 mkdir build && cd build
 
+
+
+export CFLAGS="${CFLAGS} -sRELOCATABLE=1 -fwasm-exceptions"
+export LDFLAGS="${LDFLAGS} -sRELOCATABLE=1 -fwasm-exceptions"
+export CXXFLAGS="${CXXFLAGS} -sRELOCATABLE=1 -fwasm-exceptions"
+
 # NOTE: freetype needs to be compiled with atomics/bulk-memory features
 # for use cases like cairo, pango etc.
 emcmake cmake .. ${CMAKE_ARGS} \
     -DCMAKE_INSTALL_PREFIX=${PREFIX} \
-    -DCMAKE_C_FLAGS="-matomics -mbulk-memory"
+    -DCMAKE_C_FLAGS="-matomics -mbulk-memory -fwasm-exceptions" \
+    -DCMAKE_CXX_FLAGS="-matomics -mbulk-memory -fwasm-exceptions" \
+    -DCMAKE_EXE_LINKER_FLAGS="-matomics -mbulk-memory -fwasm-exceptions"
 
 make install -j${CPU_COUNT}
