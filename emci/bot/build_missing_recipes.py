2128877bb1910c0bcacc75688b9e477daa98bcf4
Thorsten Beier <derthorstenbeier@gmail.com>
2025-10-17 10:16:02 +0200

    use correct branch for making prs
    Add branch switching logic for PR handling.

--- emci/bot/build_missing_recipes.py
+++ emci/bot/build_missing_recipes.py
@@ -70,8 +70,23 @@ def build_missing_recipes(recipes_dir, pr_target_branch, use_bot=True, pr_limit=
             user_ctx = empty_context_manager
 
 
-    # get all opened PRs
     with user_ctx():
+
+
+        current_branch_name = get_current_branch_name()
+        if current_branch_name == pr_target_branch:
+            print(f"Already on target branch {pr_target_branch}")
+        else:
+            print(f"switching from {current_branch_name} to {pr_target_branch}")
+            # switch to the target branch
+            subprocess.run(['git', 'stash'], check=False)
+            print(f"fetch {pr_target_branch}")
+            subprocess.check_output(['git', 'fetch', 'origin', pr_target_branch])
+            print(f"checkout {pr_target_branch}")
+            subprocess.check_output(['git', 'checkout', pr_target_branch])
+            print("checkout done")
+
+
 
         # download repodata
         repodata_url = branch_to_repodata_mapping.get(pr_target_branch)
