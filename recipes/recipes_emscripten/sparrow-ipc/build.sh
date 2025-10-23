0bfa5c23810dd834b1bef3ed21df6f6efaafba8a
Alexis Placet <alexis.placet@quantstack.net>
2025-10-23 13:32:14 +0200

    Fix shared build, set flatbuffer version in variant.yaml

--- recipes/recipes_emscripten/sparrow-ipc/build.sh
+++ recipes/recipes_emscripten/sparrow-ipc/build.sh
@@ -12,20 +12,15 @@ emcmake cmake .. \
   -DCMAKE_BUILD_TYPE=Release \
   -DCMAKE_FIND_ROOT_PATH=$PREFIX \
   -DCMAKE_INSTALL_PREFIX=$PREFIX \
-  -Dflatbuffers_DIR=$PREFIX/lib/cmake/flatbuffers
+  -DSPARROW_IPC_BUILD_SHARED=ON \
+  -Dflatbuffers_DIR=$PREFIX/lib/cmake/flatbuffers \
+  -DCMAKE_PROJECT_INCLUDE=${RECIPE_DIR}/overwriteProp.cmake
 
 # Build step
 emmake make -j8
 
-emcc libsparrow-ipc.a \
-  $EM_FORGE_SIDE_MODULE_LDFLAGS \
-  -o libsparrow-ipc.so
-
 # Install step
 emmake make install -j8
-
-# Manually install the shared library
-cp libsparrow-ipc.so "$PREFIX/lib/"
 
 cp -r ../include/sparrow_ipc "$PREFIX/include/"
 rm -rf "$PREFIX/include/include/sparrow_ipc"
