f2ceb6c349411539787dd162d1409d9743130773
DerThorsten <derthorstenbeier@gmail.com>
2025-11-05 11:15:13 +0100

    more lapack

--- recipes/recipes_emscripten/openblas-flang/build.sh
+++ recipes/recipes_emscripten/openblas-flang/build.sh
@@ -2,42 +2,11 @@
 
 set -ex
 
-
-
-
-
-
-
-LLVM_PKG_URL="https://github.com/IsabelParedes/llvm-project/releases/download/v20.1.7_emscripten-wasm32/llvm_emscripten-wasm32-20.1.7-h2e33cc4_5.tar.bz2"
-LLVM_PKG="llvm.tar.bz2"
-export LLVM_DIR=$PWD/llvm
-
-wget -O "$LLVM_PKG" "$LLVM_PKG_URL"
-mkdir -p $LLVM_DIR
-tar -xjvf $LLVM_PKG -C $LLVM_DIR --exclude='info/*' --exclude='share/*' --exclude='libexec/*'
-
-export EM_LLVM_ROOT=$LLVM_DIR
-
-echo "LLVM installation complete."
-
-export FC=$LLVM_DIR/bin/flang-new
-export FFLAGS="-g --target=wasm32-unknown-emscripten -fPIC"
-export FCLIBS="-lFortranRuntime"
-
-
-
-# create an empty make.inc in the "interfaces" directory to
-
-touch lapack-netlib/make.inc
-
-
-
 emmake make libs netlib \
     CC=emcc \
     HOSTCC=gcc \
     TARGET=RISCV64_GENERIC \
     NO_LAPACKE=1 \
-    NO_LAPACK=0 \
     USE_THREAD=0 \
     INTERFACE64=0
     
