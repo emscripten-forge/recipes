fe7cc7e91d5b31fe9a1da89200cdb105e5a25b2d
DerThorsten <derthorstenbeier@gmail.com>
2025-11-06 10:58:03 +0100

    more

--- recipes/recipes_emscripten/openblas-flang/build.sh
+++ recipes/recipes_emscripten/openblas-flang/build.sh
@@ -6,7 +6,7 @@ emmake make libs netlib \
     CC=emcc \
     HOSTCC=gcc \
     TARGET=RISCV64_GENERIC \
-    NO_LAPACKE=0 \
+    NO_LAPACKE=1 \
     USE_THREAD=0
 
 emmake make install PREFIX=$PREFIX NO_SHARED=1
