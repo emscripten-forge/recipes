e4c8bd5417b35236411e1e051f190c717a6b6840
DerThorsten <derthorstenbeier@gmail.com>
2025-11-06 12:28:48 +0100

    more

--- recipes/recipes_emscripten/scipy_flang/build.sh
+++ recipes/recipes_emscripten/scipy_flang/build.sh
@@ -32,8 +32,7 @@ export FCLIBS="-lFortranRuntime"
 
 
 
-# copy scipy/_build_utils/_generate_blas_wrapper.py to the source dir
-cp $RECIPE_DIR/_generate_blas_wrapper.py scipy/_build_utils/_generate_blas_wrapper.py
+cp $RECIPE_DIR/_generate_pyx.py  scipy/linalg/_generate_pyx.py
 
 
 
