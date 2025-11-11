8b6685a3fc3e1a5da07563e5111e7690a3ff01f1
R2 <136319114+emscripten-forge-bot@users.noreply.github.com>
2025-11-11 12:04:58 +0100

    Build segyio for emscripten-4x (#3673)
    * Build segyio for emscripten-4x
    
    * Update FindPython.cmake
    
    * Update recipe.yaml
    
    ---------
    
    Co-authored-by: emscripten-forge-bot <emscripten-forge-bot@users.noreply.github.com>
    Co-authored-by: Thorsten Beier <thorsten.beier@quantstack.net>

--- recipes/recipes/cross-python_emscripten-wasm32/FindPython.cmake
+++ recipes/recipes/cross-python_emscripten-wasm32/FindPython.cmake
@@ -105,12 +105,10 @@ endif()
 
 
 
-# create python_add_library impl. 
-# Python_add_library (<name> [STATIC | SHARED | MODULE [USE_SABI <version>] [WITH_SOABI]]
-                    # <source1> [<source2> ...])
+
 
 function(python_add_library name)
-    cmake_parse_arguments(PYLIB "" "TYPE;USE_SABI;WITH_SOABI" "SOURCES" ${ARGN})
+    cmake_parse_arguments(PYLIB "WITH_SOABI" "TYPE;USE_SABI" "SOURCES" ${ARGN})
 
     # Default type
     if(NOT PYLIB_TYPE)
