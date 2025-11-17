f29614d005683c722ebd029273aafdacc3c6917e
DerThorsten <derthorstenbeier@gmail.com>
2025-11-17 09:46:17 +0100

    debug

--- recipes/recipes_emscripten/cryptography/build.sh
+++ recipes/recipes_emscripten/cryptography/build.sh
@@ -1,3 +1,25 @@
 #!/bin/bash
+
+
+
+# $PREFIX/bin/python is wrongly used?
+
+# # remove $PREFIX/bin/python
+# rm -f ${PREFIX}/bin/python
+
+# # create a symlink to $PYTHON
+
+
+$PREFIX/bin/python - << 'EOF'
+import platform, sys
+print("system:", platform.system())
+print("platform:", platform.platform())
+print("sysconfig:", __import__("sysconfig").get_platform())
+EOF
+
+
+
+
+
 ${PYTHON} -m pip  install . -vvv
 
