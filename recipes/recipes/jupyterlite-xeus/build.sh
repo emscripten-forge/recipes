816355ab4f3c6ecc116e508be048ac06395f1889
Isabel Paredes <isabel.paredes@quantstack.net>
2025-11-17 19:31:18 +0100

    Add patched jupyterlite-xeus

---
+++ recipes/recipes/jupyterlite-xeus/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -eux
+
+pushd mambajs
+    yarn install
+    yarn run build
+popd
+
+pushd jupyterlite-xeus
+    yarn link --relative ../mambajs --private
+    yarn link --relative ../mambajs/packages/mambajs
+    yarn link --relative ../mambajs/packages/mambajs-core
+    cat package.json
+
+    pip install . --prefix $PREFIX
+popd
