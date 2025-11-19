9efb16e0086f0ef0d8337375c92b961437d31e31
DerThorsten <derthorstenbeier@gmail.com>
2025-11-19 08:58:23 +0100

    flags

--- recipes/recipes_emscripten/lua/build.sh
+++ recipes/recipes_emscripten/lua/build.sh
@@ -1,6 +1,12 @@
 mkdir build
 cd build
 
+
+export EMCC_CFLAGS="-sSUPPORT_LONGJMP=wasm -fwasm-exceptions"
+
+LDFLAGS="$LDFLAGS -sSUPPORT_LONGJMP=wasm -fwasm-exceptions"
+CFLAGS="$CFLAGS -sSUPPORT_LONGJMP=wasm -fwasm-exceptions"
+
 # Configure step
 cmake ${CMAKE_ARGS} ..             \
     -DCMAKE_BUILD_TYPE=Release     \
