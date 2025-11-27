1a2f7bdc1eec504d77067ebf2ea0981771c07c67
Isabel Paredes <isabel.paredes@quantstack.net>
2025-11-27 12:48:33 +0100

    Test pycrdt

--- recipes/recipes_emscripten/pycrdt/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-
-export MATURIN_PYTHON_SYSCONFIGDATA_DIR=${PREFIX}/etc/conda/_sysconfigdata__emscripten_wasm32-emscripten.py
-export PYO3_USE_ABI3_FORWARD_COMPATIBILITY=1 
-${PYTHON} -m pip  install . -vvv
-
