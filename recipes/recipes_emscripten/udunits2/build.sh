8f4cb664b7dfee2de0d2521f53366216c8f41f2a
Isabel Paredes <isabel.paredes@quantstack.net>
2025-11-28 14:17:11 +0100

    Add udunits2

--- recipes/recipes_emscripten/udunits2/build.sh
+++ recipes/recipes_emscripten/udunits2/build.sh
@@ -10,38 +10,9 @@ sed -i s@INSERT_UDUNITS_XML_PATH_HERE@${UDUNITS2_XML_PATH}@g lib/xml.c
 
 cat lib/xml.c
 
-# if [[ $(uname) == Darwin ]]; then
-#   export LDFLAGS="${LDFLAGS} -Wl,-rpath,${PREFIX}/lib"
-# fi
-
 emconfigure ./configure --prefix=${PREFIX}  \
             --enable-static     \
             --disable-shared
 
 make -j${CPU_COUNT} ${VERBOSE_AT}
-
-# if [[ "${CONDA_BUILD_CROSS_COMPILATION:-0}" != "1" ]]; then
-#     make check
-# fi
-
 make install
-
-# if [[ $(uname) == Darwin ]]; then
-#     cp ${RECIPE_DIR}/patchbinary.py ${PREFIX}/
-#     echo ${PREFIX} > ${PREFIX}/build_prefix.a
-# fi
-
-# Make sure ENV variables and set
-# ACTIVATE_DIR=$PREFIX/etc/conda/activate.d
-# DEACTIVATE_DIR=$PREFIX/etc/conda/deactivate.d
-# mkdir -p $ACTIVATE_DIR
-# mkdir -p $DEACTIVATE_DIR
-
-# for EXT in sh csh fish
-# do
-#   cp $RECIPE_DIR/scripts/activate.$EXT $ACTIVATE_DIR/udunits2-activate.$EXT
-#   cp $RECIPE_DIR/scripts/deactivate.$EXT $DEACTIVATE_DIR/udunits2-deactivate.$EXT
-# done
-
-# We can remove this when we start using the new conda-build.
-# find $PREFIX -name '*.la' -delete
