fec96aa65d7dec83b45380b934ac59f32c324da6
Isabel Paredes <isabel.paredes@quantstack.net>
2025-12-04 12:56:36 +0100

    Add all the exceptions

--- recipes/recipes_emscripten/tiktoken/build.sh
+++ recipes/recipes_emscripten/tiktoken/build.sh
@@ -2,5 +2,12 @@
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 
 export MATURIN_PYTHON_SYSCONFIGDATA_DIR=${PREFIX}/etc/conda/_sysconfigdata__emscripten_wasm32-emscripten.py
+
+export CFLAGS="$CFLAGS -fexceptions"
+export CXXFLAGS="$CXXFLAGS -fexceptions"
+export LDFLAGS="$LDFLAGS -fexceptions"
+
+export EMCC_CFLAGS="-fexceptions"
+
 ${PYTHON} -m pip  install . -vvv
 
