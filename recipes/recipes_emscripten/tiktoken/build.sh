81799cd656267bcd5d8bb298d459596f8c7fa9d1
Isabel Paredes <isabel.paredes@quantstack.net>
2025-12-04 11:50:44 +0100

    Fix tiktoken flags

--- recipes/recipes_emscripten/tiktoken/build.sh
+++ recipes/recipes_emscripten/tiktoken/build.sh
@@ -3,6 +3,6 @@ cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 
 # export CFLAGS_wasm32-unknown-emscripten="-fwasm-exceptions"
 
-export RUSTFLAGS="-Z emscripten-wasm-eh -Cpanic=abort. -fwasm-exceptions"
+# export RUSTFLAGS="-Z emscripten-wasm-eh -Cpanic=abort. -fwasm-exceptions"
 ${PYTHON} -m pip  install . -vvv
 
