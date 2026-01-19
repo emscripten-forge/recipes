8e907c832bbb8a8463813338db2920aafb352d23
TANIGUCHI Masaya <ta2gch@gmail.com>
2026-01-19 17:22:48 +0400

    refactor: simplify xeus-haskell build by removing custom CMake properties and the xtl dependency.

--- recipes/recipes_emscripten/xeus-haskell/build.sh
+++ recipes/recipes_emscripten/xeus-haskell/build.sh
@@ -2,12 +2,6 @@
 #!/bin/bash
 set -ex
 
-cat <<EOF > overwriteProp.cmake
-set_property(GLOBAL PROPERTY TARGET_SUPPORTS_SHARED_LIBS TRUE)
-set(CMAKE_SHARED_LIBRARY_CREATE_C_FLAGS "-s SIDE_MODULE=1")
-set(CMAKE_SHARED_LIBRARY_CREATE_CXX_FLAGS "-s SIDE_MODULE=1")
-EOF
-
 emcmake cmake \
   -S $PWD \
   -B build \
@@ -19,16 +13,6 @@ emcmake cmake \
   -DCMAKE_CXX_FLAGS="" \
   -DCMAKE_EXE_LINKER_FLAGS="" \
   -DCMAKE_SHARED_LINKER_FLAGS="" \
-  -DCMAKE_PROJECT_INCLUDE=$PWD/overwriteProp.cmake \
   -DCMAKE_SYSTEM_NAME=Emscripten
 
 emmake make -C build install
-
-# NOTE: Xeus-Haskell requires the following environment variables to be set
-# export MHSDIR="$PREFIX/share/microhs";
-# export MHS_LIBRARY_PATH="$PREFIX/usr/lib/haskell-packages/microhs";
-# jupyter lite serve \
-#   --XeusAddon.prefix="$PREFIX" \
-#   --XeusAddon.mounts="$MHSDIR:/share/microhs" \
-#   --XeusAddon.mounts="$PWD/src/XHaskell:/usr/lib/haskell-packages/microhs/XHaskell" \
-#   --contents notebooks \
