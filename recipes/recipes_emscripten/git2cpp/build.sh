b20a46026f2d83547e58b8621ce76051e89d32d0
Ian Thomas <ianthomas23@gmail.com>
2026-01-27 16:22:17 +0000

    git2cpp: export PROXYFS (#4313)

--- recipes/recipes_emscripten/git2cpp/build.sh
+++ recipes/recipes_emscripten/git2cpp/build.sh
@@ -10,21 +10,21 @@ export CONFIG_LDFLAGS="\
     --minify=0 \
     -sALLOW_MEMORY_GROWTH=1 \
     -sEXIT_RUNTIME=1 \
-    -sEXPORTED_RUNTIME_METHODS=FS,ENV,TTY,stringToNewUTF8,writeArrayToMemory \
+    -sEXPORTED_RUNTIME_METHODS=FS,ENV,PROXYFS,TTY,stringToNewUTF8,writeArrayToMemory \
     -sFORCE_FILESYSTEM=1 \
     -sMODULARIZE=1 \
     --pre-js $RECIPE_DIR/pre.js \
     -sSTACK_SIZE=1MB \
+    -lproxyfs.js \
     "
-
-export CXXFLAGS="$CXXFLAGS $CONFIG_CXXFLAGS"
-export LDFLAGS="$LDFLAGS $CONFIG_LDFLAGS"
 
 emcmake cmake \
     -Bbuild \
     -DCMAKE_PREFIX_PATH=$PREFIX \
     -Dlibgit2_DIR=$PREFIX/lib/cmake/libgit2 \
-    -Dtermcolor_DIR=$BUILD_PREFIX/lib/cmake/termcolor
+    -Dtermcolor_DIR=$BUILD_PREFIX/lib/cmake/termcolor \
+    -DCMAKE_CXX_FLAGS="$CXXFLAGS $CONFIG_CXXFLAGS" \
+    -DCMAKE_EXE_LINKER_FLAGS="$LDFLAGS $CONFIG_LDFLAGS"
 
 cd build
 emmake make -j$CPU_COUNT
