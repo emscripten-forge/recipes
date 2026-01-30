d3d90b59072f51e11eec0d49e4b5d143c62507cc
Isabel Paredes <isabel.paredes@quantstack.net>
2026-01-30 17:34:34 +0100

    Add spatialgeometry

--- recipes/recipes_emscripten/spatialgeometry/test_spatialgeometry.py
+++ recipes/recipes_emscripten/spatialgeometry/test_spatialgeometry.py
@@ -1,11 +1,3 @@
-import pyjs
-import pytest
-
-
-@pytest.mark.skipif(
-    not pyjs.js.Function('return typeof importScripts === "function"')(),
-    reason="requires browser, not node",
-)
 def test_spatialgeometry():
     import spatialgeometry as gm
 
