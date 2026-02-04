716c092ecaa1e65860182829fc5b23234478773f
AntoinePrv <AntoinePrv@users.noreply.github.com>
2026-02-04 16:59:21 +0100

    Add maturin options

--- recipes/recipes_emscripten/polars/build_polars_runtime.sh
+++ recipes/recipes_emscripten/polars/build_polars_runtime.sh
@@ -6,6 +6,28 @@ cd $PKG_NAME
 
 export CARGO_PROFILE_RELEASE_STRIP=symbols
 
+MATURIN_PEP517_ARGS="--exclude-features async \
+    --exclude-features aws                \
+    --exclude-features azure              \
+    --exclude-features cloud              \
+    --exclude-features decompress         \
+    --exclude-features default            \
+    --exclude-features docs-selection     \
+    --exclude-features extract_jsonpath   \
+    --exclude-features fmt                \
+    --exclude-features gcp                \
+    --exclude-features csv                \
+    --exclude-features ipc                \
+    --exclude-features ipc_streaming      \
+    --exclude-features json               \
+    --exclude-features nightly            \
+    --exclude-features parquet            \
+    --exclude-features performant         \
+    --exclude-features streaming          \
+    --exclude-features http               \
+    --exclude-features full               \
+    --exclude-features test"
+
 $PYTHON -m pip install . -vv
 
 # The root level Cargo.toml is part of an incomplete workspace
