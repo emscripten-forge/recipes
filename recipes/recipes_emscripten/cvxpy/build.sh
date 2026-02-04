3cd4c67b5423fea8ddea39c07750f59fe0b8f7df
Isabel Paredes <isabel.paredes@quantstack.net>
2026-02-04 17:02:29 +0100

    Add cvxpy-base (#4421)
    * Add cvxpy-base
    
    * Add test notes

---
+++ recipes/recipes_emscripten/cvxpy/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -eux
+
+# `DNDEBUG` flag is used to disable debug mode, this may lead to issues.
+# See: https://github.com/cvxpy/cvxpy/issues/1690
+# Emscripten flags for Python extension modules:
+# - `-fPIC`: Required for building shared libraries (.so files) for WebAssembly/emscripten
+# targets. Without it, wasm-ld will fail with relocation errors.
+# - `SIDE_MODULE=1`: Build as side module for WebAssembly
+# - `WASM_BIGINT`: Enable big integer support
+# - `MODULARIZE=1, LINKABLE=1, EXPORT_ALL=1`: Export Python module initialization functions
+# - `fexceptions`: Enable exception handling
+export FLAGS="-DNDEBUG -fPIC -sWASM_BIGINT -s SIDE_MODULE=1 -fwasm-exceptions"
+export CXXFLAGS="${CXXFLAGS:-} ${FLAGS}"
+export CFLAGS="${CFLAGS:-} ${FLAGS}"
+export LDFLAGS="${LDFLAGS:-} -s MODULARIZE=1 -s LINKABLE=1 -s EXPORT_ALL=1 -s WASM=1 -s SIDE_MODULE=1 -sWASM_BIGINT -fwasm-exceptions -L$PREFIX/lib"
+${PYTHON} -m pip install . --no-deps -vvv
+
