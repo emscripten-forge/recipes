2cff4b389d202a53f57a51f5eb0701afceff345f
Isabel Paredes <isabel.paredes@quantstack.net>
2026-02-05 11:31:01 +0100

    Add dtks

--- recipes/recipes_emscripten/dtks/build.sh
+++ recipes/recipes_emscripten/dtks/build.sh
@@ -6,25 +6,25 @@ NANOBIND_CMAKE_DIR=$(python -m nanobind --cmake_dir)
 CMAKE_ARGS="${CMAKE_ARGS} -Dnanobind_DIR=${NANOBIND_CMAKE_DIR}"
 
 
-export PYTHON_LIBRARIES=$PREFIX/lib/libpython3.13.a
-export PYTHON_INCLUDE_DIRS=$PREFIX/include/python3.13
-export PYTHON_SITE_PACKAGES=$PREFIX/lib/python3.13/site-packages
+export PYTHON_LIBRARIES=$PREFIX/lib/libpython${PY_VER}.a
+export PYTHON_INCLUDE_DIRS=$PREFIX/include/python${PY_VER}
+export PYTHON_SITE_PACKAGES=$PREFIX/lib/python${PY_VER}/site-packages
 export PYTHON_PREFIX=$PREFIX
 export PYTHON_MODULE_EXTENSION=".so"
 export PYTHON_MODULE_PREFIX=""
 export PYTHON_IS_DEBUG=0
 
 CMAKE_ARGS="${CMAKE_ARGS} \
-    -DPython_INCLUDE_DIR=$PREFIX/include/python3.13 \
+    -DPython_INCLUDE_DIR=$PREFIX/include/python${PY_VER} \
     -DPYTHONLIBS_FOUND=TRUE \
-    -DPYTHON_LIBRARIES=$PREFIX/lib/libpython3.13.a \
-    -DPYTHON_INCLUDE_DIRS=$PREFIX/include/python3.13 \
-    -DPYTHON_SITE_PACKAGES=$PREFIX/lib/python3.13/site-packages \
+    -DPYTHON_LIBRARIES=$PREFIX/lib/libpython${PY_VER}.a \
+    -DPYTHON_INCLUDE_DIRS=$PREFIX/include/python${PY_VER} \
+    -DPYTHON_SITE_PACKAGES=$PREFIX/lib/python${PY_VER}/site-packages \
     -DPYTHON_PREFIX=$PREFIX \
     -DPYTHON_MODULE_EXTENSION=.so \
     -DPYTHON_MODULE_PREFIX= \
     -DPYTHON_IS_DEBUG=0 \
-    -DPYTHON_VERSION=3.13"
+    -DPYTHON_VERSION=${PY_VER}"
 
 
 export CMAKE_ARGS
