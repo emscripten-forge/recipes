6fc8d3851daf44d030d11186575b4dc0d22242e6
AntoinePrv <AntoinePrv@users.noreply.github.com>
2026-02-05 16:50:33 +0100

    Fix typos

--- recipes/recipes_emscripten/polars/build_polars_runtime.sh
+++ recipes/recipes_emscripten/polars/build_polars_runtime.sh
@@ -6,14 +6,14 @@ cd $PKG_NAME
 
 export CARGO_PROFILE_RELEASE_STRIP=symbols
 export RUSTFLAGS="${RUSTFLAGS} -C link-arg=--no-entry"
-# Deactivate warnings as they are too many (probably created to deactivated features)
+# Deactivate warnings as there are too many (probably created by deactivated features)
 export RUSTFLAGS="-A warnings"
 
 # FIXME should be fixed in emscripten-forge but we currenty need this step
 rustup target add wasm32-unknown-emscripten
 
-# Polars for emscripten-wams32 does not currently have a clean build
-# In CI it is tested with the following build steps
+# Polars for emscripten-wams32 does not currently have a clean build.
+# In their CI it is tested with the following build steps.
 # TODO worth checking on every release to see if features are changing.
 # see https://github.com/pola-rs/polars/blob/main/.github/workflows/test-pyodide.yml
 FEATURES="csv|ipc|ipc_streaming|parquet|async|scan_lines|json|extract_jsonpath|catalog|cloud|polars_cloud|tokio|clipboard|decompress|new_streaming"
