a0bd6ecd5ee52326b4bde38c124fca4dc6eb280d
Isabel Paredes <isabel.paredes@quantstack.net>
2026-02-06 13:57:39 +0100

    Rename openblas-flang pkg to openblas (#4449)
    * Rename openblas-flang pkg to openblas
    
    * Bump build number

--- recipes/recipes_emscripten/openblas/build.sh
+++ recipes/recipes_emscripten/openblas/build.sh
@@ -1,42 +1,29 @@
-chmod u+x c_check
-chmod u+x f_check
-chmod u+x exports/gensymbol
+#!/bin/bash
 
+set -ex
 
-SED_CMD="sed"
-if [[ "$OSTYPE" == "darwin"* ]]; then
-    SED_CMD="gsed"
-fi
+export CC=emcc
+export FC=flang-new
+export CCOMMON_OPT="$CFLAGS -Wno-implicit-function-declaration -Wno-macro-redefined -fwasm-exceptions -sSUPPORT_LONGJMP=wasm"
+export LDFLAGS="$LDFLAGS $EM_FORGE_SIDE_MODULE_LDFLAGS $FCLIBS"
 
-# Replace void returns by int returns
-$SED_CMD -ri 's/void(\s+)BLASFUNC/int\1BLASFUNC/g' common_interface.h
-$SED_CMD -ri 's/void(\s+)cblas_/int\1cblas_/g' cblas.h ctest/*.c
-$SED_CMD -ri 's/void(\s+)(C?NAME)/int\1\2/g' interface/*.c
-$SED_CMD -ri 's/((extern)?.+) void ([a-z0-9]+_)/\1\2 int \3/g' lapack-netlib/SRC/*.c \
-    lapack-netlib/SRC/DEPRECATED/*.c
-# For some functions (mostly handling complex I think) f2c actually
-# generate a function that returns void so I need to revert the void to int
-# change the previous line does.
-$SED_CMD -ri 's@int ([cz](dotc|dotu|ladiv))@void \1@g' lapack-netlib/SRC/*.c\
-    lapack-netlib/SRC/DEPRECATED/*.c
+# Shared library is libopenblas-flang.so to avoid conflict with non-flang built version of openblas
+# in libopenblas.so. Also has corresponding patch in openblas.pc.in and related machinery.
+export LIBSONAMEBASE=openblas-flang
 
-emmake make libs shared CC=emcc HOSTCC=gcc TARGET=RISCV64_GENERIC NOFORTRAN=1 NO_LAPACKE=1 \
-    USE_THREAD=0 LDFLAGS="$EM_FORGE_SIDE_MODULE_LDFLAGS"
+# Need to build on a single core otherwise libopenblas.so can contain undefined symbols.
+export BUILD_CORES=-j1
 
-# Add libf2c symbols to libopenblas.so
-emcc $PREFIX/lib/libf2c.a libopenblas.a $EM_FORGE_SIDE_MODULE_LDFLAGS \
-    -o libopenblas.so
+# Shared library is created separately from static library by emcc below.
+export NO_SHARED=1
+export USE_THREAD=0
+
+emmake make libs netlib \
+    $BUILD_CORES \
+    HOSTCC=gcc \
+    TARGET=RISCV64_GENERIC
 
 emmake make install PREFIX=$PREFIX
 
-# make sure to remove all shared libs **that dont link to libf2c**
-rm -rf $PREFIX/lib/libopenblas.*
-
-# copy the shared lib from the build dir to the install dir
-# (this is the one that links to libf2c)
-
-cp libopenblas.so $PREFIX/lib/
-
-
-# fix openblas.pc replace "extralib=-lpthread -lgfortran -lgfortran" with "extralib="
-$SED_CMD -i 's/extralibs=-lpthread -lgfortran -lgfortran/extralibs=/' $PREFIX/lib/pkgconfig/openblas.pc
+emcc libopenblas-flang.a $EM_FORGE_SIDE_MODULE_LDFLAGS $FCLIBS -o libopenblas-flang.so
+cp libopenblas-flang.so $PREFIX/lib
