53555b5fbd0d14178f6dcfa114fd10e63ed89445
arjxn-py <arjxn.py@gmail.com>
2026-02-08 18:02:12 +0530

    use octave pkg

--- recipes/recipes_emscripten/octave-image/build.sh
+++ recipes/recipes_emscripten/octave-image/build.sh
@@ -3,19 +3,10 @@ set -euxo pipefail
 
 PKG=image
 VER=2.18.1
-OCTVER=10.3.0
 
-SRC_DIR=image
-PKG_ROOT="$PREFIX/lib/octave/packages/${PKG}-${VER}"
+# The source is already unpacked into ./image by rattler
+# Create a tarball exactly like Octave expects
+tar -czf ${PKG}-${VER}.tar.gz ${PKG}
 
-mkdir -p "$PKG_ROOT"
-
-test -d "$SRC_DIR/inst"
-
-cp -r "$SRC_DIR/inst" "$PKG_ROOT/"
-
-for f in DESCRIPTION PKG_ADD PKG_DEL; do
-  [ -f "$SRC_DIR/$f" ] && cp "$SRC_DIR/$f" "$PKG_ROOT/"
-done
-
-[ -d "$SRC_DIR/doc" ] && cp -r "$SRC_DIR/doc" "$PKG_ROOT/" || true
+# Install using Octave's package manager
+octave -W -H --eval "pkg install -global -verbose ${PKG}-${VER}.tar.gz"
