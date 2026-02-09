3fc67907713d07f6c67a299116a636f6515ec98a
Isabel Paredes <isabel.paredes@quantstack.net>
2026-02-09 17:26:05 +0100

    Add python-symengine

--- recipes/recipes_emscripten/python-symengine/build.sh
+++ recipes/recipes_emscripten/python-symengine/build.sh
@@ -3,11 +3,11 @@
 cp $RECIPE_DIR/CMakeLists.txt $SRC_DIR
 
 
-unset CMAKE_CXX_FLAGS
-export CMAKE_CXX_FLAGS=""
+# unset CMAKE_CXX_FLAGS
+# export CMAKE_CXX_FLAGS=""
 
-export CXXFLAGS="--std=c++17"
-export CMAKE_CXX_STANDARD=17
+# export CXXFLAGS="--std=c++17"
+# export CMAKE_CXX_STANDARD=17
 
-export LDFLAGS="-s MODULARIZE=1  -s LINKABLE=1  -s EXPORT_ALL=1  -s WASM=1  -std=c++14  -s LZ4=1 -s SIDE_MODULE=1 -sWASM_BIGINT"
+# export LDFLAGS="-sMODULARIZE=1 -sLINKABLE=1 -sEXPORT_ALL=1 -sLZ4=1 -sSIDE_MODULE=1 -sWASM_BIGINT"
 ${PYTHON} -m pip install .
