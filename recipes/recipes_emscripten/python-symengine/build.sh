0e5cd346c0c22685179bb8544f0d1c3780e5a30b
Isabel Paredes <isabel.paredes@quantstack.net>
2026-02-09 17:31:41 +0100

    Clean up

--- recipes/recipes_emscripten/python-symengine/build.sh
+++ recipes/recipes_emscripten/python-symengine/build.sh
@@ -2,12 +2,4 @@
 
 cp $RECIPE_DIR/CMakeLists.txt $SRC_DIR
 
-
-# unset CMAKE_CXX_FLAGS
-# export CMAKE_CXX_FLAGS=""
-
-# export CXXFLAGS="--std=c++17"
-# export CMAKE_CXX_STANDARD=17
-
-# export LDFLAGS="-sMODULARIZE=1 -sLINKABLE=1 -sEXPORT_ALL=1 -sLZ4=1 -sSIDE_MODULE=1 -sWASM_BIGINT"
 ${PYTHON} -m pip install .
