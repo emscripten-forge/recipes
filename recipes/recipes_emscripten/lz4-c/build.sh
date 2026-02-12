a683de716b10518f48e551a9ee917f20df0a1613
Alexis Placet <2400067+Alex-PLACET@users.noreply.github.com>
2026-02-12 15:47:22 +0100

    Improve lz4 recipe by adding tests and cmake config file

--- recipes/recipes_emscripten/lz4-c/build.sh
+++ recipes/recipes_emscripten/lz4-c/build.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 set -ex
 
-export CFLAGS="${CFLAGS} -O3 -fPIC"
-export LDFLAGS="${LDFLAGS} -lrt"
+export CFLAGS="$CFLAGS $EM_FORGE_SIDE_MODULE_CFLAGS"
+export LDFLAGS="$LDFLAGS $EM_FORGE_SIDE_MODULE_LDFLAGS"
 
 # Build
-VERBOSE=1 make -j${CPU_COUNT} PREFIX=${PREFIX} HAVE_MULTITHREAD=0
+emmake make PREFIX=${PREFIX} CFLAGS="${CFLAGS}" LDFLAGS="${LDFLAGS}" HAVE_MULTITHREAD=0 VERBOSE=1
 
 # Install
-make install PREFIX=${PREFIX}
+emmake make install PREFIX=${PREFIX}
+
+# Install CMake config file
+mkdir -p ${PREFIX}/lib/cmake/lz4
+sed "s/@VERSION@/${PKG_VERSION}/g" ${RECIPE_DIR}/lz4Config.cmake > ${PREFIX}/lib/cmake/lz4/lz4Config.cmake
