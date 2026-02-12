35c53533aa2f5fbff19fc7fb2472600e88248e22
arjxn-py <arjxn.py@gmail.com>
2026-02-12 15:44:53 +0530

    try install

--- recipes/recipes_emscripten/octave-image/build.sh
+++ recipes/recipes_emscripten/octave-image/build.sh
@@ -52,6 +52,12 @@ ls -lah "${PKG}-${VER}.tar.gz"
 log "Running pkg build (no install, keep build dir)"
 octave -W -H --eval "pkg build ${BUILD_DIR} ${PKG}-${VER}.tar.gz -verbose" || true
 
+log "Installing package into PREFIX"
+octave -W -H --eval "pkg prefix ${PREFIX}; pkg install -nodeps ${BUILD_DIR}/*.tar.gz"
+
+log "Verify installation"
+find "${PREFIX}" -maxdepth 5 -print
+
 
 log "PWD after build"
 pwd
