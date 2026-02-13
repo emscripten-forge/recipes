c5988234b600588726182088444b166f7a1e2453
Alexis Placet <2400067+Alex-PLACET@users.noreply.github.com>
2026-02-13 13:53:39 +0100

    wip

--- recipes/recipes_emscripten/arrow/build_r_arrow.sh
+++ recipes/recipes_emscripten/arrow/build_r_arrow.sh
@@ -4,5 +4,8 @@
 # Build R Arrow Bindings
 # ============================================================================
 
+export ARROW_HOME=${PREFIX}
+export LD_LIBRARY_PATH=${PREFIX}/lib:$LD_LIBRARY_PATH
+
 cd $SRC_DIR/r
 $R CMD INSTALL $R_ARGS .
