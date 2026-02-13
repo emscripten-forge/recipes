a7d2ec9eb1d56b0e264b1631eb51f0986c08ff81
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2026-02-12 21:54:17 -0800

    recipes/recipes_emscripten/gsl: Fixups

--- recipes/recipes_emscripten/gsl/build.sh
+++ recipes/recipes_emscripten/gsl/build.sh
@@ -3,6 +3,7 @@ emconfigure ./configure \
     LDFLAGS="$LDFLAGS `pkg-config --libs-only-L openblas`" \
     CPPFLAGS="$CPPFLAGS `pkg-config --cflags-only-I openblas`" \
     CFLAGS="$CFLAGS -fPIC `pkg-config --cflags-only-other openblas`" \
+    --prefix=${PREFIX} \
     --disable-shared
 emmake make -j${CPU_COUNT}
 emmake make install
