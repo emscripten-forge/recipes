9aadfd8429456f7387eca698e89abb57d41c77af
martinRenou <martin.renou@gmail.com>
2026-02-16 10:30:17 +0100

    Hey

--- recipes/recipes_emscripten/whiteboxgui/test_whitebox.py
+++ recipes/recipes_emscripten/whiteboxgui/test_whitebox.py
@@ -5,5 +5,5 @@ def test_whitebox():
     # This is how it's supposed to behave for now on emscripten platform
     try:
         import whiteboxgui
-    except SystemExit:
+    except Exception:
         pass
