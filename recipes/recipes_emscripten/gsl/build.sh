fff4db86639a42fd7afc47c46551f4de578c7f8f
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2026-02-16 10:33:11 -0800

    recipes/recipes_emscripten/gsl: Remove openblas dependency

--- recipes/recipes_emscripten/gsl/build.sh
+++ recipes/recipes_emscripten/gsl/build.sh
@@ -1,8 +1,5 @@
 emconfigure ./configure \
-    LIBS="`pkg-config --libs-only-l openblas` -lm" \
-    LDFLAGS="$LDFLAGS `pkg-config --libs-only-L openblas`" \
-    CPPFLAGS="$CPPFLAGS `pkg-config --cflags-only-I openblas`" \
-    CFLAGS="$CFLAGS -fPIC `pkg-config --cflags-only-other openblas`" \
+    CFLAGS="$CFLAGS -fPIC" \
     --prefix=${PREFIX} \
     --disable-shared
 emmake make -j${CPU_COUNT}
