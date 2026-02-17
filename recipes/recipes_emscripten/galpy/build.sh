ecafa3a52c45e0433670f925d5a8c83795c86583
Isabel Paredes <isabel.paredes@quantstack.net>
2026-02-17 14:48:53 +0100

    Build extension

--- recipes/recipes_emscripten/galpy/build.sh
+++ recipes/recipes_emscripten/galpy/build.sh
@@ -13,9 +13,5 @@ export PYODIDE=1
 # Disable OpenMP for WebAssembly compatibility
 export GALPY_COMPILE_NO_OPENMP=1
 
-# For now, disable C extensions until GSL is available for emscripten
-# This allows galpy to install and work with pure Python implementations
-export GALPY_COMPILE_NO_EXT=1
-
 # Install with verbose output for debugging
 ${PYTHON} -m pip install . --no-deps --verbose
