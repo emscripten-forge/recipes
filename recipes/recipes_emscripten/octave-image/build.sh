a4821c0e440cd276700f0588bee12d0b7ba7c0b0
arjxn-py <arjxn.py@gmail.com>
2026-02-19 02:21:07 +0530

    prefix

--- recipes/recipes_emscripten/octave-image/build.sh
+++ recipes/recipes_emscripten/octave-image/build.sh
@@ -67,7 +67,7 @@ ls -lah "$BUILD_PREFIX"
 ls -lah "$BUILD_PREFIX/bin"
 
 echo $(node "$PREFIX/bin/octave-cli" --eval "disp (computer)")
-$BUILD_PREFIX/bin/octave-cli --eval "
+$PREFIX/bin/octave-cli --eval "
 pkg prefix '${PREFIX}/share/octave/packages' '${PREFIX}/lib/octave/packages';
 pkg install -nodeps ${BUILD_DIR}/*.tar.gz;
 pkg list;
