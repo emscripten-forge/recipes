c26af49d0e90760e8102dec43b9b3877b96af66e
Isabel Paredes <isabel.paredes@quantstack.net>
2026-02-19 16:20:46 +0100

    Fix whiteboxgui tests

--- recipes/recipes_emscripten/whiteboxgui/test_whitebox.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-
-def test_whitebox():
-    # This is how it's supposed to behave for now on emscripten platform
-    try:
-        import whiteboxgui
-    except Exception:
-        pass
