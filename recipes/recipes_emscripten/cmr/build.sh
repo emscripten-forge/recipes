a1a2591dcb7e5d84b49564fc00056f28bba0c8e8
Matthias Köppe <mkoeppe@math.ucdavis.edu>
2026-02-19 08:42:32 -0800

    Add cmr, passagemath-cmr [emscripten-3x] (#4879)

---
+++ recipes/recipes_emscripten/cmr/build.sh
@@ -0,0 +1,16 @@
+mkdir build
+cd build
+mkdir -p $PREFIX
+
+# Configure step
+cmake ${CMAKE_ARGS} ..                    \
+    -GNinja                               \
+    -DCMAKE_PREFIX_PATH:PATH=${PREFIX}    \
+    -DCMAKE_INSTALL_PREFIX:PATH=${PREFIX} \
+    -DCMAKE_POSITION_INDEPENDENT_CODE=ON  \
+    -DCMAKE_BUILD_TYPE=Release
+
+ninja install
+
+# Copy wasm files also
+cp *.wasm $PREFIX/bin/
