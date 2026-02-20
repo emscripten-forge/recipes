e32476d19fda95eb2d2c4130ca400aaea07b6fbe
Wolf Vollprecht <w.vollprecht@gmail.com>
2026-02-20 18:08:35 +0100

    fix test

--- recipes/recipes_emscripten/python-igraph/test_igraph.py
+++ recipes/recipes_emscripten/python-igraph/test_igraph.py
@@ -1,6 +1,7 @@
 import igraph
 
-g = igraph.Graph.Famous("Petersen")
-assert g.vcount() == 10
-assert g.ecount() == 15
-print("python-igraph OK")
+
+def test_igraph():
+    g = igraph.Graph.Famous("Petersen")
+    assert g.vcount() == 10
+    assert g.ecount() == 15
