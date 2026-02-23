f22c62076e799910fe3760cc7b44b10fbb288911
Isabel Paredes <isabel.paredes@quantstack.net>
2026-02-23 18:54:19 +0100

    Test without branching

--- emci/bot/bump_recipes_versions.py
+++ emci/bot/bump_recipes_versions.py
@@ -133,12 +133,12 @@ def bump_recipe_version(recipe_dir, target_pr_branch_name):
     # Create branch name with actual versions
     branch_name = f"bumpTST-{name}_{current_version}_to_{new_version}_for_{target_pr_branch_name}"
 
-    with git_branch_ctx(branch_name, stash_current=False):
+    # with git_branch_ctx(branch_name, stash_current=False):
         # The recipe is already bumped, so we just need to commit and create PR
         # commit the changes and make a PR
-        pr_title = make_pr_title(name, current_version, new_version, target_pr_branch_name)
-        print(f"Making PR: {pr_title} with target branch {target_pr_branch_name}")
-        print("TODO TESTING make pr for recipe")
+    pr_title = make_pr_title(name, current_version, new_version, target_pr_branch_name)
+    print(f"Making PR: {pr_title} with target branch {target_pr_branch_name}")
+    print("TODO TESTING make pr for recipe")
         # make_pr_for_recipe(recipe_dir=recipe_dir, pr_title=pr_title, branch_name=branch_name,
         #     target_branch_name=target_pr_branch_name,
         #     automerge=automerge)
