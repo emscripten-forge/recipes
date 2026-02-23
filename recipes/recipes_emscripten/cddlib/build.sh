3609dc41a66ba2dd0cc5f541f8f189661029d62a
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2026-02-22 23:04:05 -0800

    recipes/recipes_emscripten/cddlib: New

---
+++ recipes/recipes_emscripten/cddlib/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Get an updated config.sub and config.guess
+cp $BUILD_PREFIX/share/gnuconfig/config.* . || true
+
+emconfigure ./configure \
+    CPPFLAGS="$CPPFLAGS -I$PREFIX/include" \
+    CFLAGS="$CFLAGS -fPIC" \
+    --prefix=$PREFIX \
+    --enable-shared=no
+
+make -j${CPU_COUNT}
+make install
