2afe161c59b96c270cdea1ad5bb6efafe8461250
DerThorsten <derthorstenbeier@gmail.com>
2026-03-02 11:00:22 +0100

    info

--- recipes/recipes_emscripten/octave-image/empp-wrapper.sh
+++ recipes/recipes_emscripten/octave-image/empp-wrapper.sh
@@ -44,7 +44,7 @@ done
 filtered+=("-sLINKABLE=1")
 # -s EXPORT_ALL=1
 filtered+=("-sEXPORT_ALL=1")
-
+filtered+=("-sSIDE_MODULE=1")
 
 echo "Filtered arguments: ${filtered[*]}"
 echo "Original arguments: $*"
