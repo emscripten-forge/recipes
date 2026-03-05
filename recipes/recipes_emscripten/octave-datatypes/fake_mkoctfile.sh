5d44f0a180be3b5247d9f28d36f45b7aa74b1e1a
arjxn-py <arjxn.py@gmail.com>
2026-03-05 19:02:58 +0530

    try fix

--- recipes/recipes_emscripten/octave-datatypes/fake_mkoctfile.sh
+++ recipes/recipes_emscripten/octave-datatypes/fake_mkoctfile.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-SOURCE_FILE="$1"
+SOURCE_FILE="${1:-}"
 
 # if the first argument is -c
 if [ "$SOURCE_FILE" == "-c" ]; then
@@ -15,9 +15,5 @@ else
     BASENAME=$(basename "$SOURCE_FILE" .cc)
     EXTRA_ARG=" -o ${BASENAME}.oct"
 fi
-
-
-
-
 
 emcc -sSIDE_MODULE=1 -O3 -I "$PREFIX/include/octave-${OCTAVE_VER}" "$@" $EXTRA_ARG
