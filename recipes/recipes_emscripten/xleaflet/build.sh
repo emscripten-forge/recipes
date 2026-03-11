c80ce2ab5a7d6e99f1f649ae449c4d12729d1eb8
arjxn-py <arjxn.py@gmail.com>
2026-03-11 16:22:16 +0530

    cmake path

--- recipes/recipes_emscripten/xleaflet/build.sh
+++ recipes/recipes_emscripten/xleaflet/build.sh
@@ -5,7 +5,7 @@ cd build
 # Configure step
 emcmake cmake       \
   -DCMAKE_BUILD_TYPE=Release     \
-  -DCMAKE_PREFIX_PATH=$PREFIX    \
+  -DCMAKE_PREFIX_PATH="$PREFIX;$PREFIX/lib/cmake;$PREFIX/share/cmake" \
   -DCMAKE_INSTALL_PREFIX=$PREFIX \
   -DCMAKE_FIND_ROOT_PATH=$PREFIX \
   ..
