fee3ac16e1077050c173e60b98d64c902bd2522b
arjxn-py <arjxn.py@gmail.com>
2026-03-19 19:29:56 +0530

    try

---
+++ recipes/recipes_emscripten/fiona/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Emscripten flags
+export CFLAGS="$CFLAGS $EM_FORGE_SIDE_MODULE_CFLAGS"
+export LDFLAGS="$LDFLAGS $EM_FORGE_SIDE_MODULE_LDFLAGS"
+
+# Point Fiona to GDAL (this is critical)
+export GDAL_VERSION=$(gdal-config --version || echo "unknown")
+export GDAL_INCLUDE_PATH=$PREFIX/include
+export GDAL_LIBRARY_PATH=$PREFIX/lib
+
+# Sometimes needed to bypass config detection
+export GDAL_CONFIG=/bin/false
+
+# Install
+${PYTHON} -m pip install . --no-deps --no-build-isolation -vv
