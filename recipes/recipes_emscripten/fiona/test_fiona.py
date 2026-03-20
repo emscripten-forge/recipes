81885fb83dd3aa02f34b98d18b496191a8b933d8
arjxn-py <arjxn.py@gmail.com>
2026-03-20 17:55:22 +0530

    test

---
+++ recipes/recipes_emscripten/fiona/test_fiona.py
@@ -0,0 +1,18 @@
+def test_import_fiona():
+    import fiona
+
+
+def test_fiona_env():
+    import fiona
+    from fiona.env import Env
+
+    # Basic context manager test (does not require filesystem)
+    with Env():
+        assert True
+
+
+def test_supported_drivers():
+    import fiona
+
+    drivers = fiona.supported_drivers
+    assert isinstance(drivers, dict)
