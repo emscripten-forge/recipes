e3e1d0d809fc72f7bd3b360350040b6df81a4fac
Adam Blake <adam@coursekata.org>
2026-04-02 15:21:55 -0700

    fix(libflang): patch narrowing conversion in descriptor.cpp for wasm32
    On wasm32, size_t is 4 bytes but DerivedType::sizeInBytes() returns
    uint64_t. Brace initialization flags this as a narrowing error.
    Switch to parenthesized initialization to allow the implicit conversion.

--- recipes/recipes_emscripten/libflang/build.sh
+++ recipes/recipes_emscripten/libflang/build.sh
@@ -37,6 +37,16 @@ set(FLANG_RT_INSTALL_RESOURCE_LIB_PATH "lib")
 # Meta-target that add_flangrt_library adds dependencies to
 add_custom_target(flang-rt)
 
+# Generate config.h (normally done by flang-rt/CMakeLists.txt)
+find_package(Backtrace)
+set(HAVE_BACKTRACE ${Backtrace_FOUND})
+set(BACKTRACE_HEADER ${Backtrace_HEADER})
+# wasm32/linux: strerror_r exists, strerror_s (Windows) does not
+set(HAVE_STRERROR_R 1)
+set(HAVE_DECL_STRERROR_S 0)
+configure_file("${FLANG_RT_SOURCE_DIR}/cmake/config.h.cmake.in" config.h)
+include_directories(${CMAKE_CURRENT_BINARY_DIR})
+
 add_subdirectory("${FLANG_RT_SOURCE_DIR}/lib/runtime" runtime_build)
 CMAKEOF
 
@@ -48,7 +58,25 @@ $(which cmake) --build _fbuild_runtime --target install
 
 
 # Build libFortranDecimal.a
-emcmake cmake -S flang/lib/Decimal -B _fbuild_decimal -GNinja \
+# In LLVM 21, flang/lib/Decimal uses add_flang_library which pulls in the
+# entire LLVM cmake infrastructure. For just 2 source files, build directly.
+mkdir -p _wrapper_decimal
+cat > _wrapper_decimal/CMakeLists.txt << 'CMAKEOF'
+cmake_minimum_required(VERSION 3.20)
+project(flang-decimal C CXX)
+
+set(FLANG_SOURCE_DIR "${CMAKE_CURRENT_SOURCE_DIR}/../flang")
+include_directories("${FLANG_SOURCE_DIR}/include")
+
+add_library(FortranDecimal STATIC
+    "${FLANG_SOURCE_DIR}/lib/Decimal/binary-to-decimal.cpp"
+    "${FLANG_SOURCE_DIR}/lib/Decimal/decimal-to-binary.cpp"
+)
+
+install(TARGETS FortranDecimal ARCHIVE DESTINATION lib)
+CMAKEOF
+
+emcmake cmake -S _wrapper_decimal -B _fbuild_decimal -GNinja \
                 -DCMAKE_BUILD_TYPE=Release \
                 -DCMAKE_INSTALL_PREFIX=$PREFIX
 $(which cmake) --build _fbuild_decimal
