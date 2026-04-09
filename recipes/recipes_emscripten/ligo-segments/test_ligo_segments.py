0bc043a2a4f8898d3120318cc72970ddda49103a
arjxn-py <arjxn.py@gmail.com>
2026-04-09 16:47:37 +0530

    update test

--- recipes/recipes_emscripten/ligo-segments/test_ligo_segments.py
+++ recipes/recipes_emscripten/ligo-segments/test_ligo_segments.py
@@ -1,10 +1,12 @@
-from ligo.segments import segment, segmentlist
+from ligo.segments import segment
 
-a = segment(1, 2)
-b = segment(2, 3)
-c = segment(5, 6)
 
-assert a.connects(b)
-assert a in (segmentlist([a]) + segmentlist([b]))
-assert (segmentlist([a]) + segmentlist([b])).intersects(b)
-assert a.disjoint(c)
+def test_ligo_segments():
+    a = segment(1, 2)
+    b = segment(2, 3)
+    c = segment(5, 6)
+
+    assert a.connects(b)
+    assert a in (a + b)
+    assert (a + b).intersects(b)
+    assert a.disjoint(c)
