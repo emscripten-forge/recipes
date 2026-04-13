9c5326320d80ab0b95540c6512b4f182afe7aca3
DerThorsten <derthorstenbeier@gmail.com>
2026-04-13 10:24:01 +0200

    json-c

--- recipes/recipes_emscripten/json-c/build.sh
+++ recipes/recipes_emscripten/json-c/build.sh
@@ -1,26 +1,7 @@
 #!/bin/bash
-# Get an updated config.sub and config.guess
-# cp $BUILD_PREFIX/share/gnuconfig/config.* .
 
 # https://github.com/json-c/json-c/issues/406
 export CPPFLAGS="${CPPFLAGS/-DNDEBUG/}"
-
-
-
-# # Enable or disable features. By default, all features are turned off.
-# option(DISABLE_BSYMBOLIC              "Avoid linking with -Bsymbolic-function."               OFF)
-# option(DISABLE_THREAD_LOCAL_STORAGE   "Disable using Thread-Local Storage (HAVE___THREAD)."   OFF)
-# option(DISABLE_WERROR                 "Avoid treating compiler warnings as fatal errors."     OFF)
-# option(ENABLE_RDRAND                  "Enable RDRAND Hardware RNG Hash Seed."                 OFF)
-# option(ENABLE_THREADING               "Enable partial threading support."                     OFF)
-# option(OVERRIDE_GET_RANDOM_SEED       "Override json_c_get_random_seed() with custom code."   OFF)
-# option(DISABLE_EXTRA_LIBS             "Avoid linking against extra libraries, such as libbsd." OFF)
-# option(DISABLE_JSON_POINTER           "Disable JSON pointer (RFC6901) and JSON patch support." OFF)
-# option(DISABLE_JSON_PATCH             "Disable JSON patch (RFC6902) support."                 OFF)
-# option(NEWLOCALE_NEEDS_FREELOCALE     "Work around newlocale bugs in old FreeBSD and macOS by calling freelocale"  OFF)
-# option(BUILD_APPS                     "Default to building apps" ON)
-
-
 
 
 mkdir build
