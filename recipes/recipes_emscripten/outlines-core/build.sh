a45053aa82bcf503d83695ae8cc85ea42eec652c
DerThorsten <derthorstenbeier@gmail.com>
2026-04-15 09:38:05 +0200

    wip

--- recipes/recipes_emscripten/outlines-core/build.sh
+++ recipes/recipes_emscripten/outlines-core/build.sh
@@ -4,5 +4,6 @@ rustup target add wasm32-unknown-emscripten
 
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 export MATURIN_PYTHON_SYSCONFIGDATA_DIR=${PREFIX}/etc/conda/_sysconfigdata__emscripten_wasm32-emscripten.py
+export OPENSSL_DIR=$PREFIX
 ${PYTHON} -m pip  install . -vvv
 
