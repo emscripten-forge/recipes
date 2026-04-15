f763b988e1e1ad2a6ce578fb96d5d73e77094f58
Ian Thomas <ianthomas23@gmail.com>
2026-04-15 07:59:55 +0100

    Update less to v692

--- recipes/recipes_emscripten/less/build.sh
+++ recipes/recipes_emscripten/less/build.sh
@@ -7,42 +7,32 @@ echo $NCURSES_CFLAGS
 echo $NCURSES_LDFLAGS
 ls -l $XTERM_256COLOR
 
-export CONFIG_CFLAGS="\
-    $NCURSES_CFLAGS \
-    -Os \
-    -Wno-incompatible-function-pointer-types \
-    "
+export EMCC_CFLAGS="$EMCC_CFLAGS -Os"
 
-export CONFIG_LDFLAGS="\
+export CFLAGS="$CFLAGS $NCURSES_CFLAGS"
+
+export LDFLAGS="\
+    $LDFLAGS \
     $NCURSES_LDFLAGS \
-    -Os \
     --minify=0 \
     -sALLOW_MEMORY_GROWTH=1 \
     -sEXIT_RUNTIME=1 \
-    -sEXPORTED_RUNTIME_METHODS=FS,ENV,getEnvStrings,TTY \
+    -sEXPORTED_RUNTIME_METHODS=FS,ENV,PROXYFS,TTY \
     -sFORCE_FILESYSTEM=1 \
     -sMODULARIZE=1 \
     -sLZ4 \
+    -lproxyfs.js \
     "
 
-# Use same return type for signals that is used in ncurses.
-sed -i -e 's/#define RETSIGTYPE void/#define RETSIGTYPE int/g' defines.h.in
-
-# Use ncurses' winch not local copy.
-sed -i -e 's/RETSIGTYPE winch/RETSIGTYPE notwinch/g' signal.c
+emmake make -f Makefile.aut distfiles
 
 emconfigure ./configure \
     --host=wasm32-unknown-emscripten \
-    CFLAGS="$CFLAGS $CONFIG_CFLAGS" \
-    LDFLAGS="$LDFLAGS $CONFIG_LDFLAGS" \
-    ac_cv_func__setjmp=no \
-    ac_cv_func_popen=no \
-    ac_cv_func_sigprocmask=no
+    --with-regex=pcre2
 
 # Build less but not lessecho and lesskey.
-emmake make less.js EXEEXT=.js -j${CPU_COUNT} LDFLAGS=" \
+emmake make less.js EXEEXT=.js -j${CPU_COUNT} LDFLAGS="
     $LDFLAGS \
-    $CONFIG_LDFLAGS \
     --preload-file $XTERM_256COLOR@/usr/local/share/terminfo/x/xterm-256color \
     "
 
