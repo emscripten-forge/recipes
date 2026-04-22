1a566e0f91c950e66e8acfcbde97b8e311207ed8
Thorsten Beier <thorsten.beier@quantstack.net>
2026-04-22 14:30:46 +0200

    Copy PNG files to kernel directory
    Added PNG files to the kernel directory during build.

--- recipes/recipes_emscripten/xeus-shakespearelang/build.sh
+++ recipes/recipes_emscripten/xeus-shakespearelang/build.sh
@@ -9,7 +9,7 @@ KERNEL_DIR=$PREFIX/share/jupyter/kernels/xeus_shakespearelang
 # copy 
 mkdir -p $KERNEL_DIR
 cp -r examples/xeus_shakespearelang/kernelspec/xeus_shakespearelang/kernel.json.in  $KERNEL_DIR/kernel.json
-
+cp examples/xeus_shakespearelang/kernelspec/xeus_shakespearelang/*.png $KERNEL_DIR/
 if [[ "$OSTYPE" == "darwin"* ]]; then
   # macOS / BSD - note the space after -i ''
   sed -i '' "s|@XEUS_PYWRAP_KERNELSPEC_PATH@|$PREFIX/bin/|g" "$KERNEL_DIR/kernel.json"
