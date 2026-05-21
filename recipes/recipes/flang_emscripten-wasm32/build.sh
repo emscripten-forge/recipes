daae7fc55992a4cfe0ba86e33c249439a41730a2
Isabel Paredes <isabel.paredes@quantstack.net>
2026-05-21 19:08:09 +0200

    Update flang to v22.1.5

--- recipes/recipes/flang_emscripten-wasm32/build.sh
+++ recipes/recipes/flang_emscripten-wasm32/build.sh
@@ -1,11 +1,33 @@
 #!/bin/bash
 
-set -ex
+set -eux
 
-# FIXME: This is the old runtime library compiled with em-3.1.45
-# `libflang` does not produce a compatible library.
-# See: https://github.com/emscripten-forge/recipes/pull/2046
-cp $RECIPE_DIR/libFortranRuntime_emscripten-wasm32.a $PREFIX/lib/
+mkdir _build
+cd _build
+
+cmake -G Ninja \
+    ${CMAKE_ARGS} \
+    -DBUILD_SHARED_LIBS=ON \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_CXX_STANDARD=17 \
+    -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
+    -DCMAKE_INSTALL_PREFIX=$PREFIX \
+    -DCMAKE_MODULE_PATH=../cmake/Modules \
+    -DCMAKE_PREFIX_PATH=$PREFIX \
+    -DLLVM_DEFAULT_TARGET_TRIPLE=wasm32-unknown-emscripten \
+    -DLLVM_EXTERNAL_LIT=$PREFIX/bin/lit \
+    -DLLVM_LIT_ARGS=-v \
+    -DLLVM_CMAKE_DIR=$PREFIX/lib/cmake/llvm \
+    -DLLVM_DIR=$PREFIX/lib/cmake/llvm \
+    -DLLVM_TARGETS_TO_BUILD="WebAssembly" \
+    -DLLVM_ENABLE_PROJECTS="clang;flang;mlir" \
+    -DCLANG_DIR=$PREFIX/lib/cmake/clang \
+    -DFLANG_INCLUDE_TESTS=OFF \
+    -DMLIR_DIR=$PREFIX/lib/cmake/mlir \
+    ../flang
+
+cmake --build . -j4
+cmake --install .
 
 # Copy the [de]activate scripts to $PREFIX/etc/conda/[de]activate.d.
 # This will allow them to be run on environment activation.
