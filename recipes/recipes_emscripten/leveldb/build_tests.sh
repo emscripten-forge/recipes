c883456d597001c680ee8c8570d394d2a1d639f0
Alexis Placet <alexis.placet.dev@pm.me>
2026-05-28 09:30:26 +0200

    Add leveldb

---
+++ recipes/recipes_emscripten/leveldb/build_tests.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+# For test executables, we need standalone mode, not side modules
+export CFLAGS="$CFLAGS"
+export CXXFLAGS="$CXXFLAGS"
+
+emcmake cmake -S tests -B build_tests \
+  -DCMAKE_BUILD_TYPE=Release
+
+emmake make -C build_tests
+
+node build_tests/test_leveldb.js
