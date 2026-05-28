40f4bf6f5d59ecdcdef8ebad0c9a763f902a01b6
Alexis Placet <alexis.placet.dev@pm.me>
2026-05-28 10:08:08 +0200

    improvements

--- recipes/recipes_emscripten/leveldb/build_tests.sh
+++ recipes/recipes_emscripten/leveldb/build_tests.sh
@@ -6,7 +6,9 @@ export CFLAGS="$CFLAGS"
 export CXXFLAGS="$CXXFLAGS"
 
 emcmake cmake -S tests -B build_tests \
-  -DCMAKE_BUILD_TYPE=Release
+  -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_PREFIX_PATH=$PREFIX \
+  -Dleveldb_DIR=$PREFIX/lib/cmake/leveldb
 
 emmake make -C build_tests
 
