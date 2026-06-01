bcdc52ad6bcc2737850be5b22403d6640ba7f4ee
Isabel Paredes <isabel.paredes@quantstack.net>
2026-06-01 17:48:49 +0200

    Test askpass

---
+++ recipes/recipes_emscripten/r-askpass/test-r-askpass.R
@@ -0,0 +1,12 @@
+library(askpass)
+
+stopifnot(is.function(askpass))
+stopifnot(is.function(ssh_askpass))
+
+# Non-interactive, must not prompt, returns NULL per package docs
+pw <- askpass("CI test password:")
+stopifnot(is.null(pw))
+
+pkgdir <- system.file(package = "askpass")
+stopifnot(nzchar(pkgdir))
+cat("askpass smoke test passed\n")
