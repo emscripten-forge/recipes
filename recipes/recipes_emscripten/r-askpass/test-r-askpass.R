2430ea4a0697fbb7577974ff84a5ef798cff8620
Isabel Paredes <isabel.paredes@quantstack.net>
2026-06-01 18:17:37 +0200

    Restore askpass test

--- recipes/recipes_emscripten/r-askpass/test-r-askpass.R
+++ recipes/recipes_emscripten/r-askpass/test-r-askpass.R
@@ -1,6 +1,6 @@
 library(askpass)
 
-stopifnot(is.function(askpassion))
+stopifnot(is.function(askpass))
 stopifnot(is.function(ssh_askpass))
 
 # Non-interactive, must not prompt, returns NULL per package docs
