774b47ca4ec931246b7c9e56a8504843b91c62e0
Isabel Paredes <isabel.paredes@quantstack.net>
2026-06-01 18:32:38 +0200

    Add R tester packages (#5579)
    * Add r-wasm-tester
    
    * Test r-bit
    
    * Test askpass
    
    * Test failure
    
    * Simplify
    
    * Restore askpass test
    
    * Add base64enc tests

---
+++ recipes/recipes/r-wasm-tester/Rtester.js
@@ -0,0 +1,53 @@
+const path = require("node:path");
+
+const wasmRHome = "/R";
+const hostRHome = path.join(process.env.PREFIX, "lib", "R");
+const rExecDir = path.join(hostRHome, "bin", "exec");
+const rLibDir = path.join(hostRHome, "lib");
+
+const rScriptPath = process.argv[2];
+const rScriptBody = require("node:fs").readFileSync(rScriptPath, "utf8");
+const rArgs = ["--no-restore", "--vanilla", "-e", rScriptBody];
+
+var Module = {
+  noInitialRun: true,
+  locateFile: (file) => {
+    const fs = require("node:fs");
+    for (const dir of [rExecDir, rLibDir]) {
+      const candidate = path.join(dir, file);
+      if (fs.existsSync(candidate)) {
+        return candidate;
+      }
+    }
+    return path.join(rExecDir, file);
+  },
+  preRun: [() => {
+    const fs = require("node:fs");
+    const copyTree = (srcRoot, dstRoot) => {
+      Module.FS.mkdirTree(dstRoot);
+      for (const entry of fs.readdirSync(srcRoot, { withFileTypes: true })) {
+        const src = path.join(srcRoot, entry.name);
+        const dst = `${dstRoot}/${entry.name}`;
+        if (entry.isDirectory()) {
+          copyTree(src, dst);
+        } else if (entry.isFile()) {
+          Module.FS.writeFile(dst, fs.readFileSync(src));
+        }
+      }
+    };
+
+    // R_HOME tree (etc, share, library, modules) and /lib for dynload paths.
+    copyTree(hostRHome, wasmRHome);
+    copyTree(rLibDir, "/lib");
+  }],
+  onRuntimeInitialized() {
+    Module.callMain(rArgs);
+  },
+};
+
+let glue = require("node:fs").readFileSync(path.join(rExecDir, "R"), "utf8");
+const envLiteral = JSON.stringify({
+  R_HOME: wasmRHome,
+});
+glue = glue.replace("var ENV={};", `var ENV=${envLiteral};`);
+eval(glue);
