d55c0cbce42f6f64d84f0388954b5629178c7cb1
Alexis Placet <alexis.placet.dev@pm.me>
2026-06-01 14:17:44 +0200

    fix

--- recipes/recipes_emscripten/stb/build.sh
+++ recipes/recipes_emscripten/stb/build.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 set -e
 
+export CFLAGS="${CFLAGS} ${EM_FORGE_SIDE_MODULE_CFLAGS}"
+
+# Install headers
 mkdir -p $PREFIX/include
 cp *.h $PREFIX/include/
+
+# Build stb_vorbis as a static library
+mkdir -p $PREFIX/lib
+${CC} -c stb_vorbis.c -DSTB_VORBIS_IMPLEMENTATION ${CFLAGS} -o stb_vorbis.o
+${AR} rcs $PREFIX/lib/libstb_vorbis.a stb_vorbis.o
 
 # Install CMake config files
 mkdir -p $PREFIX/lib/cmake/stb
--- recipes/recipes_emscripten/stb/stbTargets.cmake
+++ recipes/recipes_emscripten/stb/stbTargets.cmake
@@ -1,10 +1,19 @@
 # stb CMake targets file
 #
 # Defines the header-only imported target stb::headers.
+# Defines the static library target stb::vorbis.
 
 if(NOT TARGET stb::headers)
     add_library(stb::headers INTERFACE IMPORTED)
     set_target_properties(stb::headers PROPERTIES
+        INTERFACE_INCLUDE_DIRECTORIES "${CMAKE_CURRENT_LIST_DIR}/../../../include"
+    )
+endif()
+
+if(NOT TARGET stb::vorbis)
+    add_library(stb::vorbis STATIC IMPORTED)
+    set_target_properties(stb::vorbis PROPERTIES
+        IMPORTED_LOCATION "${CMAKE_CURRENT_LIST_DIR}/../../../lib/libstb_vorbis.a"
         INTERFACE_INCLUDE_DIRECTORIES "${CMAKE_CURRENT_LIST_DIR}/../../../include"
     )
 endif()
