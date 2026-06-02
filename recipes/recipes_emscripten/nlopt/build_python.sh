9326203fce4f5610a3ef45db28938dccfbaea170
Isabel Paredes <isabel.paredes@quantstack.net>
2026-06-02 17:21:54 +0200

    Add libnlopt

--- recipes/recipes_emscripten/nlopt/build_python.sh
+++ recipes/recipes_emscripten/nlopt/build_python.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cp $RECIPE_DIR/setup.py .
+
 NUMPY_INCLUDE_DIR="$PREFIX/lib/python${PY_VER}/site-packages/numpy/_core/include/"
 export CFLAGS="${CFLAGS} -fPIC -DHAVE_UINT32_T -I${NUMPY_INCLUDE_DIR}"
 export CXXFLAGS="${CXXFLAGS} -fPIC -I${NUMPY_INCLUDE_DIR}"
