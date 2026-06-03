3053af4d9cb547740cddaa191e4eecf57553116e
DerThorsten <derthorstenbeier@gmail.com>
2026-06-03 16:05:50 +0200

    genomicranges

--- recipes/recipes_emscripten/bioconductor-rhtslib/build.sh
+++ recipes/recipes_emscripten/bioconductor-rhtslib/build.sh
@@ -3,4 +3,9 @@ rm $PREFIX/lib/libz.so* || true
 
 
 
+# in src/htslib-1.18/hfile.c we want to replace #ifdef HAVE_LIBCURL with #if 0
+# using sed
+sed -i.bak 's/#ifdef HAVE_LIBCURL/#if 0/g' src/htslib-1.18/hfile.c
+rm -f src/htslib-1.18/hfile.c.bak
+
 $R CMD INSTALL $R_ARGS  . --disable-libcurl
