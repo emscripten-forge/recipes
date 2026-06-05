b55c97c1fdf6a5857ff719dd4224533470da4ffc
DerThorsten <derthorstenbeier@gmail.com>
2026-06-05 10:40:10 +0200

    a lot of hacks

--- recipes/recipes_emscripten/bioconductor-rhtslib/build.sh
+++ recipes/recipes_emscripten/bioconductor-rhtslib/build.sh
@@ -3,9 +3,34 @@ rm $PREFIX/lib/libz.so* || true
 
 
 
-# in src/htslib-1.18/hfile.c we want to replace #ifdef HAVE_LIBCURL with #if 0
-# using sed
-sed -i.bak 's/#ifdef HAVE_LIBCURL/#if 0/g' src/htslib-1.18/hfile.c
-rm -f src/htslib-1.18/hfile.c.bak
+echo "remove threading"
 
-$R CMD INSTALL $R_ARGS  . --disable-libcurl
+find . \( -name "*.h" -o -name "*.c" \) -type f \
+    -exec sed -i.bak 's/#ifndef NO_THREADS/#ifdef ENABLE_THREADS/g' {} +
+
+echo "remove libcurl"
+
+sed -i.bak \
+    's/if test "\$enable_libcurl" != no; then/if false; then/' \
+    src/htslib-1.18/configure
+
+echo "remove libcurl even more"
+
+find . \( -name "*.h" -o -name "*.c" \) -type f \
+    -exec sed -i.bak 's/#ifdef HAVE_LIBCURL/#ifdef WE_NEVER_HAVE_LIBCURL/g' {} +
+
+cp $RECIPE_DIR/Makefile.Rhtslib src/htslib-1.18/Makefile.Rhtslib
+
+
+echo 'remove linkage to libcurl src/Makevars'
+#  PKG_LIBS+=-lcurl -lbz2 -llzma -lz
+# we replace -lcurl with an empty string to avoid linking against libcurl, which is not supported in Emscripten
+
+
+sed -i.bak 's/-lcurl//g' src/Makevars
+sed -i.bak 's/-lcurl//g' R/zzz.R
+
+
+
+
+$R CMD INSTALL $R_ARGS  .
