7aa203d698bbfbcd299f5a057c8f78b187ca6469
Alexis Placet <alexis.placet@quantstack.net>
2026-06-08 14:23:25 +0200

    Add miniaudio-c (#5569)
    * Add miniaudio
    
    * rename miniaudio-c
    
    * update directory name

---
+++ recipes/recipes_emscripten/miniaudio-c/build.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -e
+
+mkdir build
+cd build
+
+# Set compiler flags
+export CFLAGS="$CFLAGS $EMCC_CFLAGS"
+export CXXFLAGS="$CXXFLAGS $EMCC_CFLAGS"
+export LDFLAGS="$LDFLAGS $EM_FORGE_SIDE_MODULE_LDFLAGS"
+
+# Configure step
+emcmake cmake ${CMAKE_ARGS} .. \
+    -GNinja \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_PREFIX_PATH=$PREFIX \
+    -DCMAKE_INSTALL_PREFIX=$PREFIX \
+    -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
+    -DCMAKE_C_FLAGS_RELEASE="$CFLAGS" \
+    -DCMAKE_CXX_FLAGS_RELEASE="$CXXFLAGS" \
+    -DCMAKE_MODULE_LINKER_FLAGS_RELEASE="$LDFLAGS" \
+    -DCMAKE_PROJECT_INCLUDE=$RECIPE_DIR/overwriteProp.cmake \
+    -DMINIAUDIO_BUILD_EXAMPLES=OFF \
+    -DMINIAUDIO_BUILD_TESTS=OFF \
+    -DMINIAUDIO_BUILD_TOOLS=OFF \
+    -DMINIAUDIO_NO_EXTRA_NODES=ON
+
+# Build step
+emmake ninja -v
+
+# Install step
+ninja install
+
+# Install CMake config file
+mkdir -p $PREFIX/lib/cmake/miniaudio
+cp $RECIPE_DIR/miniaudioConfig.cmake $PREFIX/lib/cmake/miniaudio/miniaudioConfig.cmake
