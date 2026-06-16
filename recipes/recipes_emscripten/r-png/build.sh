99d8cb9c17b23b75e5d1038aa65dff13ef8d0683
DerThorsten <derthorstenbeier@gmail.com>
2026-06-16 10:17:51 +0200

    namsepace

--- recipes/recipes_emscripten/r-png/build.sh
+++ recipes/recipes_emscripten/r-png/build.sh
@@ -27,10 +27,22 @@ set -eux
 #     │ │ wasm-ld: error: function signature mismatch: R_registerRoutines
 #     │ │ >>> defined as () -> void in dummy.o
 #     │ │ >>> defined as (i32, i32, i32, i32, i32) -> i32 in $PREFIX/lib/R/lib/libR.so
-
+#
+# therefore we just delete it :)
+rm src/dummy.c
 
 # delete shared libz (this forces static linking of zlib)
 rm -f $PREFIX/lib/R/lib/libz.so
 
-rm src/dummy.c
+# the file "NAMESPACE" looks like
+#
+#    useDynLib(png, write_png, read_png)
+#    exportPattern(".*PNG")
+#
+# but we do not use a shared libpng
+rm -f NAMESPACE
+cp $RECIPE_DIR/NAMESPACE .
+
+
+
 R CMD INSTALL $R_ARGS .
