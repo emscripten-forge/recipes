bf0592155fc204ee48273f33186083809448c5b5
Yanshu Wang <wangyenshu@outlook.com>
2026-06-30 18:19:03 +0800

    add cocoalib, nauty

--- recipes/recipes_emscripten/nauty/build.sh
+++ recipes/recipes_emscripten/nauty/build.sh
@@ -4,9 +4,7 @@ set -euxo pipefail
 CPPFLAGS="-I${PREFIX}/include" \
 LDFLAGS="-L${PREFIX}/lib" \
 emconfigure ./configure \
-    --with-libgmp="${PREFIX}/lib/libgmp.a" \
-    --only-cocoalib \
     --prefix="${PREFIX}"
 
-emmake make library -j8
+emmake make -j8
 emmake make install
