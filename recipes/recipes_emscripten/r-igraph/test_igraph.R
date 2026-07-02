54e9fb0a072c23ffe9cde7ccd48dd66eeeb4462d
Isabel Paredes <isabel.paredes@quantstack.net>
2026-07-02 13:34:36 +0200

    Add tests

---
+++ recipes/recipes_emscripten/r-igraph/test_igraph.R
@@ -0,0 +1,11 @@
+library(igraph)
+
+g <- make_empty_graph()
+g <- make_graph(edges = c(1, 2, 1, 5), n = 10, directed = FALSE)
+g <- make_graph(~ 1--2, 1--5, 3, 4, 5, 6, 7, 8, 9, 10)
+g <- add_vertices(g, 3)
+g <- add_edges(g, edges = c(1, 35, 1, 36, 34, 37))
+print(g)
+
+g <- make_graph("Zachary")
+print(g)
