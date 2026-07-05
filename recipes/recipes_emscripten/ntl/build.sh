5ab4d909bb2d289f6539ce68ea3c34fb63b6dd23
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2026-07-04 18:06:28 -0700

    recipes/recipes_emscripten/ntl: Update build script from https://github.com/Singular/Singular-in-browser/blob/main/build.sh

--- recipes/recipes_emscripten/ntl/build.sh
+++ recipes/recipes_emscripten/ntl/build.sh
@@ -1,6 +1,41 @@
 mkdir -p $PREFIX
 
 cd src
-./configure DEF_PREFIX="$PREFIX" SHARED=off NTL_GMP_LIP=on NTL_GF2X_LIB=off
-make
-make install
+
+# From https://github.com/Singular/Singular-in-browser/blob/main/build.sh
+
+emconfigure ./configure \
+    CXX="em++" \
+    CXXFLAGS="-O2 -fexceptions -s WASM=1 -s NODERAWFS=1" \
+    PREFIX="$PREFIX" \
+    GMP_PREFIX="$PREFIX" \
+    NTL_GMP_LIP=on \
+    NTL_STD_CXX14=on \
+    SHARED=off \
+    NATIVE=off \
+    TUNE=generic \
+    NTL_THREADS=off
+
+sed -e 's/^CC=gcc/CC=emcc -s NODERAWFS=1/' \
+    -e 's/^WIZARD=on/WIZARD=off/' \
+    makefile > makefile.patched
+mv makefile.patched makefile
+
+sed -e 's|^\t\./MakeDesc|\tchmod +x ./MakeDesc \&\& node ./MakeDesc|' \
+    -e 's|^\t\./gen_gmp_aux|\tchmod +x ./gen_gmp_aux \&\& node ./gen_gmp_aux|' \
+    -e 's|^\t\./gen_lip_gmp_aux|\tchmod +x ./gen_lip_gmp_aux \&\& node ./gen_lip_gmp_aux|' \
+    -e 's|^\t\./gen_lip_gmp_aux|\tchmod +x ./gen_lip_gmp_aux \&\& node ./gen_lip_gmp_aux|' \
+    makefile > makefile.patched
+mv makefile.patched makefile
+
+sed -i 's|if ./CheckFeatures|if node ./CheckFeatures|g' MakeCheckFeatures
+
+if [ -f MakeCheckThreads ]; then
+    sed -i 's|./CheckThreads|node ./CheckThreads|g' MakeCheckThreads
+fi
+
+emmake make -j8
+
+emmake make install
+
+emranlib "$PREFIX/lib/libntl.a"
