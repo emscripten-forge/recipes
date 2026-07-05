cf5956e0d2f9744f4be7a43285dc9e039f116c91
Yanshu Wang <wangyenshu@outlook.com>
2026-07-05 12:24:27 +0800

    add iml

---
+++ recipes/recipes_emscripten/iml/build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+emconfigure ./configure \
+    --build=i686-pc-linux-gnu \
+    -with-gmp-include="${PREFIX}/include" \
+    --with-gmp-lib="${PREFIX}/lib" \
+    --with-cblas="-L${PREFIX}/lib -lopenblas" \
+    --disable-shared \
+    --prefix="${PREFIX}" \
+    CPPFLAGS="-I${PREFIX}/include" \
+    CFLAGS="${CFLAGS:-}" \
+    LDFLAGS="${LDFLAGS:-} -L${PREFIX}/lib"
+
+emmake make -j8
+emmake make install
