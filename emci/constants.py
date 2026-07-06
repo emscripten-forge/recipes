e0d5a86ddd3146014d292fec8242d38b4c1b893e
DerThorsten <derthorstenbeier@gmail.com>
2026-07-06 12:53:15 +0200

    exp upload

--- emci/constants.py
+++ emci/constants.py
@@ -26,4 +26,3 @@ CONDA_BLD_DIR = os.path.join(CONDA_PREFIX, "conda-bld")
 Path(CONDA_BLD_DIR).mkdir(exist_ok=True)
 
 DEFAULT_EMSCRIPTEN_FORGE_CHANNEL = "emscripten-forge-4x"
-EXPERIMENTAL_EMSCRIPTEN_FORGE_CHANNEL = "emscripten-forge-4x-experimental"
