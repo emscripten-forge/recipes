528a90c364d845ff09404464b6703b8bff701867
Yanshu Wang <wangyenshu@outlook.com>
2026-07-12 21:18:27 +0800

    add e-antic, normaliz

---
+++ recipes/recipes_emscripten/normaliz/build.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+export PKG_CONFIG_PATH="${PREFIX}/lib/pkgconfig"
+export CFLAGS="-fPIC"
+export CPPFLAGS="-I${PREFIX}/include"
+export LDFLAGS="-L${PREFIX}/lib"
+export CXXFLAGS="-std=c++17 -fPIC -Wno-invalid-utf8 -Wno-unused-but-set-variable -Wno-deprecated-declarations"
+
+emconfigure ./configure \
+    --build=i686-pc-linux-gnu \
+    --host=wasm32-unknown-emscripten \
+    --disable-dependency-tracking \
+    --disable-shared \
+    --with-gmp="${PREFIX}" \
+    --with-flint="${PREFIX}" \
+    --with-cocoalib="${PREFIX}" \
+    --with-nauty="${PREFIX}" \
+    --prefix="${PREFIX}"
+
+emmake make -j8
+emmake make install
+cp sources/normaliz.wasm "${PREFIX}/bin"
