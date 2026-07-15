ea408c8b13f988117a9b43d6aee61a1e3e6ef68a
DerThorsten <derthorstenbeier@gmail.com>
2026-07-15 13:27:35 +0200

    clean

--- recipes/recipes_emscripten/r-truncnorm/test_truncnorm.R
+++
@@ -1,8 +0,0 @@
-library(truncnorm)
-
-dtruncnorm(x, a=-Inf, b=Inf, mean = 0, sd = 1)
-ptruncnorm(q, a=-Inf, b=Inf, mean = 0, sd = 1)
-qtruncnorm(p, a=-Inf, b=Inf, mean = 0, sd = 1)
-rtruncnorm(n, a=-Inf, b=Inf, mean = 0, sd = 1)
-etruncnorm(a=-Inf, b=Inf, mean=0, sd=1)
-vtruncnorm(a=-Inf, b=Inf, mean=0, sd=1)
