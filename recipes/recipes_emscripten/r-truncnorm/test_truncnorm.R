1ea0d29b27a17f812499123369fefc772931b450
DerThorsten <derthorstenbeier@gmail.com>
2026-07-15 13:17:47 +0200

    extended tests

--- recipes/recipes_emscripten/r-truncnorm/test_truncnorm.R
+++ recipes/recipes_emscripten/r-truncnorm/test_truncnorm.R
@@ -1,1 +1,8 @@
 library(truncnorm)
+
+dtruncnorm(x, a=-Inf, b=Inf, mean = 0, sd = 1)
+ptruncnorm(q, a=-Inf, b=Inf, mean = 0, sd = 1)
+qtruncnorm(p, a=-Inf, b=Inf, mean = 0, sd = 1)
+rtruncnorm(n, a=-Inf, b=Inf, mean = 0, sd = 1)
+etruncnorm(a=-Inf, b=Inf, mean=0, sd=1)
+vtruncnorm(a=-Inf, b=Inf, mean=0, sd=1)
