aa0a407d55eb691cb22cb0277fe8203096496e76
Yanshu Wang <wangyenshu@outlook.com>
2026-07-19 09:51:40 +0800

    update

--- recipes/recipes_emscripten/M2/build.sh
+++ recipes/recipes_emscripten/M2/build.sh
@@ -8,13 +8,16 @@ mkdir install_prefix
 # Strip the CMP0167 line
 sed -i '/CMP0167/d' ../../cmake/check-libraries.cmake
 
-# Excise the missing submodules (mathicgb, mathic, memtailor) from CMake targets
+# Excise the missing submodules from CMake targets
 sed -i '/add_subdirectory(memtailor)/d' ../../Macaulay2/e/CMakeLists.txt
 sed -i '/add_subdirectory(mathic)/d' ../../Macaulay2/e/CMakeLists.txt
 sed -i '/add_subdirectory(mathicgb)/d' ../../Macaulay2/e/CMakeLists.txt
 sed -i '/target_compile_options(mathicgb/,/)/d' ../../Macaulay2/e/CMakeLists.txt
-sed -i '/install(TARGETS.*mathicgb/,/)/d' ../../Macaulay2/e/CMakeLists.txt
 sed -i '/export(TARGETS.*mathicgb/,/)/d' ../../Macaulay2/CMakeLists.txt
+
+# Patch e/CMakeLists.txt using the EXACT text from your unmodified source file
+sed -i "s|PUBLIC memtailor mathic mathicgb|PUBLIC $PREFIX/lib/libmemtailor.a $PREFIX/lib/libmathic.a $PREFIX/lib/libmathicgb.a|g" ../../Macaulay2/e/CMakeLists.txt
+sed -i 's|mathicgb mathic memtailor||g' ../../Macaulay2/e/CMakeLists.txt
 
 # Patch FindNauty.cmake to prevent it from shadowing the CLI variables
 sed -i '/set(NAUTY_INCLUDE_DIR NOTFOUND)/d' ../../cmake/FindNauty.cmake
@@ -24,6 +27,9 @@ sed -i '/_NAUTY_check_version()/d' ../../cmake/FindNauty.cmake
 # Patch gc-include.h to stop it from forcing multithreading on Boehm GC
 sed -i '/#define GC_THREADS 1/d' ../../include/M2/gc-include.h
 sed -i '/#define GC_LINUX_THREADS/d' ../../include/M2/gc-include.h
+
+# Force static linking of OpenBLAS by deleting the dynamic library from the build prefix
+rm -f $PREFIX/lib/libopenblas.so*
 
 # Create active dummy executables that return a clean exit 0
 mkdir -p $PREFIX/bin
@@ -46,8 +52,8 @@ emcmake cmake -S ../.. -B . \
       -DLAPACK_LIBRARIES=$PREFIX/lib/libopenblas.a \
       -DBoost_NO_BOOST_CMAKE=ON \
       -DWITH_OMP=OFF \
-      -DCMAKE_C_FLAGS="-fopenmp -DGC_NO_THREAD_REDIRECTS -DGC_NO_THREADS=2" \
-      -DCMAKE_CXX_FLAGS="-fopenmp -DGC_NO_THREAD_REDIRECTS -DGC_NO_THREADS=2" \
+      -DCMAKE_C_FLAGS="-DGC_NO_THREAD_REDIRECTS -DGC_NO_THREADS=2" \
+      -DCMAKE_CXX_FLAGS="-DGC_NO_THREAD_REDIRECTS -DGC_NO_THREADS=2" \
       -DCMAKE_EXE_LINKER_FLAGS="-s ALLOW_MEMORY_GROWTH=1 -s NODERAWFS=1" \
       -DREADLINE_INCLUDE_DIR=$PREFIX/include \
       -DREADLINE_LIBRARY=$PREFIX/lib/libreadline.a \
