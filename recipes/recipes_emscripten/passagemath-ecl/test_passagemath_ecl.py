c742b0cf611bd0fb6d3634637a5de51bdf929521
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2026-07-26 12:48:25 -0700

    recipes/recipes_emscripten/passagemath-ecl/test_passagemath_ecl.py (test_ecl_eval): New

--- recipes/recipes_emscripten/passagemath-ecl/test_passagemath_ecl.py
+++ recipes/recipes_emscripten/passagemath-ecl/test_passagemath_ecl.py
@@ -3,3 +3,9 @@ import pytest
 
 def test_import_passagemath_ecl():
     import passagemath_ecl
+
+
+def test_ecl_eval():
+    import passagemath_ecl
+    from sage.libs.ecl import ecl_eval
+    ecl_eval('(cons 1 1)')
