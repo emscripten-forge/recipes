63d45fdedff1f8cb71839986c9a853d49017cfd7
DerThorsten <derthorstenbeier@gmail.com>
2026-07-28 10:45:05 +0200

    try native

--- recipes/recipes_emscripten/r-rcppparallel/src/RcppParallel/src/init.c
+++
@@ -1,9 +0,0 @@
-#include <R.h>
-#include <Rinternals.h>
-#include <R_ext/Rdynload.h>
-
-void R_init_RcppParallel(DllInfo *dll)
-{
-    R_registerRoutines(dll, NULL, NULL, NULL, NULL);
-    R_useDynamicSymbols(dll, FALSE);
-}
