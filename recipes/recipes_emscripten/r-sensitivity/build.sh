cc1df2296d5d1dbc73de3056ff74baf2796477fa
DerThorsten <derthorstenbeier@gmail.com>
2026-07-28 11:30:30 +0200

    undelete

--- recipes/recipes_emscripten/r-sensitivity/build.sh
+++ recipes/recipes_emscripten/r-sensitivity/build.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-echo "INSPECTING RcppParallel shared library for tbbParallelFor symbol"
-nm -D $BUILD_PREFIX/lib/R/library/RcppParallel/libs/RcppParallel.so | grep tbbParallelFor
-
-
 
 $R CMD INSTALL $R_ARGS .
