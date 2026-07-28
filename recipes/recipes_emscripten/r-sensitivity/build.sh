c5d4a04f9b612a092cfd662bef71ae6e899e500a
DerThorsten <derthorstenbeier@gmail.com>
2026-07-28 10:23:46 +0200

    src

--- recipes/recipes_emscripten/r-sensitivity/build.sh
+++ recipes/recipes_emscripten/r-sensitivity/build.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+echo "INSPECTING RcppParallel shared library for tbbParallelFor symbol"
+nm -D $BUILD_PREFIX/lib/R/library/RcppParallel/libs/RcppParallel.so | grep tbbParallelFor
+
+
+
 $R CMD INSTALL $R_ARGS .
