d711c24d129b5c25b7859165f930cc4cfa3c2104
Julien Jerphanion <git@jjerphan.xyz>
2026-08-07 16:46:06 +0200

    Have `numpy` use `openblas` for BLAS and LAPACK
    Signed-off-by: Julien Jerphanion <git@jjerphan.xyz>

--- recipes/recipes_emscripten/numpy/build.sh
+++ recipes/recipes_emscripten/numpy/build.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
+set -ex
+
 echo "PYTHON"
 
 rm -r -f branding
 
+# necessary for cross-compilation to point to the right env
+export PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig
+
 export CFLAGS="$CFLAGS -Wno-return-type -Wno-implicit-function-declaration -msimd128 -fwasm-exceptions -s SUPPORT_LONGJMP"
-export MESON_CROSS_FILE=$RECIPE_DIR/emscripten.meson.cross 
+export MESON_CROSS_FILE=$RECIPE_DIR/emscripten.meson.cross
 export LDFLAGS="$LDFLAGS -sWASM_BIGINT 	-s WASM_BIGINT -fwasm-exceptions -s SUPPORT_LONGJMP"
 
 cp $RECIPE_DIR/config/config.h.in  numpy/_core/config.h.in
-# 
+#
 
 # otherwise "cython" is not properly executable
 echo "add shebang to cython file"
@@ -22,5 +27,6 @@ sed -i 's/-fexceptions/-fwasm-exceptions/g' numpy/_core/meson.build
 
 
 MESON_ARGS="-Dhave_backtrace=false" ${PYTHON} -m pip install . -vvv --no-deps --no-build-isolation \
-    -Csetup-args="-Dallow-noblas=true" \
+    -Csetup-args="-Dblas=openblas" \
+    -Csetup-args="-Dlapack=openblas" \
     -Csetup-args="--cross-file=$MESON_CROSS_FILE"
