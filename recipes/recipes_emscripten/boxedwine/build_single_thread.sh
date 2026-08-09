1b89bcacdcb921c0e2e692d3bcef8564bbc4adc8
wangyenshu <155622798+wangyenshu@users.noreply.github.com>
2026-08-09 12:14:15 -0400

    Update build_single_thread.sh

--- recipes/recipes_emscripten/boxedwine/build_single_thread.sh
+++ recipes/recipes_emscripten/boxedwine/build_single_thread.sh
@@ -10,4 +10,6 @@ make release
 OUT_DIR="$PREFIX/share/boxedwine-single-thread"
 mkdir -p "$OUT_DIR"
 
-cp -r Build/Release/* "$OUT_DIR/"
+cp Build/Release/boxedwine.html "$OUT_DIR/"
+cp Build/Release/boxedwine.js "$OUT_DIR/"
+cp Build/Release/boxedwine.wasm "$OUT_DIR/"
