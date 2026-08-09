8dbb8bc9f9cd8ae6ace13fac51410f7453cb3858
wangyenshu <155622798+wangyenshu@users.noreply.github.com>
2026-08-09 12:15:27 -0400

    Update build_multi_thread.sh

--- recipes/recipes_emscripten/boxedwine/build_multi_thread.sh
+++ recipes/recipes_emscripten/boxedwine/build_multi_thread.sh
@@ -10,4 +10,6 @@ make multiThreaded
 OUT_DIR="$PREFIX/share/boxedwine-multi-thread"
 mkdir -p "$OUT_DIR"
 
-cp -r Build/MultiThreaded/* "$OUT_DIR/"
+cp Build/MultiThreaded/boxedwine.html "$OUT_DIR/"
+cp Build/MultiThreaded/boxedwine.js "$OUT_DIR/"
+cp Build/MultiThreaded/boxedwine.wasm "$OUT_DIR/"
