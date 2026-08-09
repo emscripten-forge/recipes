ab7097049d98f4ac0f9b696bb89a0e6b53c37d92
wangyenshu <155622798+wangyenshu@users.noreply.github.com>
2026-08-09 14:37:35 -0400

    switch to ASYNCIFY=1

--- recipes/recipes_emscripten/gap-in-the-browser/build.sh
+++ recipes/recipes_emscripten/gap-in-the-browser/build.sh
@@ -88,7 +88,7 @@ if [[ ! -f GNUmakefile ]] || ! grep '/emcc' GNUmakefile > /dev/null; then
     emconfigure ./configure --prefix="${PREFIX}" ABI=32 \
     --with-gmp=$AUX_PREFIX \
     --with-zlib=$AUX_PREFIX \
-    LDFLAGS="-s JSPI -O2"
+    LDFLAGS="-s ASYNCIFY=1 -O2"
 fi;
 
 # Target the Linux/Unix block to redefine shared lib extensions to static
@@ -123,7 +123,7 @@ fi
 
 # The EXEEXT is usually for windows, but here it lets us set GAP's extension,
 # which lets us produce a html page to run GAP in.
-emmake make -j8 LDFLAGS="-lidbfs.js -s JSPI -sTOTAL_STACK=32mb -sINITIAL_MEMORY=2048mb -O2" EXEEXT=".html"
+emmake make -j8 LDFLAGS="-lidbfs.js -s ASYNCIFY=1 -sTOTAL_STACK=32mb -sINITIAL_MEMORY=2048mb -O2" EXEEXT=".html"
 emmake make install-bin install-gaproot install-sysinfo install-headers install-libgap
 
 bash etc/emscripten/assemble-website.sh
