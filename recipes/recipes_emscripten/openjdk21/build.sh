4717941efb287d1815e801525decd93fb7f5195d
wangyenshu <155622798+wangyenshu@users.noreply.github.com>
2026-08-10 20:12:39 -0400

    build complete buildtools copy java hotspot static-libs-image

--- recipes/recipes_emscripten/openjdk21/build.sh
+++ recipes/recipes_emscripten/openjdk21/build.sh
@@ -48,9 +48,9 @@ bash configure \
   OBJDUMP="$EMSDK_LLVM_BIN/llvm-objdump" \
   BUILD_CC=clang BUILD_CXX=clang++
 
-make CONF=emscripten-zero java.base-static-libs JOBS="${CPU_COUNT:-4}"
-make CONF=emscripten-zero buildtools-hotspot java.base-copy JOBS="${CPU_COUNT:-4}"
-make CONF=emscripten-zero hotspot-only JOBS="${CPU_COUNT:-4}"
+make CONF=emscripten-zero static-libs-image JOBS="${CPU_COUNT:-4}"
+make CONF=emscripten-zero hotspot JOBS="${CPU_COUNT:-4}"
+make CONF=emscripten-zero buildtools copy java JOBS="${CPU_COUNT:-4}"
 
 WASM_NATIVE_DIR="$PREFIX/lib/wasm-native"
 mkdir -p "$WASM_NATIVE_DIR"
