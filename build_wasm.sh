3ba60054f1cf7fa7841367ec23ffca79d3358bf2
DerThorsten <derthorstenbeier@gmail.com>
2026-08-24 14:28:58 +0200

    better tests

--- build_wasm.sh
+++ build_wasm.sh
@@ -7,6 +7,15 @@ set -e
 
 RECIPE_DIR=$1
 
+echo "Building ${RECIPE_DIR} for emscripten-wasm64"
+rattler-build build \
+    -c  https://repo.prefix.dev/emscripten-forge-bot/emscripten-forge-6x \
+    -c microsoft \
+    -c conda-forge \
+    --target-platform emscripten-wasm64 \
+    -m variant.yaml \
+    --recipe $RECIPE_DIR
+    
 echo "Building ${RECIPE_DIR} for emscripten-wasm32"
 rattler-build build \
     -c  https://repo.prefix.dev/emscripten-forge-bot/emscripten-forge-6x \
@@ -16,11 +25,3 @@ rattler-build build \
     -m variant.yaml \
     --recipe $RECIPE_DIR
 
-echo "Building ${RECIPE_DIR} for emscripten-wasm64"
-rattler-build build \
-    -c  https://repo.prefix.dev/emscripten-forge-bot/emscripten-forge-6x \
-    -c microsoft \
-    -c conda-forge \
-    --target-platform emscripten-wasm64 \
-    -m variant.yaml \
-    --recipe $RECIPE_DIR
---
+++ recipes/recipes_native/generic-testing/build.sh
@@ -0,0 +1,3 @@
+ls 
+mkdir -p $PREFIX/lib/python$PY_VER/site-packages
+cp -r generic_testing $PREFIX/lib/python$PY_VER/site-packages
