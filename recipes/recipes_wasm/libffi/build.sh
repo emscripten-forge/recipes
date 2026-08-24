384694e05b0816dc2a4b3fe95b5a25460e99c67c
Thorsten Beier <thorsten.beier@quantstack.net>
2026-08-24 14:46:15 +0200

    More6x (#6337)
    * more
    
    * proper test
    
    * req
    
    * better tests
    
    * simplyfy recipe

--- recipes/recipes_wasm/libffi/build.sh
+++ recipes/recipes_wasm/libffi/build.sh
@@ -3,6 +3,12 @@
 #this build script is adapted from the libffi repo at `testsuite/emscripten/build.sh`
 set -e
 
+# GET THE TARGET_PLATFORM from the environment variable TARGET_PLATFORM
+if [ -z "$target_platform" ]; then
+    echo "target_platform is not set. Please set it to the target platform (e.g, wasm32-unknown-emscripten)."
+    exit 1
+fi
+
 # Working directories
 SOURCE_DIR=$PWD
 TARGET=$SOURCE_DIR/build
@@ -20,7 +26,15 @@ export PKG_CONFIG_PATH="$TARGET/lib/pkgconfig"
 export EM_PKG_CONFIG_PATH="$PKG_CONFIG_PATH"
 
 # Specific variables for cross-compilation
-export CHOST="wasm32-unknown-linux" # wasm32-unknown-emscripten
+if [[ "$target_platform" == "emscripten-wasm32" ]]; then
+    export CHOST="wasm32-unknown-linux" # wasm32-unknown-emscripten
+elif [[ "$target_platform" == "emscripten-wasm64" ]]; then
+    export CHOST="wasm64-unknown-linux" # wasm64-unknown-emscripten
+else
+    echo "Unsupported target_platform: $target_platform"
+    exit 1
+fi
+
 
 autoreconf -fiv
 emconfigure ./configure --host=$CHOST --prefix="$TARGET" --enable-static --disable-shared --disable-dependency-tracking \
