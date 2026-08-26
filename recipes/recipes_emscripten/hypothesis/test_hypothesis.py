ece2d943fb7b433127f7d8dfd8586c563b60ae39
Isabel Paredes <isabel.paredes@quantstack.net>
2026-08-26 17:14:03 +0200

    Sync recipes and workflows from main (#6360)
    * Sync recipes from main
    
    * Copy syncing workflow

---
+++ recipes/recipes_emscripten/hypothesis/test_hypothesis.py
@@ -0,0 +1,20 @@
+def test_import_hypothesis():
+    import hypothesis
+
+
+def test_import_native():
+    import hypothesis._native
+
+
+def test_given_integers():
+    from hypothesis import given, strategies as st
+
+    seen = []
+
+    @given(st.integers(min_value=0, max_value=10))
+    def f(n):
+        seen.append(n)
+        assert 0 <= n <= 10
+
+    f()
+    assert seen
