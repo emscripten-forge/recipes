da0e6103ea8c4cb20c2fec590836806bbf0abb40
David Brochart <david.brochart@gmail.com>
2026-08-28 11:52:34 +0200

    fix

--- recipes/recipes_emscripten/obstore/build.sh
+++ recipes/recipes_emscripten/obstore/build.sh
@@ -2,4 +2,4 @@
 
 export MATURIN_PYTHON_SYSCONFIGDATA_DIR=${PREFIX}/etc/conda/_sysconfigdata__emscripten_wasm32-emscripten.py
 export PYO3_USE_ABI3_FORWARD_COMPATIBILITY=1
-${PYTHON} -m pip install /obstore. ${PIP_ARGS}
+${PYTHON} -m pip install ./obstore ${PIP_ARGS}
