92b98ec9ace1891c0a68e33fc6b3d727ed95b684
wangyenshu <155622798+wangyenshu@users.noreply.github.com>
2026-09-02 09:05:56 -0400

    Update build.sh

--- recipes/recipes_emscripten/M2/build.sh
+++ recipes/recipes_emscripten/M2/build.sh
@@ -594,4 +594,4 @@ cp "$M2_BINARY_DIR/M2-binary.js" \
 cp "$M2_BINARY_DIR/M2-binary.wasm" \
   "${PREFIX}/bin/M2-binary.wasm"
 
-cp -a "${SRC_DIR}/web/*" "${PREFIX}/bin/"
+cp -a "$SRC_DIR"/web/* "$PREFIX/bin/"
