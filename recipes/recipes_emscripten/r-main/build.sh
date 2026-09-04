dde038807741140358d4fcbd2ade0c15b68e4625
Isabel Paredes <isabel.paredes@quantstack.net>
2026-09-04 19:57:29 +0200

    Move R-Shiny and friends to main channel (#6530)
    * Rebuild native r-httpuv
    
    * Rebuild wasm r-httpuv
    
    * Add sparkly patches
    
    * Rebuild r-shiny
    
    * Rebuild r-main
    
    * Create variants
    
    * Fix osx
    
    * Clean up

--- recipes/recipes_emscripten/r-main/build.sh
+++ recipes/recipes_emscripten/r-main/build.sh
@@ -5,6 +5,11 @@ set -eux
 export R_HOME="${PREFIX}/lib/R"
 export R_SHARE_DIR="${R_HOME}/share"
 
+# Only link libpython (and its transitive deps) for the static-libpython variant.
+if [[ "${R_MAIN_MODULE_STATIC_LIBPYTHON}" == "1" ]]; then
+    export RPY_LIBS="-lbz2 -lz -lsqlite3 -lffi -lssl -lcrypto -llzma -lpython3.13"
+fi
+
 rm $PREFIX/lib/libz.so* || true
 rm $PREFIX/lib/libssl.so* || true
 rm $PREFIX/lib/libcrypto.so* || true
