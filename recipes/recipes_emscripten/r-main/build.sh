4c1e1a15011e05b4e900fea3907e3705cd9d445a
Isabel Paredes <isabel.paredes@quantstack.net>
2026-09-04 19:17:50 +0200

    Rebuild r-main

--- recipes/recipes_emscripten/r-main/build.sh
+++ recipes/recipes_emscripten/r-main/build.sh
@@ -4,6 +4,7 @@ set -eux
 
 export R_HOME="${PREFIX}/lib/R"
 export R_SHARE_DIR="${R_HOME}/share"
+export RPY_LIBS="-lbz2 -lz -lsqlite3 -lffi -lssl -lcrypto -llzma -lpython3.13"
 
 rm $PREFIX/lib/libz.so* || true
 rm $PREFIX/lib/libssl.so* || true
