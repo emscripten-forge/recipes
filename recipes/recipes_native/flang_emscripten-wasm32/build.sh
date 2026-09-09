24d01ff9b7e0ce39fc2828546fa8451b44eb2aad
Isabel Paredes <isabel.paredes@quantstack.net>
2026-09-09 17:39:37 +0200

    Update build script to support both targets

--- recipes/recipes_native/flang_emscripten-wasm32/build.sh
+++ recipes/recipes_native/flang_emscripten-wasm32/build.sh
@@ -2,6 +2,19 @@
 
 set -eux
 
+if [ -z "${FLANG_WASM64:-}" ]; then
+    echo "FLANG_WASM64 is not set, it should be ON or OFF."
+    exit 1
+fi
+
+if [ "${FLANG_WASM64}" = "ON" ]; then
+    WASM_TARGET="wasm64-unknown-emscripten"
+else
+    WASM_TARGET="wasm32-unknown-emscripten"
+fi
+
+echo "WASM_TARGET is set to ${WASM_TARGET}"
+
 mkdir _build
 cd _build
 
@@ -9,20 +22,16 @@ cmake -G Ninja \
     ${CMAKE_ARGS} \
     -DBUILD_SHARED_LIBS=ON \
     -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_CXX_STANDARD=17 \
     -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
-    -DCMAKE_INSTALL_PREFIX=$PREFIX \
-    -DCMAKE_MODULE_PATH=../cmake/Modules \
     -DCMAKE_PREFIX_PATH=$PREFIX \
-    -DLLVM_DEFAULT_TARGET_TRIPLE=wasm32-unknown-emscripten \
-    -DLLVM_EXTERNAL_LIT=$PREFIX/bin/lit \
-    -DLLVM_LIT_ARGS=-v \
+    -DLLVM_DEFAULT_TARGET_TRIPLE="${WASM_TARGET}" \
     -DLLVM_CMAKE_DIR=$PREFIX/lib/cmake/llvm \
     -DLLVM_DIR=$PREFIX/lib/cmake/llvm \
     -DLLVM_TARGETS_TO_BUILD="WebAssembly" \
-    -DLLVM_ENABLE_PROJECTS="clang;flang;mlir" \
     -DCLANG_DIR=$PREFIX/lib/cmake/clang \
     -DFLANG_INCLUDE_TESTS=OFF \
+    -DFLANG_RUNTIME_F128_MATH_LIB="" \
+    -DFLANG_WASM64="${FLANG_WASM64}" \
     -DMLIR_DIR=$PREFIX/lib/cmake/mlir \
     ../flang
 
@@ -34,5 +43,5 @@ cmake --install .
 for TASK in "activate" "deactivate"
 do
     mkdir -p "${PREFIX}/etc/conda/${TASK}.d"
-    envsubst '$PKG_VERSION' < "${RECIPE_DIR}/${TASK}.sh" > "${PREFIX}/etc/conda/${TASK}.d/${TASK}_${PKG_NAME}.sh"
+    envsubst '${WASM_TARGET}' < "${RECIPE_DIR}/${TASK}.sh" > "${PREFIX}/etc/conda/${TASK}.d/${TASK}_${PKG_NAME}.sh"
 done
