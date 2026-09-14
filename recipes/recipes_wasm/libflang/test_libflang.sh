2d868ca6bbe8899c18d2814cd1c37a9056b86a02
Isabel Paredes <isabel.paredes@quantstack.net>
2026-09-14 16:58:48 +0200

    Fix tests

--- recipes/recipes_wasm/libflang/test_libflang.sh
+++ recipes/recipes_wasm/libflang/test_libflang.sh
@@ -8,4 +8,10 @@ emcc hello.o $LDFLAGS -o hello.js -sEXIT_RUNTIME=1 -sMAIN_MODULE=1
 if [ -f $PREFIX/lib/libflang_rt.runtime.so ]; then
     cp $PREFIX/lib/libflang_rt.runtime.so .
 fi
+
+# TODO: This may be a real bug in upstream/emscripten
+# Workaround wasm64 MAIN_MODULE callMain using raw main (missing envp → BigInt error).
+# Minified JS uses "main"; unminified uses 'main'.
+sed -i -E "s/resolveGlobalSymbol\(['\"]main['\"]\)\.sym/_main/" hello.js
+
 node hello.js | grep -F "Hello, Fortran!"
