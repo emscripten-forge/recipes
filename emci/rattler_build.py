cd998aaad4fd5bd62631a1bc604edf62cef26f01
DerThorsten <derthorstenbeier@gmail.com>
2026-09-14 11:30:42 +0200

    ci

--- emci/rattler_build.py
+++ emci/rattler_build.py
@@ -34,8 +34,8 @@ def build_with_rattler(recipe=None, recipes_dir=None, target_platform=None, skip
     # add conda forge and emscripten-forge channels
     cmd.extend([
         "-c", "https://repo.prefix.dev/emscripten-forge-bot/emscripten-forge-6x",
-        "-c", "microsoft",
         "-c", "conda-forge",
+        "-c", "microsoft",
         "-c", "bioconda"
     ])
 
