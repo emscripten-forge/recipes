13b82df2072205f7a043cb76b798d47f50b50cd8
DerThorsten <derthorstenbeier@gmail.com>
2026-09-14 11:25:13 +0200

    ci

--- build_wasm.sh
+++ build_wasm.sh
@@ -23,8 +23,8 @@ if [ "$BUILD_WASM32" = true ]; then
     echo "Building ${RECIPE_DIR} for emscripten-wasm32"
     rattler-build build \
         -c  https://repo.prefix.dev/emscripten-forge-bot/emscripten-forge-6x \
-        -c microsoft \
         -c conda-forge \
+        -c microsoft \
         --target-platform emscripten-wasm32 \
         -m variant.yaml \
         --recipe $RECIPE_DIR
@@ -35,8 +35,8 @@ if [ "$BUILD_WASM64" = true ]; then
     echo "Building ${RECIPE_DIR} for emscripten-wasm64"
     rattler-build build \
         -c  https://repo.prefix.dev/emscripten-forge-bot/emscripten-forge-6x \
-        -c microsoft \
         -c conda-forge \
+        -c microsoft \
         --target-platform emscripten-wasm64 \
         -m variant.yaml \
         --recipe $RECIPE_DIR
