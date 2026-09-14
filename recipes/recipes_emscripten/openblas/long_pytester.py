258435e8a8cd6f3082c25acda1746fa6c30aab9a
Julien Jerphanion <git@jjerphan.xyz>
2026-09-14 10:22:06 +0200

    Give NumPy and SciPy OpenBLAS tests a 2-hour Playwright timeout.
    Signed-off-by: Julien Jerphanion <git@jjerphan.xyz>

--- recipes/recipes_emscripten/openblas/long_pytester.py
+++ recipes/recipes_emscripten/openblas/long_pytester.py
@@ -4,7 +4,7 @@ import os
 from playwright.async_api import Page
 
 # pyjs_code_runner hardcodes 4 minutes, which is too short for these suites.
-_TIMEOUT_MS = int(os.environ.get("PYTESTER_TIMEOUT_MIN", "60")) * 60 * 1000
+_TIMEOUT_MS = int(os.environ.get("PYTESTER_TIMEOUT_MIN", "120")) * 60 * 1000
 _orig_set_default_timeout = Page.set_default_timeout
 
 
