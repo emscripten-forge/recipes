0573810c3087806a49ad3b30536357ac52357207
Isabel Paredes <isabel.paredes@quantstack.net>
2026-09-14 14:29:54 +0200

    Add tests

---
+++ recipes/recipes_wasm/libflang/test_static.sh
@@ -0,0 +1,7 @@
+
+export LDFLAGS="-L$PREFIX/lib -lflang_rt.runtime"
+
+# Test the library
+flang $FFLAGS -c ./hello.f90 -o hello.o
+emcc hello.o $LDFLAGS -o hello.js -sEXIT_RUNTIME=1
+node hello.js | grep -F "Hello, Fortran!"
