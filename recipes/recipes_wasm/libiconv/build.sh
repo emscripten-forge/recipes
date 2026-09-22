fab0ed9e59d0dda46cd74dfc3b4974840677c7e2
Isabel Paredes <isabel.paredes@quantstack.net>
2026-09-22 12:54:05 +0200

    Add libiconv (#6890)
    * Move recipe
    
    * Enable wasm64

--- recipes/recipes_wasm/libiconv/build.sh
+++ recipes/recipes_wasm/libiconv/build.sh
@@ -5,17 +5,23 @@ set -ex
 mkdir _build
 cd _build
 
+if [[ "${target_platform}" == "emscripten-wasm64" ]]; then
+  host="wasm64-unknown-emscripten"
+else
+  host="wasm32-unknown-emscripten"
+fi
+
 export CFLAGS="$CFLAGS -fPIC -fwasm-exceptions"
 export CXXFLAGS="$CXXFLAGS -fwasm-exceptions"
 export LDFLAGS="$LDFLAGS -fwasm-exceptions"
 
 emconfigure ../configure \
-    CFLAGS="$CFLAGS" \
-    --prefix=$PREFIX \
-    --build="x86_64-conda-linux-gnu" \
-    --host="wasm32-unknown-emscripten" \
-    --disable-dependency-tracking \
-    --disable-shared
+  CFLAGS="$CFLAGS" \
+  --prefix=$PREFIX \
+  --build="x86_64-conda-linux-gnu" \
+  --host="${host}" \
+  --disable-dependency-tracking \
+  --disable-shared
 emmake make -j ${CPU_COUNT:-3} install
 
 find . -iname "iconv.wasm" -exec cp {} $PREFIX/bin/ \;
