32cd521b38a4ec70f96306d5f8c92d3254bbe3d3
wangyenshu <155622798+wangyenshu@users.noreply.github.com>
2026-09-22 11:26:44 -0400

    restore test_passagemath_gap.py

--- recipes/recipes_emscripten/passagemath-gap/test_passagemath_gap.py
+++ recipes/recipes_emscripten/passagemath-gap/test_passagemath_gap.py
@@ -3,9 +3,3 @@ import pytest
 
 def test_import_passagemath_gap():
     import passagemath_gap
-
-
-def test_libgap():
-    import passagemath_gap
-    from sage.libs.gap.libgap import libgap
-    libgap(10)
