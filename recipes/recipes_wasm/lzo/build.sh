1e8754fb6f4f35c8907144c7c2721b40828c8a39
Isabel Paredes <isabel.paredes@quantstack.net>
2026-09-23 17:06:25 +0200

    Update recipe

--- recipes/recipes_wasm/lzo/build.sh
+++ recipes/recipes_wasm/lzo/build.sh
@@ -5,12 +5,14 @@ set -ex
 mkdir -p build
 cd build
 
+unset cmake
+
 emcmake cmake -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_PREFIX_PATH=${PREFIX} \
       -DCMAKE_INSTALL_PREFIX=${PREFIX} \
       -DCMAKE_INSTALL_LIBDIR=${PREFIX}/lib \
       -DCMAKE_INSTALL_RPATH=${PREFIX}/lib \
-      -DENABLE_SHARED=OFF \
+      -DENABLE_SHARED=ON \
       -DENABLE_STATIC=ON \
       ${CMAKE_ARGS} ${SRC_DIR}
 
@@ -18,5 +20,7 @@ make -j${CPU_COUNT} ${VERBOSE_CM}
 
 make -j${CPU_COUNT} install
 
-# Remove examples
-rm -r $PREFIX/libexec/lzo
+cp ./lzopack.wasm  $PREFIX/libexec/lzo/examples/
+cp ./lzotest.wasm  $PREFIX/libexec/lzo/examples/
+cp ./simple.wasm   $PREFIX/libexec/lzo/examples/
+cp ./testmini.wasm $PREFIX/libexec/lzo/examples/
