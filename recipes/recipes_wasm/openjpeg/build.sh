4eb124a1b86402295cfee114928b585142891914
Thorsten Beier <thorsten.beier@quantstack.net>
2026-09-24 16:06:58 +0200

    more deps (#6940)

--- recipes/recipes_wasm/openjpeg/build.sh
+++ recipes/recipes_wasm/openjpeg/build.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-declare -a CMAKE_PLATFORM_FLAGS
-if [[ ${target_platform} == osx-64 ]]; then
-  CMAKE_PLATFORM_FLAGS+=(-DCMAKE_OSX_SYSROOT="${CONDA_BUILD_SYSROOT}")
-fi
+
+
 
 mkdir build || true
 pushd build
@@ -17,15 +15,12 @@ pushd build
         -DPNG_PNG_INCLUDE_DIR=$PREFIX/include \
         -DZLIB_LIBRARY=$PREFIX/lib/libz.a \
         -DZLIB_INCLUDE_DIR=$PREFIX/include \
+        -DOPJ_USE_THREAD=OFF \
         -DBUILD_JPWL=OFF \
         -DBUILD_CODEC=OFF \
-        "${CMAKE_PLATFORM_FLAGS[@]}" \
         $SRC_DIR
 
   make -j${CPU_COUNT} ${VERBOSE_CM}
-if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR}" != "" ]]; then
-  ctest
-fi
   make install -j${CPU_COUNT}
 
 popd
