46709f34935fc37937bc085842dbf483cc406756
Isabel Paredes <isabel.paredes@quantstack.net>
2026-09-24 13:46:55 +0200

    Update pcre2

--- recipes/recipes_wasm/pcre2/build.sh
+++ recipes/recipes_wasm/pcre2/build.sh
@@ -8,17 +8,27 @@ fi
 
 export CFLAGS="$CFLAGS -fPIC -fwasm-exceptions -I$PREFIX/include"
 export LDFLAGS="$LDFLAGS -fwasm-exceptions -L$PREFIX/lib"
+export AM_LDFLAGS="-static" # for executables
 
 ./autogen.sh
+
+# Remove this from the copied file and use -shared only
+sed -i 's/-sSIDE_MODULE=2 //g' m4/libtool.m4
+
 emconfigure ./configure \
     --prefix=$PREFIX \
     --host="${host}" \
-    --disable-shared \
+    --enable-shared \
     --enable-pcre2-16 \
     --enable-pcre2-32
+
+# Needs to be removed again from the generated file
+sed -i 's/-sSIDE_MODULE=2 //g' libtool
 
 make -j${CPU_COUNT}
 make install
 
 # Install wasm files as well
 cp ./pcre2*.wasm $PREFIX/bin
+
+rm $PREFIX/lib/*.la
