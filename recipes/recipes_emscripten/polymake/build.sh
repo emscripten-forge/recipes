a42d35f8849594896828b458b98900c5730b5673
wangyenshu <155622798+wangyenshu@users.noreply.github.com>
2026-09-26 11:15:14 -0400

    Update build.sh

--- recipes/recipes_emscripten/polymake/build.sh
+++ recipes/recipes_emscripten/polymake/build.sh
@@ -89,8 +89,8 @@ emcc -c "${RECIPE_DIR}/polymake_wasm_stubs.c" -o "${SRC_DIR}/polymake_wasm_stubs
     --without-java \
     --without-javaview \
     --without-polydb \
-    --with-scip="${PREFIX}" \
-    --with-soplex="${PREFIX}" \
+    --without-scip \
+    --without-soplex \
     --without-sympol
 
 # configure used the native Perl toolchain; retarget generated build settings to WASM.
